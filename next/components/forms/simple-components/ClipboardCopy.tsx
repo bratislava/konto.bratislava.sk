160ba4e6137e942a99a92f534fdb5ac8f7d40f55
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-03-16 14:39:50 +0100

    copy to clipboard text translation

--- next/components/forms/simple-components/ClipboardCopy.tsx
+++ next/components/forms/simple-components/ClipboardCopy.tsx
@@ -1,4 +1,5 @@
 import ContentCopy from '@assets/images/content_copy.svg'
+import { useTranslation } from 'next-i18next'
 import { useSnackbar } from 'react-simple-snackbar'
 import { useCopyToClipboard } from 'usehooks-ts'
 
@@ -9,13 +10,14 @@ const optionsInfo = {
 }
 const ClipboardCopy = ({ copyText }: { copyText: string }) => {
   const [_, copy] = useCopyToClipboard()
+  const { t } = useTranslation('account')
   const [openSnackbarInfo] = useSnackbar(optionsInfo)
   return (
     <button
       type="button"
       onClick={() => {
         copy(copyText)
-        openSnackbarInfo('IBAN bol skopírovaný', 3000)
+        openSnackbarInfo(t('iban_copied'), 3000)
       }}
     >
       <ContentCopy />

8e949fcdb39e2936ba259413c9ea3b43efb8a202
Valerii Medvezhonkov <65829237+ValeriiMedvezhonkov@users.noreply.github.com>
2023-03-19 23:58:49 +0100

    Form 266/payment data footer fix accordion (#13)
    * Accordion payment schedule as accordion
    
    * remove console log
    
    * add translations
    
    * fixed 'expand more icon', made it responsive.
    
    * fix translation
    
    * switcher
    
    * Form 306/stepper mobile fix (#738)
    
    * fix: scrolling in stepper
    
    * fix: not possible to skip to first
    
    * fix: mobile breakpoints
    
    * chore: remove mysterious spamming console log
    
    * fix: scroll problem (tested multiple things but i dont see problems which were sent from testing
    
    * fix: broken skipping of steps because of new skip modal
    
    * Fix UA chat (#717)
    
    * Fix UA chat
    
    * Move chat from _app.tsx
    
    * Remove import
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>
    
    * fix translations
    
    * copy to clipboard text translation
    
    * fix next config issue from main repo
    
    * fix package.json issue from main repo
    
    * remove shadow effect, add TODO comment

--- next/components/forms/simple-components/ClipboardCopy.tsx
+++ next/components/forms/simple-components/ClipboardCopy.tsx
@@ -1,10 +1,25 @@
 import ContentCopy from '@assets/images/content_copy.svg'
+import { useTranslation } from 'next-i18next'
+import { useSnackbar } from 'react-simple-snackbar'
 import { useCopyToClipboard } from 'usehooks-ts'
 
+const optionsInfo = {
+  style: {
+    backgroundColor: 'rgb(var(--color-gray-700))',
+  },
+}
 const ClipboardCopy = ({ copyText }: { copyText: string }) => {
   const [_, copy] = useCopyToClipboard()
+  const { t } = useTranslation('account')
+  const [openSnackbarInfo] = useSnackbar(optionsInfo)
   return (
-    <button type="button" onClick={() => copy(copyText)}>
+    <button
+      type="button"
+      onClick={() => {
+        copy(copyText)
+        openSnackbarInfo(t('iban_copied'), 3000)
+      }}
+    >
       <ContentCopy />
     </button>
   )

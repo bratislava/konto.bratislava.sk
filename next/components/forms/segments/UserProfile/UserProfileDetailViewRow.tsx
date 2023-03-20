88a89020ec5e23cc3e1a13bbeaa62868613173bf
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-03-20 13:04:05 +0100

    Revert "Merge branch 'master' into FORM-297/municipal-services-fix-filter-and-pagination"
    This reverts commit 9b791ec699f2e87b3d63424b31f6b2539e83d503, reversing
    changes made to 08cefc207c4bb3526c3cb29f0a0a20eb6c216187.

--- next/components/forms/segments/UserProfile/UserProfileDetailViewRow.tsx
+++ next/components/forms/segments/UserProfile/UserProfileDetailViewRow.tsx
@@ -9,11 +9,11 @@ interface UserProfileDetailViewRowProps {
 const UserProfileDetailViewRow = ({ label, value, tooltip }: UserProfileDetailViewRowProps) => {
   return (
     <div className={cx('w-full flex-wrap flex flex-col gap-2', 'md:flex-row')}>
-      <div className="grow md:w-[50%] flex flex-row items-center gap-3">
+      <div className="grow w-[50%] flex flex-row items-center gap-3">
         <span className="text-p2-semibold">{label}</span>
         {tooltip && <Tooltip text={tooltip} />}
       </div>
-      <span className="text-p2 grow md:w-[49%]">{!value || value === '' ? '-' : value}</span>
+      <span className="text-p2 grow w-[49%]">{!value || value === '' ? '-' : value}</span>
     </div>
   )
 }

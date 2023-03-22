a6223016670a89a7ae2b8936d2795aef88b8067e
Martin Pinter <mpinter@users.noreply.github.com>
2023-03-22 12:58:15 +0100

    FORM-XXX: Loading button with spinner (#42)
    * Loading button with spinner
    
    * Fix spinner colors and spacing
    
    * Remove needless Copilot comment

--- next/components/forms/simple-components/Spinner.tsx
+++ next/components/forms/simple-components/Spinner.tsx
@@ -1,19 +1,28 @@
 import cx from 'classnames'
 
 type SpinnerBase = {
-  size?: 'lg' | 'md' |'sm'
+  size?: 'lg' | 'md' | 'sm'
+  variant?: 'black' | 'gray' | 'category' | 'negative'
   className?: string
 }
 
-const Spinner = ({ size = 'md', className }:SpinnerBase) => {
-  const style = cx('rounded-[50%] animate-spin border-solid border-gray-700 border-t-gray-300', className, {
-    'w-5 h-5 border-2 border-t-2': size === 'sm',
-    'w-8 h-8 border-3 border-t-3': size === 'md',
-    'w-12 h-12 border-4 border-t-4': size === 'lg'
-  })
-  return (
-    <div className={style}/>
+const Spinner = ({ size = 'md', variant = 'black', className }: SpinnerBase) => {
+  const style = cx(
+    'rounded-[50%] animate-spin border-solid',
+    {
+      'border-category-400 border-t-gray-50': variant === 'category',
+      'border-gray-400 border-t-gray-50': variant === 'gray',
+      'border-gray-700 border-t-gray-300': variant === 'black',
+      'border-negative-400 border-t-gray-50': variant === 'negative',
+    },
+    className,
+    {
+      'w-5 h-5 border-2 border-t-2': size === 'sm',
+      'w-8 h-8 border-3 border-t-3': size === 'md',
+      'w-12 h-12 border-4 border-t-4': size === 'lg',
+    },
   )
+  return <div className={style} />
 }
 
 export default Spinner

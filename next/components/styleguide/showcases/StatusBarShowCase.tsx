fc0b786a251878f87aeff838ac68cfcee3b80b3d
Martin Pinter <mpinter@users.noreply.github.com>
2023-03-24 00:00:22 +0100

    FROM-331: StatusBar component on top of the page (#50)
    * StatusBar component on top of the page
    
    * Fix header and status bar behaviour

---
+++ next/components/styleguide/showcases/StatusBarShowCase.tsx
@@ -0,0 +1,28 @@
+import { useStatusBarContext } from 'components/forms/info-components/StatusBar'
+import Button from '../../forms/simple-components/Button'
+import { Stack } from '../Stack'
+import { Wrapper } from '../Wrapper'
+
+interface StatusBarShowCaseProps {}
+
+const StatusBarShowCase = ({}: StatusBarShowCaseProps) => {
+  const { setStatusBarContent } = useStatusBarContext()
+  return (
+    <Wrapper direction="column" title="StatusBar">
+      <Stack>
+        <Button
+          variant="category"
+          text="Show status bar at the top of the page"
+          onPress={() =>
+            setStatusBarContent(
+              'Laborum velit dolore enim voluptate sint anim occaecat est sit. Mollit nisi incididunt eu officia Lorem occaecat culpa consectetur voluptate aute veniam. Est eu irure ut eiusmod ut nisi ex fugiat laboris qui ad eiusmod. Enim aliqua ut occaecat nisi minim sint veniam ipsum sit in aute.',
+            )
+          }
+        />
+        <Button variant="category" text="Hide status bar" onPress={() => setStatusBarContent('')} />
+      </Stack>
+    </Wrapper>
+  )
+}
+
+export default StatusBarShowCase

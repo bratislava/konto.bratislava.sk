31b1c15bd00f4eda7430d4dc056e0a703b9ce92a
Martin Pinter <martin.pinter@bratislava.sk>
2023-04-03 13:19:28 +0200

    Faro user status tracing & minor fixes

--- next/utils/logger.ts
+++ next/utils/logger.ts
@@ -14,7 +14,7 @@ let _faro: Faro
 if (isBrowser()) {
   _logger = new Logger({ type: isProductionDeployment() ? 'hidden' : 'pretty' })
   // no remote logging in local development
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'production') {
     _faro = initializeFaro({
       url: 'https://faro.bratislava.sk/collect',
       apiKey: process.env.NEXT_PUBLIC_FARO_SECRET,

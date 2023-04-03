a7ba21a2dced7cdd993257599fbafdb9fcbcaff6
matejpeluha <matej.peluha@gmail.com>
2023-04-03 18:04:01 +0200

    refactor: remove only-warn

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['auto', 'plugin:tailwindcss/recommended', 'plugin:@next/next/recommended'],
-  plugins: ['only-warn'],
+  plugins: [],
   rules: {
     'react/react-in-jsx-scope': 'off',
     /** We use this a lot with isDefined and hasAttributes */

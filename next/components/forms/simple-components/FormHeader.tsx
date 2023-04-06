f48667fd846d41b330b0f0d6b5effda15472103a
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-04-06 19:28:09 +0200

    FORM-243
    creating menu for form header, modifying Menu component, finding a solution

--- next/components/forms/simple-components/FormHeader.tsx
+++ next/components/forms/simple-components/FormHeader.tsx
@@ -2,10 +2,47 @@ import DiskIcon from '@assets/images/forms/disk-icon.svg'
 import ThreePointsIcon from '@assets/images/forms/three-points-icon.svg'
 import Waves from 'components/forms/icon-components/Waves'
 import Button from 'components/forms/simple-components/Button'
+import FormMenu from 'components/forms/simple-components/FormMenu/FormMenu'
+import Menu from 'components/forms/simple-components/Menu/Menu'
+import { useTranslation } from 'next-i18next'
+import { ReactNode } from 'react'
+import { Item } from 'react-stately'
+
+export interface MenuItem {
+  id: number
+  title: string
+  icon: ReactNode
+  link: string
+  backgroundColor?: string // ex. bg-negative-700
+}
+
+const FormMenuItem = ({ menuItem }: { menuItem: MenuItem }) => {
+  const { t } = useTranslation()
+
+  return (
+    <button type="button" tabIndex={1} className="cursor-pointer flex py-2 px-5">
+      <div
+        className={`flex relative flex-row items-start gap-2 rounded-xl p-4 ${
+          menuItem.backgroundColor ?? 'bg-gray-50'
+        }`}
+      >
+        <div className="flex h-2 w-2 items-center justify-center">
+          <span>{menuItem.icon}</span>
+        </div>
+      </div>
+      <div
+        className="text-p2 hover:text-p2-semibold w-fit-title text-font p-2 whitespace-nowrap"
+        title="sss"
+      >
+        sss11
+      </div>
+    </button>
+  )
+}
 
 const FormHeader = () => {
   return (
-    <div className="flex flex-col pt-[57px] relative">
+    <div className="flex flex-col relative">
       <div className="w-full h-[68px] bg-main-200">
         <div className="max-w-screen-lg mx-auto h-full flex items-center">E-sluzby</div>
       </div>
@@ -20,12 +57,20 @@ const FormHeader = () => {
               text="Uložiť ako koncept"
               className="text-gray-700 hover:text-gray-600 focus:text-gray-800"
             />
-            <Button
+            <Menu buttonIcon={<ThreePointsIcon />} label="">
+              {[
+                { id: 1, title: 'ss', icon: <ThreePointsIcon />, link: '/' },
+                { id: 2, title: 'ss', icon: <ThreePointsIcon />, link: '/' },
+              ].map((option) => (
+                <Item key={option.id}>asdasdasd</Item>
+              ))}
+            </Menu>
+            {/* <Button
               className="text-gray-700 hover:text-gray-600 focus:text-gray-800"
               size="sm"
               variant="category-outline"
               icon={<ThreePointsIcon />}
-            />
+            /> */}
           </div>
         </div>
       </div>

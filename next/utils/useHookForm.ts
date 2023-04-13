c04ea55163b1e545fc55ac74d6f77ca186ad24be
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-04-13 12:24:14 +0200

    FORM-442: Update functionality of psc inputField in profile and correspondence address modal (#153)
    FORM-442
    
    update functionality of psc inputField in profile and correspondence address modal

--- next/utils/useHookForm.ts
+++ next/utils/useHookForm.ts
@@ -23,7 +23,8 @@ export default function useHookForm<T extends FieldValues>({ schema, defaultValu
         email:
           "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$",
         password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d).{8,}$/,
-        postalCode: '^([0-9]{5}|)$',
+        postalCode: '^\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d$',
+        // postalCode: '^([0-9]{5}|)$',
         idCard: '^([a-zA-Z]{2})([0-9]{6})([0-9]?)$',
         rc: (value: string) => {
           value = value.replace('/', '')

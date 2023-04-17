49f5148208616500a29a827f22db645385fea836
matejpeluha <matej.peluha@gmail.com>
2023-04-17 16:36:01 +0200

    fix: simple-import-sort/imports

--- next/pages/registracia.tsx
+++ next/pages/registracia.tsx
@@ -2,6 +2,7 @@ import { ROUTES } from '@utils/constants'
 import { formatUnicorn } from '@utils/string'
 import { AsyncServerProps } from '@utils/types'
 import useAccount, { AccountStatus } from '@utils/useAccount'
+import { isProductionDeployment } from '@utils/utils'
 import AccountActivator from 'components/forms/segments/AccountActivator/AccountActivator'
 import AccountContainer from 'components/forms/segments/AccountContainer/AccountContainer'
 import AccountMarkdown from 'components/forms/segments/AccountMarkdown/AccountMarkdown'
@@ -10,12 +11,11 @@ import EmailVerificationForm from 'components/forms/segments/EmailVerificationFo
 import RegisterForm from 'components/forms/segments/RegisterForm/RegisterForm'
 import LoginRegisterLayout from 'components/layouts/LoginRegisterLayout'
 import { GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useRouter } from 'next/router'
 
 import PageWrapper from '../components/layouts/PageWrapper'
-import { isProductionDeployment } from '../utils/utils'
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const locale = ctx.locale ?? 'sk'

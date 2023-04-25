ba8f3866dc58061e1928eb9a940751f494bddaa2
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-04-25 19:37:09 +0200

    FORM-449: Add warning Alert (need to refactor all component) to profile and add success tag to header (#169)
    * FORM-449
    
    Add warning Alert (need to refactor all component) to profile and add success tag to header
    
    * FORM-449
    
    Add component IdentityVerificationStatus to header and hamburgerMenu
    
    * FORM-449
    
    Refactor Alert component to more flexible component, edit Alert in UserProfileDetail
    
    * FORM-449
    
    update design for IdentityVerificationStatus, fix Alert buttonPosition ('right') for mobile, add texts to locale file
    
    * FORM-449
    
    fix github comments

--- next/components/forms/segments/UserProfile/UserProfileSectionHeader.tsx
+++ next/components/forms/segments/UserProfile/UserProfileSectionHeader.tsx
@@ -1,4 +1,7 @@
+import useAccount, { AccountStatus } from '@utils/useAccount'
 import cx from 'classnames'
+import Alert from 'components/forms/info-components/Alert'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 interface UserProfileSectionHeaderProps {
@@ -8,6 +11,7 @@ interface UserProfileSectionHeaderProps {
   isMobileColumn?: boolean
   isEditing?: boolean
   children?: React.ReactNode
+  mainHeader?: boolean
 }
 
 const UserProfileSectionHeader = ({
@@ -17,20 +21,50 @@ const UserProfileSectionHeader = ({
   isMobileColumn,
   isEditing,
   children,
+  mainHeader,
 }: UserProfileSectionHeaderProps) => {
+  const { status } = useAccount()
+  const { t } = useTranslation('account')
   return (
     <div
-      className={cx('border-gray-200 p-4 flex gap-3', 'md:flex-row md:flex-wrap md:px-8 md:py-6', {
-        'border-b-2': underline,
-        'flex-col': isMobileColumn,
-        'flex-row': !isMobileColumn,
-      })}
+      className={cx(
+        'border-gray-200 p-4 flex flex-col gap-6',
+        'md:flex-row md:flex-wrap md:px-8 md:py-6',
+        {
+          'border-b-2': underline,
+          'flex-col': isMobileColumn,
+          'flex-row': !isMobileColumn,
+        },
+      )}
     >
-      <div className="flex flex-col grow">
-        <h5 className={cx('text-h5-bold', 'md:text-h4-bold')}>{title}</h5>
-        <p className={cx('text-p2-normal', 'md:block', { hidden: isEditing })}>{text}</p>
+      <div className="w-full flex items-center justify-between">
+        <div className="flex flex-col grow gap-2">
+          <div className="flex items-center gap-3 md:gap-2">
+            <h5 className={cx('text-h5-bold', 'md:text-h4-bold')}>{title}</h5>
+            {mainHeader && status === AccountStatus.IdentityVerificationSuccess && (
+              <span className="text-p3-medium px-2 bg-success-100 text-success-700 rounded-[4px]">
+                {t('verification_status_success')}
+              </span>
+            )}
+          </div>
+          <p className={cx('text-p2-normal', 'md:block', { hidden: isEditing })}>{text}</p>
+        </div>
+        {children && <div>{children}</div>}
       </div>
-      {children && <div>{children}</div>}
+      {mainHeader && status !== AccountStatus.IdentityVerificationSuccess && (
+        <Alert
+          title={t('verification_status_required')}
+          message={t('verification_status_required_alert')}
+          type="warning"
+          buttons={[
+            {
+              title: t('verification_url_text'),
+              link: '/overenie-identity',
+            },
+          ]}
+          fullWidth
+        />
+      )}
     </div>
   )
 }

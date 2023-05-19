d5724c7c482085e702dc2a56698397fad29d090c
matejpeluha <matej.peluha@gmail.com>
2023-05-19 19:42:02 +0200

    chore: add console logs

--- next/frontend/api/api.ts
+++ next/frontend/api/api.ts
@@ -373,7 +373,7 @@ export const deleteFileFromBucket = async (fileName: string) => {
 
 export const scanFile = async (token: string | null, data: ScanFileDto) => {
   if (!token) throw new Error(MISSING_TOKEN)
-  console.log("DATA FOR SCAN", data)
+  console.log("DATA FOR SCAN", '\nformId:', data.formId, '\npospId', data.pospId, '\nuserExternalId', data.userExternalId)
   if (!data.pospId || !data.formId || !data.userExternalId || !data.fileUid) throw new Error(API_ERROR_TEXT)
   console.log("LETS START FETCH POST ON /files/scan")
   return fetchJsonApi(`${String(process.env.NEXT_PUBLIC_FORMS_URL)}/files/scan`, {

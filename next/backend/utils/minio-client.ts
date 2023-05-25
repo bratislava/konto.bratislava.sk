07b5f9d242cfe16cf44d004f876e5c0861f38ce3
matejpeluha <matej.peluha@gmail.com>
2023-05-25 15:33:14 +0200

    feat: change env for minio client

--- next/backend/utils/minio-client.ts
+++ next/backend/utils/minio-client.ts
@@ -1,22 +1,23 @@
 import { Client } from 'minio'
 
 export const region = "us-east-1"
-export const bucketName = process.env.NEXT_PUBLIC_MINIO_UNSCANNED_BUCKET || ''
-
+export const unscannedBucketName = process.env.MINIO_UNSCANNED_BUCKET || ''
+export const safeBucketName = process.env.MINIO_SAFE_BUCKET || ''
+export const infectedBucketName = process.env.MINIO_INFECTED_BUCKET || ''
 /*
 https://www.npmjs.com/package/minio
 TODO just for dev, likely to be replaced by slightly different approach
 TODO replace with a more restrictive key, this one limits it just to the bucket but allows all operations
  */
 const minioClient = new Client({
-  endPoint: process.env.NEXT_PUBLIC_MINIO_ENDPOINT || '',
-  port: parseInt(process.env.NEXT_PUBLIC_MINIO_ENDPOINT || '0', 10),
-  useSSL: typeof process.env.NEXT_PUBLIC_MINIO_USE_SSL === 'boolean'
-    ? process.env.NEXT_PUBLIC_MINIO_USE_SSL
+  endPoint: process.env.MINIO_ENDPOINT || '',
+  port: parseInt(process.env.MINIO_ENDPOINT || '0', 10),
+  useSSL: typeof process.env.MINIO_USE_SSL === 'boolean'
+    ? process.env.MINIO_USE_SSL
     : true,
   // eslint-disable-next-line no-secrets/no-secrets
-  secretKey: process.env.NEXT_PUBLIC_MINIO_SECRET_KEY || '',
-  accessKey: process.env.NEXT_PUBLIC_MINIO_ACCESS_KEY || ''
+  secretKey: process.env.MINIO_SECRET_KEY || '',
+  accessKey: process.env.MINIO_ACCESS_KEY || ''
 })
 
 export default minioClient

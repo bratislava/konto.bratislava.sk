768918ff1062b32ce8c2ec2031038ced2c76e35d
matejpeluha <matej.peluha@gmail.com>
2023-05-25 12:13:11 +0200

    featL apply envs

--- next/backend/utils/minio-client.ts
+++ next/backend/utils/minio-client.ts
@@ -1,7 +1,7 @@
 import { Client } from 'minio'
 
 export const region = "us-east-1"
-export const bucketName = "calmav-unscanned-bucket"
+export const bucketName = process.env.NEXT_PUBLIC_MINIO_UNSCANNED_BUCKET
 
 /*
 https://www.npmjs.com/package/minio
@@ -9,12 +9,14 @@ TODO just for dev, likely to be replaced by slightly different approach
 TODO replace with a more restrictive key, this one limits it just to the bucket but allows all operations
  */
 const minioClient = new Client({
-  endPoint: 'cdn-api.bratislava.sk',
-  port: 443,
-  useSSL: true,
+  endPoint: process.env.NEXT_PUBLIC_MINIO_ENDPOINT || '',
+  port: parseInt(process.env.NEXT_PUBLIC_MINIO_ENDPOINT || '0', 10),
+  useSSL: typeof process.env.NEXT_PUBLIC_MINIO_USE_SSL === 'boolean'
+    ? process.env.NEXT_PUBLIC_MINIO_USE_SSL
+    : true,
   // eslint-disable-next-line no-secrets/no-secrets
-  secretKey: 'nRcXzl7zbO9jjt4dotpXJ011OtL8d8kj',
-  accessKey: 'FORMS_DEV'
+  secretKey: process.env.NEXT_PUBLIC_MINIO_SECRET_KEY || '',
+  accessKey: process.env.NEXT_PUBLIC_MINIO_ACCESS_KEY || ''
 })
 
 export default minioClient

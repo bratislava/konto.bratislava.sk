0c45e09d32e6f041899db5ed0975989bc630d582
Marek Bodinger <marek.bodinger@gmail.com>
2023-06-21 16:14:46 +0200

    Axios instance Bearer

--- next/backend/utils/axios-instance.ts
+++ next/backend/utils/axios-instance.ts
@@ -1,3 +1,18 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create()
+
+// Adds a possibility to provide `accessToken` to Axios request options.
+// TODO: Move to client factories after Cognito refactor.
+declare module 'axios' {
+  interface InternalAxiosRequestConfig {
+    accessToken: boolean
+  }
+}
+axiosInstance.interceptors.request.use((config) => {
+  if (config.accessToken) {
+    // eslint-disable-next-line no-param-reassign,@typescript-eslint/restrict-template-expressions
+    config.headers.Authorization = `Bearer ${config.accessToken}`
+  }
+  return config
+})

0dff251be3f4596f02f697d8207c143b29bf4036
Marek Bodinger <marek.bodinger@gmail.com>
2023-06-29 16:58:00 +0200

    Feature toggles in env variables

--- next/environment.ts
+++ next/environment.ts
@@ -1,7 +1,7 @@
 // Inspired by https://jfranciscosousa.com/blog/validating-environment-variables-with-zod/
 // Secures typesafe access to environmental variables.
-// In browser process.env is an empty object, the values are replaced during the build time so they need to be accessed
-// via process.env.NEXT_PUBLIC_*
+// In browser process.env is an empty object, the values are replaced during the build time, so they need to be accessed
+// via process.env.NEXT_PUBLIC_...
 
 /* eslint-disable no-process-env */
 function assertEnv<T>(variable: string, value: T) {
@@ -42,4 +42,17 @@ export const environment = {
     process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY,
   ),
   faroSecret: assertEnv('NEXT_PUBLIC_FARO_SECRET', process.env.NEXT_PUBLIC_FARO_SECRET),
+  featureToggles: {
+    forms:
+      assertEnv(
+        'NEXT_PUBLIC_FEATURE_TOGGLE_FORMS',
+        process.env.NEXT_PUBLIC_FEATURE_TOGGLE_FORMS,
+      ) === 'true',
+    pravnickaOsobaRegistration:
+      assertEnv(
+        // eslint-disable-next-line no-secrets/no-secrets
+        'NEXT_PUBLIC_FEATURE_TOGGLE_PRAVNICKA_OSOBA_REGISTRATION',
+        process.env.NEXT_PUBLIC_FEATURE_TOGGLE_PRAVNICKA_OSOBA_REGISTRATION,
+      ) === 'true',
+  },
 }

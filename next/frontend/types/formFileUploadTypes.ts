edcb4ace89763e3ab956fb040b46c8d27819f323
Marek Bodinger <marek.bodinger@gmail.com>
2023-07-10 15:51:29 +0200

    Form file upload types

---
+++ next/frontend/types/formFileUploadTypes.ts
@@ -0,0 +1,50 @@
+export interface FormFileUploadContextType {
+  uploadFiles: (files: File[], constraints: FormFileUploadConstraints) => string[]
+  removeFiles: (ids: string[]) => void
+  retryFile: (id: string, constraints: FormFileUploadConstraints) => string | null
+  getFileInfoById: (id: string) => FormFileUploadFileInfo
+}
+
+export interface FormFileUploadConstraints {
+  maxFileSize?: number
+  supportedFormats?: string[]
+}
+
+export enum FormFileUploadStatusEnum {
+  UploadQueued = 'UploadQueued',
+  Uploading = 'Uploading',
+  UploadError = 'UploadError',
+  UploadDone = 'UploadDone',
+  Scanning = 'Scanning',
+  ScanError = 'ScanError',
+  ScanDone = 'ScanDone',
+  ScanInfected = 'ScanInfected',
+  UnknownFile = 'UnknownFile',
+}
+
+export type FormFileUploadClientFileStatus =
+  | { type: FormFileUploadStatusEnum.UploadQueued }
+  | {
+      type: FormFileUploadStatusEnum.Uploading
+      progress: number
+    }
+  | { type: FormFileUploadStatusEnum.UploadError; error: string; canRetry: boolean }
+  | { type: FormFileUploadStatusEnum.UploadDone }
+  | { type: FormFileUploadStatusEnum.UnknownFile }
+
+export type FormFileUploadClientFileInfo = {
+  id: string
+  file: File
+  status: FormFileUploadClientFileStatus
+}
+export type FormFileUploadServerFileStatus =
+  | { type: FormFileUploadStatusEnum.Scanning }
+  | { type: FormFileUploadStatusEnum.ScanError }
+  | { type: FormFileUploadStatusEnum.ScanDone }
+  | { type: FormFileUploadStatusEnum.ScanInfected }
+
+export type FormFileUploadFileStatus =
+  | FormFileUploadClientFileStatus
+  | FormFileUploadServerFileStatus
+
+export type FormFileUploadFileInfo = { status: FormFileUploadFileStatus; fileName: string }

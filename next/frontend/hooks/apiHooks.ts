fedd64b4e250d75da38505b738caea7db3e013ef
Jan Rosina <jan.rosina@bratislava.sk>
2023-07-11 10:18:57 +0200

    useSWR replaced by useQuery

--- next/frontend/hooks/apiHooks.ts
+++ next/frontend/hooks/apiHooks.ts
@@ -1,7 +1,6 @@
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 import { useEffect } from 'react'
-import useSWR from 'swr'
 
 import { getTaxApi } from '../api/api'
 import { ajvFormats, ajvKeywords } from '../dtos/formStepperDto'
@@ -9,31 +8,32 @@ import { TaxApiError } from '../dtos/generalApiDto'
 import { Tax, TaxJSONSchema } from '../dtos/taxDto'
 import logger from '../utils/logger'
 import useAccount from './useAccount'
+import { useQuery } from '@tanstack/react-query'
 
 // TODO test for no tax, not yet tax, legit tax unpaid, partially paid, paid
 export const useTaxes = () => {
   const { lastAccessToken } = useAccount()
   // TODO handle 401 & token refreshing - for now, this should work reasonably as along as the page isn't opened for too long
-  const swrResult = useSWR<Tax>(
-    () => (lastAccessToken ? ['/api/taxes', lastAccessToken] : null),
-    ([, token]: [void, string]) => getTaxApi(token),
-    { shouldRetryOnError: false },
+  const queryResult = useQuery<Tax>(
+    ['/api/taxes', lastAccessToken],
+    () => getTaxApi(lastAccessToken),
+    { retry: false },
   )
 
   useEffect(() => {
     if (
-      swrResult.error &&
-      swrResult.error instanceof TaxApiError &&
-      swrResult.error.status !== 422
+      queryResult.error &&
+      queryResult.error instanceof TaxApiError &&
+      queryResult.error.status !== 422
     ) {
-      logger.error('Error while fetching tax data', swrResult.error)
+      logger.error('Error while fetching tax data', queryResult.error)
     }
-  }, [swrResult.error])
+  }, [queryResult.error])
 
   // validate data according to schema
   // if they don't look right, we still try to display them, but we'
   useEffect(() => {
-    if (swrResult.data) {
+    if (queryResult.data) {
       // todo use single ajv instance with multiple schemas, as per recommendations - https://ajv.js.org/guide/managing-schemas.html#compiling-during-initialization
       const ajv = new Ajv({
         keywords: ajvKeywords,
@@ -42,11 +42,11 @@ export const useTaxes = () => {
       addFormats(ajv)
 
       const validate = ajv.compile(TaxJSONSchema)
-      const result = validate(swrResult.data)
+      const result = validate(queryResult.data)
       if (!result) {
         logger.error('Tax data does not match expected schema', validate?.errors)
       }
     }
-  }, [swrResult.data])
-  return swrResult
+  }, [queryResult.data])
+  return queryResult
 }

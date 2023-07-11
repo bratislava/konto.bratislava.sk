0c797462ea9de7fbaa964637f79f5fc6c73c2e16
Marek Bodinger <marek.bodinger@gmail.com>
2023-07-11 14:31:49 +0200

    Add simple test

--- next/__tests__/form-file-upload.test.tsx
+++ next/__tests__/form-file-upload.test.tsx
@@ -1,27 +1,75 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { renderHook } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 import React from 'react'
 
 import {
   FormFileUploadStateProvider,
+  FormFileUploadStateProviderProps,
   useFormFileUpload,
 } from '../components/forms/useFormFileUpload'
+import { FormFileUploadStatusEnum } from '../frontend/types/formFileUploadTypes'
+import { uploadFile } from '../frontend/utils/formFileUpload'
 
 const queryClient = new QueryClient()
 
-/*
- * TODO add actual tests
+jest.mock('../frontend/utils/formFileUpload', () => ({
+  ...jest.requireActual('../frontend/utils/formFileUpload'),
+  uploadFile: jest.fn(),
+}))
+
+/**
+ * TODO: Add 20+ tests.
  */
-test('FormFileUploadStateProvider provides context to children', async () => {
-  const wrapper = ({ children }) => (
-    <QueryClientProvider client={queryClient}>
-      <FormFileUploadStateProvider initialFormData={{ formId: 'testFormId', files: [] }}>
-        {children}
-      </FormFileUploadStateProvider>
-    </QueryClientProvider>
-  )
+describe('useFormFileUpload', () => {
+  const setupTest = ({ providerProps }: { providerProps: FormFileUploadStateProviderProps }) => {
+    ;(uploadFile as jest.Mock).mockImplementation(
+      ({
+        onProgress,
+        onSuccess,
+        onError,
+      }: {
+        onProgress: (progressPercentage: number) => void
+        onSuccess: (response: any) => void
+        onError: (error: Error) => void
+      }) => {
+        // TODO: Implement
+      },
+    )
 
-  const { result } = renderHook(() => useFormFileUpload(), { wrapper })
+    const wrapper = ({ children }) => (
+      <QueryClientProvider client={queryClient}>
+        <FormFileUploadStateProvider {...providerProps}>{children}</FormFileUploadStateProvider>
+      </QueryClientProvider>
+    )
 
-  expect(result.current).toBeDefined()
+    const { result } = renderHook(() => useFormFileUpload(), { wrapper })
+
+    return { getHook: () => result.current }
+  }
+
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('FormFileUploadStateProvider provides context to children', async () => {
+    const setup = setupTest({ providerProps: { initialFormData: { formId: '', files: [] } } })
+    expect(setup.getHook()).toBeDefined()
+  })
+
+  it('FormFileUploadStateProvider can upload file', async () => {
+    const setup = setupTest({ providerProps: { initialFormData: { formId: '', files: [] } } })
+    const file = new File([''], 'filename.txt')
+
+    let ids: string[] = []
+    act(() => {
+      ids = setup.getHook().uploadFiles([file], {})
+    })
+
+    await waitFor(() =>
+      expect(setup.getHook().getFileInfoById(ids[0])).toEqual({
+        fileName: 'filename.txt',
+        status: { type: FormFileUploadStatusEnum.Uploading, progress: 0 },
+      }),
+    )
+  })
 })

9302bc2fcffdb650e85d02204e07a37d54bf6697
Marek Bodinger <marek.bodinger@gmail.com>
2023-07-18 14:28:16 +0200

    Fix ThemedForm types

--- next/components/forms/ThemedForm.tsx
+++ next/components/forms/ThemedForm.tsx
@@ -1,4 +1,5 @@
-import { withTheme } from '@rjsf/core'
+import { ThemeProps, withTheme } from '@rjsf/core'
+import { GenericObjectType } from '@rjsf/utils'
 import DatePickerWidgetRJSF from 'components/forms/widget-wrappers/DatePickerWidgetRJSF'
 import InputFieldWidgetRJSF from 'components/forms/widget-wrappers/InputFieldWidgetRJSF'
 import TimePickerWidgetRJSF from 'components/forms/widget-wrappers/TimePickerWidgetRJSF'
@@ -15,7 +16,8 @@ import UploadWidgetRJSF from './widget-wrappers/UploadWidgetRJSF'
 
 // you can add custom widgets as well as override the default ones
 // we'll want to override all the default widgets listed here https://react-jsonschema-form.readthedocs.io/en/latest/advanced-customization/custom-widgets-fields/
-const theme = {
+
+const theme: ThemeProps = {
   widgets: {
     SelectField: SelectFieldWidgetRJSF,
     InputField: InputFieldWidgetRJSF,
@@ -27,15 +29,17 @@ const theme = {
     TimePicker: TimePickerWidgetRJSF,
   },
   fields: {
+    // TODO: Fix types
+    // @ts-ignore
     doubledInput: DoubledInputWidgetFieldRJSF,
     dateFromTo: DateFromToWidgetRJSF,
+    // @ts-ignore
     timeFromTo: TimeFromToWidgetRJSF,
+    // @ts-ignore
     dateTime: DateTimeWidgetRJSF,
   },
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const ThemedForm = withTheme(theme)
+const ThemedForm = withTheme<GenericObjectType>(theme)
 
 export default ThemedForm

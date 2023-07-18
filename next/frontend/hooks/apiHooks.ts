9e5b74000654823e6fb70b751568fcd79eabac66
janickoR <krempo@bizonvitazi.sk>
2023-07-18 15:16:56 +0200

    replace useSwr with useQuery (#343)
    * branch initial commit
    
    * removed swr from apiHooks
    
    * import order
    
    * imports order
    
    ---------
    
    Co-authored-by: Jan Rosina <jan.rosina@bratislava.sk>

--- next/frontend/hooks/apiHooks.ts
+++ next/frontend/hooks/apiHooks.ts
@@ -1,7 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 import { useEffect } from 'react'
-import useSWR from 'swr'
 
 import { getTaxApi } from '../api/api'
 import { ajvFormats, ajvKeywords } from '../dtos/formStepperDto'
@@ -14,22 +14,22 @@ export const useTaxes = () => {
   // TODO handle 401 & token refreshing - for now, this should work reasonably as along as the page isn't opened for too long
   // TODO make sure log out -> log in as different user -> loading data does not load the data of previous user - without futher change it will now
   // TODO move towards react query
-  const swrResult = useSWR<Tax>(['/api/taxes'], () => getTaxApi(), { shouldRetryOnError: false })
+  const queryResult = useQuery<Tax>(['/api/taxes'], () => getTaxApi(), { retry: false })
 
   useEffect(() => {
     if (
-      swrResult.error &&
-      swrResult.error instanceof TaxApiError &&
-      swrResult.error.status !== 422
+      queryResult.error &&
+      queryResult.error instanceof TaxApiError &&
+      queryResult.error.status !== 422
     ) {
-      logger.error('Error while fetching tax data', swrResult.error)
+      logger.error('Error while fetching tax data', queryResult.error)
     }
-  }, [swrResult.error])
+  }, [queryResult.error])
 
   // validate data according to schema
   // if they don't look right, we still try to display them, but we'
   useEffect(() => {
-    if (swrResult.data) {
+    if (queryResult.data) {
       // todo use single ajv instance with multiple schemas, as per recommendations - https://ajv.js.org/guide/managing-schemas.html#compiling-during-initialization
       const ajv = new Ajv({
         keywords: ajvKeywords,
@@ -38,11 +38,11 @@ export const useTaxes = () => {
       addFormats(ajv)
 
       const validate = ajv.compile(TaxJSONSchema)
-      const result = validate(swrResult.data)
+      const result = validate(queryResult.data)
       if (!result) {
         logger.error('Tax data does not match expected schema', validate?.errors)
       }
     }
-  }, [swrResult.data])
-  return swrResult
+  }, [queryResult.data])
+  return queryResult
 }

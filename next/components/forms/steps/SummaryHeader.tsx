149ec0fa6b430c611ca1d22fecb298d85b6fb393
Marek Bodinger <marek.bodinger@gmail.com>
2023-08-15 15:14:34 +0200

    Summary header messages

--- next/components/forms/steps/SummaryHeader.tsx
+++ next/components/forms/steps/SummaryHeader.tsx
@@ -4,18 +4,29 @@ import Alert from '../info-components/Alert'
 import { useFormSummary } from './Summary/useFormSummary'
 
 const SummaryHeader = () => {
-  const { infectedFiles, scanErrorFiles } = useFormSummary()
+  const { infectedFiles, uploadingFiles, hasErrors } = useFormSummary()
   const { t } = useTranslation('forms')
 
-  // TODO: Improve messages
+  const infectedFilesFilenames = infectedFiles.map((file) => file.fileName)
 
   return (
     <>
       <h1 className="text-h1-medium font-semibold">{t('summary')}</h1>
+      {hasErrors && (
+        <Alert
+          type="error"
+          message={t('summary.form_has_errors')}
+          fullWidth
+          className="mt-4"
+          solid
+        />
+      )}
       {infectedFiles.length === 1 && (
         <Alert
           type="error"
-          message={t('errors.file_scan', { name: infectedFiles[0].fileName })}
+          message={t('summary.virus_alert', {
+            file: infectedFilesFilenames[0],
+          })}
           fullWidth
           className="mt-4"
           solid
@@ -24,16 +35,24 @@ const SummaryHeader = () => {
       {infectedFiles.length > 1 && (
         <Alert
           type="error"
-          message={t('errors.file_scan_multiple', {
-            name: infectedFiles.map((file) => file.fileName).join(', '),
+          message={t('summary.virus_alert_plural', {
+            files: infectedFilesFilenames.map((name) => `“${name}“`).join(', '),
           })}
           fullWidth
           className="mt-4"
           solid
         />
       )}
-      {scanErrorFiles.length > 0 && (
-        <Alert type="warning" message={t('warnings.file_scan')} fullWidth className="mt-4" />
+      {uploadingFiles.length > 0 && (
+        <Alert
+          type="warning"
+          message={t('summary.uploading_files', {
+            files: uploadingFiles.map((file) => file.fileName).join(', '),
+          })}
+          fullWidth
+          className="mt-4"
+          solid
+        />
       )}
     </>
   )

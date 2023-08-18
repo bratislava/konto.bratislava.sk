26e70ece5a06ed31fa4b5197d856b0a220f1c14f
Marek Bodinger <marek.bodinger@gmail.com>
2023-08-18 12:53:07 +0200

    Form leave protection hook

---
+++ next/components/forms/useFormLeaveProtection.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import React, { createContext, PropsWithChildren, useContext, useEffect, useRef } from 'react'
+import { useBeforeunload } from 'react-beforeunload'
+
+const useGetContext = () => {
+  const { t } = useTranslation('forms')
+  const router = useRouter()
+  const enabledRef = useRef(false)
+  useBeforeunload(() => (enabledRef.current ? t('info_messages.form_leave_protection') : null))
+
+  const turnOffLeaveProtection = () => {
+    enabledRef.current = false
+  }
+
+  const turnOnLeaveProtection = () => {
+    enabledRef.current = true
+  }
+
+  // https://stackoverflow.com/a/73977517
+  useEffect(() => {
+    const handler = () => {
+      // eslint-disable-next-line no-alert
+      if (enabledRef.current && !window.confirm(t('info_messages.form_leave_protection'))) {
+        router.events.emit('routeChangeError')
+        throw new Error('routeChange aborted.')
+      }
+    }
+
+    router.events.on('routeChangeStart', handler)
+
+    return () => {
+      router.events.off('routeChangeStart', handler)
+    }
+  }, [router, t])
+
+  return { turnOffLeaveProtection, turnOnLeaveProtection }
+}
+
+const FormLeaveProtectionContext = createContext<ReturnType<typeof useGetContext> | undefined>(
+  undefined,
+)
+
+export const FormLeaveProtectionProvider = ({ children }: PropsWithChildren) => {
+  const context = useGetContext()
+
+  return (
+    <FormLeaveProtectionContext.Provider value={context}>
+      {children}
+    </FormLeaveProtectionContext.Provider>
+  )
+}
+
+export const useFormLeaveProtection = () => {
+  const context = useContext(FormLeaveProtectionContext)
+  if (!context) {
+    throw new Error('useFormLeaveProtection must be used within a FormLeaveProtectionProvider')
+  }
+
+  return context
+}

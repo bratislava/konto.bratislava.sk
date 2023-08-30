fb0b8dc07b4f9dd6d17f5d801e3743d4e7d9f3de
Marek Bodinger <marek.bodinger@gmail.com>
2023-08-30 12:58:18 +0200

    Finally fix the semantics of stepper, use hashs for step navigation instead of function, remove unnecessary skip to step functions from form state, leave protection improvements

--- next/components/forms/steps/StepperViewRow.tsx
+++ next/components/forms/steps/StepperViewRow.tsx
@@ -1,23 +1,21 @@
 import { CheckIcon } from '@assets/ui-icons'
 import cx from 'classnames'
 import { useTranslation } from 'next-i18next'
-import React, { useRef } from 'react'
-import { useButton } from 'react-aria'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 import { FormStepperStep } from '../types/Steps'
 
 interface StepperViewRowProps {
   step: FormStepperStep
-  isCurrent?: boolean
-  onClick?: () => void
+  isCurrent: boolean
   className?: string
-  isButton?: boolean
 }
 
-const StepperViewRow = ({ step, isCurrent, onClick, className, isButton }: StepperViewRowProps) => {
+const StepperViewRow = ({ step, isCurrent, className }: StepperViewRowProps) => {
   const { t } = useTranslation('forms')
 
-  const { title, isSummary, isSubmitted, displayIndex } = step
+  const { title, isSubmitted, displayIndex } = step
   const iconClassName = cx(
     'min-w-8 flex h-8 w-8 shrink-0 flex-row items-center justify-center rounded-full border-2',
     {
@@ -26,38 +24,19 @@ const StepperViewRow = ({ step, isCurrent, onClick, className, isButton }: Stepp
     },
   )
 
-  const buttonRef = useRef<HTMLDivElement>(null)
-  const { buttonProps } = useButton({ onPress: onClick, elementType: 'div' }, buttonRef)
-
   return (
-    <li
-      className={cx('flex select-none flex-col', className)}
-      aria-current={isCurrent ? 'step' : undefined}
-    >
-      <div
-        className="flex cursor-pointer flex-row items-center gap-3"
-        {...(isButton ? buttonProps : {})}
-        ref={buttonRef}
-      >
-        <div className={iconClassName}>
-          {isCurrent || !isSubmitted ? (
-            displayIndex
-          ) : (
-            <CheckIcon fill="white" className="h-6 w-6" />
-          )}
-        </div>
-        <p className="text-p3-medium w-72 ">
-          {isCurrent ? <span className="sr-only">{t('steps.current_sr')}</span> : null}
-          {isSubmitted && !isCurrent ? <span className="sr-only">{t('submitted_sr')}</span> : null}
-          {title}
-        </p>
+    <div className={twMerge('flex flex-row items-center gap-3', className)}>
+      <div className={iconClassName}>
+        {isCurrent || !isSubmitted ? displayIndex : <CheckIcon fill="white" className="h-6 w-6" />}
       </div>
-      {!isSummary && isButton && (
-        <div className="flex h-8 w-8 flex-row items-center justify-center">
-          <div className="h-4 w-0.5 bg-gray-300 py-2" />
-        </div>
-      )}
-    </li>
+      <p className="text-p3-medium w-72 ">
+        {isCurrent ? <span className="sr-only">{t('steps.current_sr')}</span> : null}
+        {isSubmitted && !isCurrent ? (
+          <span className="sr-only">{t('steps.submitted_sr')}</span>
+        ) : null}
+        {title}
+      </p>
+    </div>
   )
 }
 
--- next/components/forms/useFormState.tsx
+++ next/components/forms/useFormState.tsx
@@ -1,7 +1,7 @@
 import { GenericObjectType, RJSFSchema, UiSchema } from '@rjsf/utils'
-import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import React, { createContext, PropsWithChildren, useContext, useMemo, useState } from 'react'
+import { useIsFirstRender } from 'usehooks-ts'
 
 import { InitialFormData } from '../../frontend/types/initialFormData'
 import { getFileUuidsNaive } from '../../frontend/utils/form'
@@ -12,7 +12,7 @@ import {
   getStepProperty,
   parseStepFromFieldId,
   removeUnusedPropertiesFromFormData,
-  useStepIndex,
+  useCurrentStepIndex,
 } from '../../frontend/utils/formState'
 import { FormStepIndex } from './types/Steps'
 import { useFormFileUpload } from './useFormFileUpload'
@@ -26,20 +26,16 @@ interface FormStateProviderProps {
 }
 
 const useGetContext = ({ schema, uiSchema, formSlug, initialFormData }: FormStateProviderProps) => {
-  const { query } = useRouter()
   const { t } = useTranslation('forms')
   const { keepFiles, refetchAfterImportIfNeeded } = useFormFileUpload()
   const { turnOnLeaveProtection } = useFormLeaveProtection()
+  // eslint-disable-next-line testing-library/render-result-naming-convention
+  const isFirst = useIsFirstRender()
 
   const [formData, setFormData] = useState<GenericObjectType>(initialFormData.formDataJson)
   const stepsSchemas = useMemo(() => getEvaluatedStepsSchemas(schema, formData), [schema, formData])
 
-  const [currentStepIndex, setCurrentStepIndex] = useStepIndex(
-    initialFormData.formId,
-    formSlug,
-    query,
-    stepsSchemas,
-  )
+  const { currentStepIndex, setCurrentStepIndex } = useCurrentStepIndex(stepsSchemas)
 
   /**
    * This set holds indexes of steps that have been submitted (submit button has been pressed, which means they have been validated).
@@ -61,14 +57,6 @@ const useGetContext = ({ schema, uiSchema, formSlug, initialFormData }: FormStat
     if (stepsSchemas[newIndex] !== null || newIndex === 'summary') {
       setCurrentStepIndex(newIndex)
     }
-  }
-
-  const skipToStep = (newStepIndex: FormStepIndex) => {
-    if (currentStepIndex === newStepIndex) {
-      return
-    }
-
-    goToStep(newStepIndex)
   }
 
   const getPreviousStep = () => {
@@ -104,13 +92,6 @@ const useGetContext = ({ schema, uiSchema, formSlug, initialFormData }: FormStat
     }
   }
 
-  const skipToNextStep = () => {
-    const nextStepIndex = getNextStep()
-    if (nextStepIndex !== null) {
-      skipToStep(nextStepIndex)
-    }
-  }
-
   const setStepFormData = (stepFormData: GenericObjectType) => {
     // Form displays and returns only the data for the current step, so we need to merge it with the
     // existing data, as each step contains only one root property with the data this object spread
@@ -122,7 +103,10 @@ const useGetContext = ({ schema, uiSchema, formSlug, initialFormData }: FormStat
     keepFiles(fileUuids)
 
     setFormData(pickedPropertiesData)
-    turnOnLeaveProtection()
+    // Initially the form triggers onChange with the initial data, which is not a change, so we don't want to activate leave protection.
+    if (!isFirst) {
+      turnOnLeaveProtection()
+    }
   }
 
   const setImportedFormData = (importedFormData: GenericObjectType) => {
@@ -188,12 +172,10 @@ const useGetContext = ({ schema, uiSchema, formSlug, initialFormData }: FormStat
     stepperData,
     currentStepperStep,
     currentStepSchema,
-    skipToStep,
     canGoToPreviousStep,
     goToPreviousStep,
     canGoToNextStep,
     goToNextStep,
-    skipToNextStep,
     handleFormOnChange,
     handleFormOnSubmit,
     goToStepByFieldId,

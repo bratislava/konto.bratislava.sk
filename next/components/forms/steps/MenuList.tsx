406f98bb962574cf79c6a2388685c1b51d715047
Marek Bodinger <marek.bodinger@gmail.com>
2023-08-31 11:18:24 +0200

    Implement isReadonly in form

--- next/components/forms/steps/MenuList.tsx
+++ next/components/forms/steps/MenuList.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'next-i18next'
 import { ReactNode } from 'react'
 
 import { useFormExportImport } from '../../../frontend/hooks/useFormExportImport'
+import { useFormState } from '../useFormState'
 
 type MenuItem = {
   title: string
@@ -13,16 +14,17 @@ type MenuItem = {
 }
 
 const MenuList = () => {
+  const { isReadonly } = useFormState()
   const { exportXml, exportPdf, importXml, saveConcept } = useFormExportImport()
   const { t } = useTranslation('forms')
 
-  const menuList: MenuItem[] = [
-    {
+  const menuList = [
+    !isReadonly ? {
       title: t('menu_list.save_concept'),
       icon: <DiscIcon className="h-6 w-6" />,
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onPress: () => saveConcept(),
-    },
+    } : null,
     {
       title: t('menu_list.download_xml'),
       icon: <DownloadIcon className="h-6 w-6" />,
@@ -35,12 +37,12 @@ const MenuList = () => {
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onPress: () => exportPdf(),
     },
-    {
+    !isReadonly ? {
       title: t('menu_list.upload_xml'),
       icon: <ConnectionIcon className="h-6 w-6" />,
       onPress: importXml,
-    },
-  ]
+    } : null,
+  ].filter(Boolean) as MenuItem[]
 
   return (
     <ul className="mt-4 flex flex-col gap-3 border-t-2 border-gray-200 pt-4 lg:hidden">

65d2b0ce06e3e78da1a04d986325ae864f8a81c7
Marek Bodinger <marek.bodinger@gmail.com>
2023-10-05 12:33:48 +0200

    In radio button widget add support for number | boolean values, and fix undefined/null conversion issue

--- next/components/forms/widget-wrappers/RadioButtonWidgetRJSF.tsx
+++ next/components/forms/widget-wrappers/RadioButtonWidgetRJSF.tsx
@@ -6,12 +6,14 @@ import { RadioButtonUiOptions } from 'schema-generator/generator/uiOptionsTypes'
 import Radio from '../widget-components/RadioButton/Radio'
 import RadioGroup from '../widget-components/RadioButton/RadioGroup'
 
+type ValueType = string | number | boolean | undefined
+
 interface RadioButtonFieldWidgetRJSFProps extends WidgetProps {
   options: RadioButtonUiOptions & WidgetProps['options']
-  value: string | null
+  value: ValueType
   errorMessage?: string
   schema: StrictRJSFSchema
-  onChange: (value?: string | boolean | number | undefined) => void
+  onChange: (value?: ValueType) => void
 }
 
 const RadioButtonsWidgetRJSF = ({
@@ -25,30 +27,52 @@ const RadioButtonsWidgetRJSF = ({
 }: RadioButtonFieldWidgetRJSFProps) => {
   const { enumOptions, className, variant, radioOptions = [], orientations } = options
 
-  if (!enumOptions || Array.isArray(value)) return null
-  const getTooltip = (radioValue: string) => {
-    return radioOptions.find((option) => option.value === radioValue)?.tooltip
+  if (!enumOptions) return null
+
+  // RadioGroup doesn't support any other value than string, so we need to map the values to strings.
+  // It also doesn't support undefined, so we need to map it to null.
+  const valueIndex = value == null ? -1 : enumOptions.findIndex((option) => option.value === value)
+  const valueMapped = valueIndex === -1 ? null : `value-${valueIndex}`
+
+  // In the onChange handler we need to map the string value back to the original value.
+  const handleChange = (value: string | null) => {
+    if (value == null) {
+      onChange()
+      return
+    }
+
+    const match = value.match(/^value-(\d+)$/)
+
+    if (!match) {
+      return
+    }
+
+    const index = parseInt(match[1], 10)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    onChange(enumOptions[index].value)
   }
 
   return (
     <WidgetWrapper options={options}>
       <RadioGroup
         errorMessage={rawErrors}
-        value={value ?? undefined}
-        onChange={onChange}
+        value={valueMapped}
+        onChange={handleChange}
         className={className}
         label={label}
         orientation={orientations === 'row' ? 'horizontal' : 'vertical'}
         required={required}
         disabled={readonly}
       >
-        {enumOptions.map((radioElement: EnumOptionsType) => {
+        {enumOptions.map((radioElement: EnumOptionsType, radioIndex: number) => {
+          const radioValue = `value-${radioIndex}`
+
           return (
             <Radio
-              key={radioElement.value}
+              key={radioValue}
               variant={variant}
-              value={radioElement.value}
-              tooltip={getTooltip(radioElement.value as string)}
+              value={radioValue}
+              tooltip={radioOptions[radioIndex]?.tooltip}
             >
               {radioElement.label}
             </Radio>

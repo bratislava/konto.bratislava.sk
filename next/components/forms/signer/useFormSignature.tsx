c385c5f2d4858291f7f34317741e0feca7dcc608
Marek Bodinger <marek.bodinger@gmail.com>
2023-11-02 19:17:21 +0100

    Create useFormSignature

---
+++ next/components/forms/signer/useFormSignature.tsx
@@ -0,0 +1,81 @@
+import hash from 'object-hash'
+import React, { createContext, PropsWithChildren, useCallback, useContext, useState } from 'react'
+import { useIsMounted } from 'usehooks-ts'
+
+import { useFormState } from '../useFormState'
+import { signerExamplePayload } from './signerExamplePayload'
+import { useFormSigner } from './useFormSigner'
+
+type Signature = {
+  /**
+   * We store the hash of the object that was signed. This is the easiest way to ensure the validity of the signature
+   * for the current data.
+   */
+  objectHash: string
+  signature: string
+}
+
+const useGetContext = () => {
+  const { formData, formDataRef, isSigned } = useFormState()
+  const { sign: signerSign } = useFormSigner()
+  const [signature, setSignature] = useState<Signature | null>(null)
+  const isMounted = useIsMounted()
+
+  const sign = async () => {
+    try {
+      const jsonObjectHash = hash(formData)
+      // Just an example payload for the signer until we have a proper implementation.
+      const result = await signerSign(signerExamplePayload)
+      if (!isMounted()) {
+        return
+      }
+      const currentHash = hash(formDataRef.current)
+      // The easiest way how to ensure stability (people can leave the signer open / open it multiple times, etc. and
+      // then sign the form) is to compare the hash of the current form data with the hash of the data that was signed.
+      if (currentHash !== jsonObjectHash) {
+        // TODO handle error
+        return
+      }
+      setSignature({ objectHash: currentHash, signature: result })
+    } catch (error) {
+      // TODO handle error
+    }
+  }
+
+  const remove = () => {
+    setSignature(null)
+  }
+
+  // This is an expensive calculation, therefore it's exposed as useCallback (not useMemo), therefore it's called only
+  // when needed. useMemo would be recalculated on each render as this provider is global for the page.
+  const isValidSignature = useCallback(() => {
+    if (!isSigned) {
+      return true
+    }
+
+    if (!signature) {
+      return false
+    }
+
+    return signature.objectHash === hash(formData)
+  }, [isSigned, signature, formData])
+
+  return { signature, sign, remove, isValidSignature }
+}
+
+const FormSignatureContext = createContext<ReturnType<typeof useGetContext> | undefined>(undefined)
+
+export const FormSignatureProvider = ({ children }: PropsWithChildren) => {
+  const context = useGetContext()
+
+  return <FormSignatureContext.Provider value={context}>{children}</FormSignatureContext.Provider>
+}
+
+export const useFormSignature = () => {
+  const context = useContext(FormSignatureContext)
+  if (!context) {
+    throw new Error('useFormSignature must be used within a FormSignatureProvider')
+  }
+
+  return context
+}

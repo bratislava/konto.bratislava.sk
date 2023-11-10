9392c6fe1e589b18353aedf5402bd34a56c1bd22
sebastian-vass <sebastian9vass@gmail.com>
2023-11-10 21:48:45 +0100

    Add smoke test

--- next/cypress.config.ts
+++ next/cypress.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true, // Allows to run all tests in open mode
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.chromeWebSecurity = false
+      config.video = false
+      config.baseUrl = config.env.BASEURL
+
+      config.retries = {
+        runMode: 1,
+        openMode: 0,
+      }
+
+      return config
     },
   },
-});
+})

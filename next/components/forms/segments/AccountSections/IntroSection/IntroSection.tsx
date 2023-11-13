93a1a1f3e31e459cae158632baf147a426d79ebd
Adam Grund <adam.grund@gmail.com>
2023-11-13 15:22:10 +0100

    [bratislava/private-konto.bratislava.sk#30] add new ServiceCards to IntroSection

--- next/components/forms/segments/AccountSections/IntroSection/IntroSection.tsx
+++ next/components/forms/segments/AccountSections/IntroSection/IntroSection.tsx
@@ -12,8 +12,7 @@ import { useTranslation } from 'next-i18next'
 import { useEffect, useState } from 'react'
 
 import TaxesIcon from '../../../../../assets/icons/city-bratislava/taxes.svg'
-import LibraryIcon from '../../../../../assets/icons/culture-communities/library.svg'
-import SwimmingPoolIcon from '../../../../../assets/icons/education-sport/swimming-pool.svg'
+import SpatialPlanningIcon from '../../../../../assets/icons/environment-construction/spatial-planning.svg'
 import ParkingIcon from '../../../../../assets/icons/transport-and-maps/parking.svg'
 import { ROUTES } from '../../../../../frontend/api/constants'
 import { AccountType } from '../../../../../frontend/dtos/accountDto'
@@ -113,12 +112,28 @@ const IntroSection = () => {
             </div>
             <div className="flex gap-3 overflow-x-scroll px-4 scrollbar-hide lg:gap-8 lg:px-0">
               <ServiceCard
-                title={t('account_section_services.cards.32.title')}
-                description={t('account_section_services.cards.32.description')}
-                icon={<SwimmingPoolIcon className="h-10 w-10 text-education-700 lg:h-12 lg:w-12" />}
-                buttonText={t('account_section_services.cards.32.buttonText')}
-                href="https://kupaliska.bratislava.sk"
-                plausibleProps={{ id: `Domov: ${t('account_section_services.cards.32.title')}` }}
+                title={t('account_section_services.cards.34.title')}
+                description={t('account_section_services.cards.34.description')}
+                icon={
+                  <SpatialPlanningIcon className="h-10 w-10 text-environment-700 lg:h-12 lg:w-12" />
+                }
+                buttonText={t('account_section_services.cards.34.buttonText')}
+                href={ROUTES.MUNICIPAL_SERVICES_INVESTING}
+                plausibleProps={{ id: `Domov: ${t('account_section_services.cards.34.title')}` }}
+                tag={t('account_section_services.cards.34.tag')}
+                tagStyle="text-environment-700 bg-environment-100"
+              />
+              <ServiceCard
+                title={t('account_section_services.cards.35.title')}
+                description={t('account_section_services.cards.35.description')}
+                icon={
+                  <SpatialPlanningIcon className="h-10 w-10 text-environment-700 lg:h-12 lg:w-12" />
+                }
+                buttonText={t('account_section_services.cards.35.buttonText')}
+                href={ROUTES.MUNICIPAL_SERVICES_INVESTING_INTENT}
+                plausibleProps={{ id: `Domov: ${t('account_section_services.cards.35.title')}` }}
+                tag={t('account_section_services.cards.35.tag')}
+                tagStyle="text-environment-700 bg-environment-100"
               />
               <ServiceCard
                 title={t('account_section_services.cards.1.title')}
@@ -135,14 +150,6 @@ const IntroSection = () => {
                 buttonText={t('account_section_services.cards.4.buttonText')}
                 href="https://paas.sk/"
                 plausibleProps={{ id: `Domov: ${t('account_section_services.cards.4.title')}` }}
-              />
-              <ServiceCard
-                title={t('account_section_services.cards.5.title')}
-                description={t('account_section_services.cards.5.description')}
-                icon={<LibraryIcon className="h-10 w-10 text-culture-700 lg:h-12 lg:w-12" />}
-                buttonText={t('account_section_services.cards.5.buttonText')}
-                href="https://mestskakniznica.sk/sluzby/citanie/ako-sa-prihlasit-do-kniznice"
-                plausibleProps={{ id: `Domov: ${t('account_section_services.cards.5.title')}` }}
               />
             </div>
             <Button

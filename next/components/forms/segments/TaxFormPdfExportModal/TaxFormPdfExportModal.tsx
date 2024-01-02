9b374e08667ac8f5e6a5915db11176c8afa8e44e
Marek Bodinger <marek.bodinger@gmail.com>
2024-01-02 17:21:07 +0100

    Fix eslint

--- next/components/forms/segments/TaxFormPdfExportModal/TaxFormPdfExportModal.tsx
+++ next/components/forms/segments/TaxFormPdfExportModal/TaxFormPdfExportModal.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 import ModalV2, { ModalV2Props } from '../../simple-components/ModalV2'
@@ -6,8 +5,6 @@ import ModalV2, { ModalV2Props } from '../../simple-components/ModalV2'
 type TaxFormPdfExportModalBase = ModalV2Props
 
 const TaxFormPdfExportModal = (props: TaxFormPdfExportModalBase) => {
-  const { t } = useTranslation('forms')
-
   return (
     <ModalV2 modalClassname="md:max-w-[796px] md:pt-8" mobileFullScreen {...props}>
       TODO

8709bf55a547613469e86fd765aaa80addab4dbe
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-01-05 17:23:20 +0100

    Dane improvements v30 (#734)
    * Fix stretched for unstyled button
    
    * Temp tax form landing page
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

--- next/components/forms/TaxFormLandingPage.tsx
+++ next/components/forms/TaxFormLandingPage.tsx
@@ -9,8 +9,10 @@ import useSnackbar from '../../frontend/hooks/useSnackbar'
 import AccountPageLayout from '../layouts/AccountPageLayout'
 import PageWrapper from '../layouts/PageWrapper'
 import { GetSSRCurrentAuth } from '../logic/ServerSideAuthProvider'
+import TaxFormLandingPageCard, {
+  TaxFormLandingPageCardProps,
+} from './info-components/TaxFormLandingPageCard'
 import AccountMarkdown from './segments/AccountMarkdown/AccountMarkdown'
-import ButtonNew from './simple-components/ButtonNew'
 import Waves from './simple-components/Waves/Waves'
 
 export type TaxFormLandingPageProps = {
@@ -27,6 +29,7 @@ const TaxFormLandingPage = ({ page, latestVersionId }: TaxFormLandingPageProps) 
   const router = useRouter()
   const { t } = useTranslation('forms')
   const [openSnackbarError] = useSnackbar({ variant: 'error' })
+  const [openSnackbarInfo, closeSnackbarInfo] = useSnackbar({ variant: 'info' })
 
   const { mutate, isPending } = useMutation({
     mutationFn: () =>
@@ -37,15 +40,50 @@ const TaxFormLandingPage = ({ page, latestVersionId }: TaxFormLandingPageProps) 
         { accessToken: 'onlyAuthenticated' },
       ),
     networkMode: 'always',
+    onMutate: () => {
+      openSnackbarInfo(t('tax_form_landing_page.redirect_info'))
+    },
     onSuccess: async (response) => {
+      closeSnackbarInfo()
       await router.push(
         `${ROUTES.MUNICIPAL_SERVICES}/priznanie-k-dani-z-nehnutelnosti/${response.data.id}`,
       )
     },
     onError: () => {
+      closeSnackbarInfo()
       openSnackbarError(t('tax_form_landing_page.redirect_error'))
     },
   })
+
+  const cards: TaxFormLandingPageCardProps[] = [
+    {
+      title: 'Vyplniť cez Bratislavské konto',
+      isEid: false,
+      description:
+        'Vyplňte daňové priznanie jednoducho, s návodom a pomocnými kalkulačkami, v Bratislavskom konte. PDF si následne stiahnete a odošlete poštou.',
+      // icon: BinIcon,
+      onPress: () => {
+        mutate()
+      },
+      disabled: isPending,
+    },
+    {
+      title: 'Vyplniť a podať cez esluzby.bratislava.sk',
+      isEid: true,
+      description:
+        'Vyplňte daňové priznanie cez esluzby.bratislava.sk. Na jeho odoslanie potrebujete mať aktívny elektronický občiansky preukaz (eID).',
+      // icon: BinIcon,
+      href: 'https://esluzby.bratislava.sk/info/203?slug=podavanie-danoveho-priznania-k-dani-z-nehnutelnosti',
+    },
+    {
+      title: 'Stiahnuť PDF priznanie',
+      isEid: false,
+      description:
+        'Ak si prajete vyplniť papierové priznanie ručne, stiahnite si PDF. Vyplnené papierové tlačivo je potrebné odoslať poštou.',
+      // icon: BinIcon,
+      href: 'https://cdn-api.bratislava.sk/strapi-homepage/upload/Priznanie_k_dani_z_nehnutelnosti_1bda8bd949.pdf',
+    },
+  ]
 
   return (
     <PageWrapper locale={page.locale}>
@@ -64,17 +102,14 @@ const TaxFormLandingPage = ({ page, latestVersionId }: TaxFormLandingPageProps) 
             wavePosition="bottom"
           />
         </div>
-        <div className="mx-auto flex w-full max-w-screen-lg flex-col gap-10 pb-6 pt-0 lg:flex-row lg:gap-20 lg:py-10">
+        <div className="mx-auto flex w-full max-w-screen-lg flex-col gap-10 p-4 pb-6 lg:flex-row lg:gap-20 lg:p-0 lg:py-10">
           <div className="flex max-w-[800px] flex-col gap-10">
-            <AccountMarkdown content={t('tax_form_landing_page.instructions')} variant="normal" />
-            <ButtonNew
-              variant="black-outline"
-              className="self-end"
-              onPress={() => mutate()}
-              isLoading={isPending}
-            >
-              {t('tax_form_landing_page.redirect_button')}
-            </ButtonNew>
+            <AccountMarkdown content={t('tax_form_landing_page.instructions')} variant="sm" />
+            <div className="flex flex-col rounded-xl border-2 border-gray-200">
+              {cards.map((card, index) => (
+                <TaxFormLandingPageCard key={index} {...card} />
+              ))}
+            </div>
           </div>
         </div>
       </AccountPageLayout>

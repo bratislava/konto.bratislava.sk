6ee6bdf566c3d0bd12adb2126f6da48721b660fa
Marek Bodinger <marek.bodinger@gmail.com>
2024-01-16 15:50:58 +0100

    Agreement text

--- next/components/forms/steps/Summary/SummaryFormLegalText.tsx
+++ next/components/forms/steps/Summary/SummaryFormLegalText.tsx
@@ -2,14 +2,19 @@ import AccountMarkdown from 'components/forms/segments/AccountMarkdown/AccountMa
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 
+import { useFormState } from '../../useFormState'
+
 const SummaryFormLegalText = () => {
+  const { isTaxForm } = useFormState()
   const { t } = useTranslation('forms')
 
   return (
     <div>
       <h3 className="text-h3 mb-4">{t('summary.vop_agreement_title')}</h3>
       <AccountMarkdown
-        content={t('summary.vop_agreement_content')}
+        content={
+          isTaxForm ? t('summary.vop_agreement_content_tax') : t('summary.vop_agreement_content')
+        }
         className="rounded-10 bg-gray-50 p-8"
       />
     </div>

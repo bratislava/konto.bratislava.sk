94c061f4fb7fc7565a423b82c16d597f20263275
Adam Grund <adam.grund@gmail.com>
2024-02-13 10:49:34 +0100

    [#780] add possibility to close status bar

--- next/components/forms/info-components/StatusBar.tsx
+++ next/components/forms/info-components/StatusBar.tsx
@@ -1,6 +1,7 @@
 /** A react component with state provided through context
  * which shows a red status bar with white text on top of the page
  */
+import { CrossIcon } from '@assets/ui-icons'
 import cx from 'classnames'
 import { useTranslation } from 'next-i18next'
 import React, { createContext, forwardRef, useContext, useState } from 'react'
@@ -9,6 +10,7 @@ import { useEffectOnce } from 'usehooks-ts'
 import WarningIcon from '../icon-components/WarningIcon'
 import AccountMarkdown from '../segments/AccountMarkdown/AccountMarkdown'
 import { SectionContainer } from '../segments/SectionContainer/SectionContainer'
+import Button from '../simple-components/ButtonNew'
 
 type StatusBarVariantBase = 'warning' | 'error' | 'info'
 
@@ -64,8 +66,11 @@ export const StatusBarProvider: React.FC<StatusBarProviderProps> = ({ children }
 export const useStatusBarContext = () => useContext(StatusBarContext)
 
 export const StatusBar = forwardRef<HTMLDivElement>((props, forwardedRef) => {
+  const { t } = useTranslation('common')
+  const [showStatusBar, setShowStatusBar] = useState(true)
+
   const { statusBarConfiguration } = useStatusBarContext()
-  return statusBarConfiguration.content ? (
+  return showStatusBar && statusBarConfiguration.content ? (
     <div
       ref={forwardedRef}
       className={cx('w-full text-white', {
@@ -75,9 +80,9 @@ export const StatusBar = forwardRef<HTMLDivElement>((props, forwardedRef) => {
       })}
       data-cy="info-bar"
     >
-      <div className="container mx-auto flex h-full items-center justify-center">
-        <SectionContainer>
-          <div className="flex items-center py-4">
+      <SectionContainer>
+        <div className="flex justify-between py-4">
+          <div className="flex">
             <span className="mr-3">
               <WarningIcon solid className="h-5 w-5" />
             </span>
@@ -86,8 +91,14 @@ export const StatusBar = forwardRef<HTMLDivElement>((props, forwardedRef) => {
               content={statusBarConfiguration.content as string}
             />
           </div>
-        </SectionContainer>
-      </div>
+          <Button
+            className="h-fit shrink-0"
+            icon={<CrossIcon />}
+            aria-label={t('ariaCloseStatusBar') ?? ''}
+            onPress={() => setShowStatusBar(false)}
+          />
+        </div>
+      </SectionContainer>
     </div>
   ) : null
 })

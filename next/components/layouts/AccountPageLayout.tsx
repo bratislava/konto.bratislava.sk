58682762f2cca91ea57bdbc05d504bd27be9bcaf
Adam Grund <adam.grund@gmail.com>
2024-02-26 14:18:37 +0100

    [#811] unauthenticated user menu (#812)
    * [#811] add mobile menu for unauthenticated user
    
    * refactoring - removed unused files, comments
    
    * [#811] types fix

--- next/components/layouts/AccountPageLayout.tsx
+++ next/components/layouts/AccountPageLayout.tsx
@@ -12,6 +12,8 @@ import { Auth } from 'aws-amplify'
 import cx from 'classnames'
 import NavBar, { MenuSectionItemBase } from 'components/forms/segments/NavBar/NavBar'
 import { MenuItemBase } from 'components/forms/simple-components/MenuDropdown/MenuDropdown'
+import { useConditionalFormRedirects } from 'components/forms/useFormRedirects'
+import { useServerSideAuth } from 'frontend/hooks/useServerSideAuth'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { ReactNode, useRef, useState } from 'react'
@@ -32,6 +34,8 @@ declare module 'react' {
 }
 
 const AccountPageLayout = ({ className, children, hiddenHeaderNav }: AccountPageLayoutBase) => {
+  const { isAuthenticated } = useServerSideAuth()
+
   const router = useRouter()
   const headerRef = useRef<HTMLDivElement>(null)
   const mainRef = useRef<HTMLDivElement>(null)
@@ -51,6 +55,20 @@ const AccountPageLayout = ({ className, children, hiddenHeaderNav }: AccountPage
     await Auth.signOut()
     await router.push(ROUTES.LOGIN)
   }
+
+  // we need to keep the work in progress of the open form if navigating away form it
+  const optionalFormRedirectsContext = useConditionalFormRedirects()
+  const login = optionalFormRedirectsContext
+    ? () => optionalFormRedirectsContext.login()
+    : async () => {
+        await router.push(ROUTES.LOGIN)
+      }
+
+  const register = optionalFormRedirectsContext
+    ? () => optionalFormRedirectsContext.register()
+    : async () => {
+        await router.push(ROUTES.REGISTER)
+      }
 
   const sectionsList: MenuSectionItemBase[] = [
     {
@@ -85,28 +103,43 @@ const AccountPageLayout = ({ className, children, hiddenHeaderNav }: AccountPage
     },
   ].filter(isDefined)
 
-  const menuItems: MenuItemBase[] = [
-    {
-      id: 1,
-      title: t('account:menu_profile_link'),
-      icon: <ProfileIcon className="h-5 w-5" />,
-      url: ROUTES.USER_PROFILE,
-    },
-    // TODO this is duplicated today in mobile menu, we need a flag or two different lists for mobile & desktop
-    {
-      id: 2,
-      title: t('account:menu_help_link'),
-      icon: <HelpIcon className="h-5 w-5" />,
-      url: ROUTES.HELP,
-    },
-    {
-      id: 3,
-      title: t('account:menu_logout_link'),
-      icon: <LogoutIcon className="h-5 w-5 text-negative-700" />,
-      onPress: logoutHandler,
-      itemClassName: 'bg-negative-50',
-    },
-  ]
+  // TODO consider using this in desktop menu
+  const menuItems: MenuItemBase[] = isAuthenticated
+    ? [
+        {
+          id: 0,
+          title: t('account:menu_profile_link'),
+          icon: <ProfileIcon className="h-5 w-5" />,
+          url: ROUTES.USER_PROFILE,
+        },
+        {
+          id: 1,
+          title: t('account:menu_help_link'),
+          icon: <HelpIcon className="h-5 w-5" />,
+          url: ROUTES.HELP,
+        },
+        {
+          id: 2,
+          title: t('account:menu_logout_link'),
+          icon: <LogoutIcon className="h-5 w-5 text-negative-700" />,
+          onPress: logoutHandler,
+          itemClassName: 'bg-negative-50',
+        },
+      ]
+    : [
+        {
+          id: 0,
+          title: t('account:menu_login_link'),
+          icon: <ProfileIcon className="h-5 w-5" />,
+          onPress: login,
+        },
+        {
+          id: 1,
+          title: t('account:menu_register_link'),
+          icon: <ProfileIcon className="h-5 w-5" />,
+          onPress: register,
+        },
+      ]
 
   return (
     <div className={cx('flex min-h-screen flex-col', className)}>

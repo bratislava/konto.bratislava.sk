c610a05e2133ae838da5c9b740afed51e678029a
Marek Bodinger <marek.bodinger@gmail.com>
2024-02-26 15:08:09 +0100

    Rework and rename SsrAuthContext for consistency

---
+++ next/components/logic/SsrAuthContext.tsx
@@ -0,0 +1,32 @@
+import { UserData } from 'frontend/dtos/accountDto'
+import React, { ComponentType, createContext } from 'react'
+
+export const ssrAuthContextPropKey = '__ssrAuthContext'
+
+export interface SsrAuthContextType {
+  isSignedIn: boolean
+  userData: UserData | null
+}
+
+export const SsrAuthContext = createContext<SsrAuthContextType | null>(null)
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const SsrAuthProviderHOC = <Props extends {}>(Wrapped: ComponentType<Props>) => {
+  // eslint-disable-next-line func-names
+  return function (props: Props & { [ssrAuthContextPropKey]?: SsrAuthContextType }) {
+    // eslint-disable-next-line react/destructuring-assignment
+    const ssrAuthContext = props[ssrAuthContextPropKey]
+
+    if (!ssrAuthContext) {
+      throw new Error(`SsrAuthProviderHOC: '${ssrAuthContextPropKey}' is missing in page props. To fix the issue:
+1. Use 'amplifyGetServerSideProps' to get server side auth data.
+2. Remove 'skipSsrAuthContext: true' from amplifyGetServerSideProps options.`)
+    }
+
+    return (
+      <SsrAuthContext.Provider value={ssrAuthContext}>
+        <Wrapped {...props} />
+      </SsrAuthContext.Provider>
+    )
+  }
+}

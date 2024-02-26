e15a19403a3dff3406e75a1457e89a4c210e9604
Marek Bodinger <marek.bodinger@gmail.com>
2024-02-26 15:09:59 +0100

    Create new Amplify utils/config

---
+++ next/frontend/utils/amplifyClient.ts
@@ -0,0 +1,62 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { AuthError } from '@aws-amplify/auth/src'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { fetchAuthSession } from '@aws-amplify/core'
+import { getCurrentUser } from 'aws-amplify/auth'
+import { PropsWithChildren, useRef } from 'react'
+
+import { ROUTES } from '../api/constants'
+import { amplifyConfig } from './amplifyConfig'
+import logger from './logger'
+
+/**
+ * Based on: https://docs.amplify.aws/nextjs/build-a-backend/server-side-rendering/nextjs/#configure-amplify-library-for-client-side-usage
+ */
+export function AmplifyProvider({ children }: PropsWithChildren) {
+  useRef(amplifyConfig) // This is a hack to make sure amplifyConfig is used, so that it's not tree-shaken away.
+
+  return children
+}
+
+const fetchAccessTokenString = async () => {
+  const session = await fetchAuthSession()
+  return session.tokens?.accessToken?.toString()
+}
+
+export const getAccessToken = async () => {
+  try {
+    return await fetchAccessTokenString()
+  } catch (error) {
+    if (error instanceof AuthError && error.name === 'NotAuthorizedException') {
+      return null
+    }
+    throw error
+  }
+}
+
+export const getAccessTokenOrLogout = async () => {
+  try {
+    const accessToken = await getAccessToken()
+    if (!accessToken) {
+      throw new Error('No access token found.')
+    }
+    return accessToken
+  } catch (error) {
+    logger.error('Error getting access token - redirect to login.', error)
+    window.location.assign(ROUTES.LOGIN)
+    throw error
+  }
+}
+
+// TODO remove
+// Auth.getCurrentAuthenticatedUser throws when not authenticated
+// this helper changes that and ignores any other potential errors
+export const getCurrentAuthenticatedUser = async () => {
+  try {
+    return await getCurrentUser()
+    // TODO should be solved in v6 release of aws-amplify
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  } catch (error) {
+    return null
+  }
+}
---
+++ next/frontend/utils/amplifyServer.ts
@@ -0,0 +1,123 @@
+import { ParsedUrlQuery } from 'node:querystring'
+
+/* eslint-disable import/no-extraneous-dependencies */
+import { createServerRunner } from '@aws-amplify/adapter-nextjs'
+import { AuthError } from '@aws-amplify/auth'
+import { fetchUserAttributes, getCurrentUser } from '@aws-amplify/auth/server'
+import { AmplifyServer } from '@aws-amplify/core/dist/esm/adapterCore'
+import { fetchAuthSession } from '@aws-amplify/core/server'
+/* eslint-enable import/no-extraneous-dependencies */
+import { GetServerSideProps } from 'next'
+import { GetServerSidePropsContext, GetServerSidePropsResult, PreviewData } from 'next/types'
+
+import { ssrAuthContextPropKey, SsrAuthContextType } from '../../components/logic/SsrAuthContext'
+import { ROUTES } from '../api/constants'
+import { amplifyConfig } from './amplifyConfig'
+
+export const { runWithAmplifyServerContext } = createServerRunner({
+  config: amplifyConfig,
+})
+
+const getIsSignedIn = async (context: AmplifyServer.ContextSpec) => {
+  try {
+    const { userId } = await getCurrentUser(context)
+    return Boolean(userId)
+  } catch (error) {
+    if (error instanceof AuthError && error.name === 'UserUnAuthenticatedException') {
+      return false
+    }
+    throw error
+  }
+}
+
+const getAccessToken = async (amplifyContextSpec: AmplifyServer.ContextSpec) => {
+  const authSession = await fetchAuthSession(amplifyContextSpec)
+  return authSession.tokens?.accessToken.toString() ?? null
+}
+
+/**
+ * In Amplify V6, `getServerSideProps` must run in Amplify server context to execute Amplify operations. This is a
+ * wrapper for getServerSideProps that runs in Amplify server context and handles common SSR auth logic:
+ *
+ * - injects SSR auth context (is user signed in, user attributes) into props
+ * - redirects to login page if requiresSignIn is set to true and user is not signed in
+ * - redirects to home page if requiresSignOut is set to true and user is signed in
+ *
+ * Also, for some reason `fetchUserAttributes` takes very long (~450ms) to fetch (note - it takes only ~100ms in the
+ * browser), in the past, this blocked the rendering of the page, so in this solution it is fetched in parallel with
+ * `getServerSidePropsFn` and then injected into the props. It is what makes this function complicated, but it is worth
+ * the effort. In the future, we should examine why it takes so long and then we will be able to simplify this function.
+ */
+export const amplifyGetServerSideProps = <
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Props extends { [key: string]: any } = { [key: string]: any },
+  Params extends ParsedUrlQuery = ParsedUrlQuery,
+  Preview extends PreviewData = PreviewData,
+>(
+  getServerSidePropsFn: (args: {
+    context: GetServerSidePropsContext<Params, Preview>
+    amplifyContextSpec: AmplifyServer.ContextSpec
+    getAccessToken: () => Promise<string | null>
+    isSignedIn: boolean
+  }) => Promise<GetServerSidePropsResult<Props>>,
+  options?: {
+    requiresSignIn?: boolean
+    requiresSignOut?: boolean
+    skipSsrAuthContext?: boolean
+  },
+) => {
+  const wrappedFn: GetServerSideProps<Props, Params, Preview> = (context) =>
+    runWithAmplifyServerContext({
+      nextServerContext: { request: context.req, response: context.res },
+      operation: async (contextSpec) => {
+        const isSignedIn = await getIsSignedIn(contextSpec)
+
+        if (options?.requiresSignIn && !isSignedIn) {
+          return {
+            redirect: {
+              destination: `${ROUTES.LOGIN}?from=${context.resolvedUrl}`,
+              permanent: false,
+            },
+          }
+        }
+
+        if (options?.requiresSignOut && isSignedIn) {
+          return {
+            redirect: {
+              destination: ROUTES.HOME,
+              permanent: false,
+            },
+          }
+        }
+
+        const shouldFetchUserAttributes = isSignedIn && !options?.skipSsrAuthContext
+        const [userAttributes, getServerSidePropsResult] = await Promise.all([
+          shouldFetchUserAttributes ? fetchUserAttributes(contextSpec) : Promise.resolve(null),
+          getServerSidePropsFn({
+            context,
+            amplifyContextSpec: contextSpec,
+            getAccessToken: isSignedIn
+              ? () => getAccessToken(contextSpec)
+              : () => Promise.resolve(null),
+            isSignedIn,
+          }),
+        ])
+
+        if ('props' in getServerSidePropsResult && !options?.skipSsrAuthContext) {
+          return {
+            props: {
+              // props could be a promise, so we need to await it, if it is not a promise, it will be resolved immediately
+              ...(await getServerSidePropsResult.props),
+              [ssrAuthContextPropKey]: { isSignedIn, userData: userAttributes },
+            },
+          } satisfies GetServerSidePropsResult<
+            Props & { [ssrAuthContextPropKey]: SsrAuthContextType }
+          >
+        }
+
+        return getServerSidePropsResult
+      },
+    })
+
+  return wrappedFn
+}

1a403ca74518ea79f19958d59f3a2192d5793571
Marek Bodinger <marek.bodinger@gmail.com>
2024-02-27 12:55:02 +0100

    In new Amplify the errors have "name" instead of "code", change the logic

--- next/frontend/utils/errors.ts
+++ next/frontend/utils/errors.ts
@@ -1,12 +1,9 @@
-// as a class so that we can instantiate when we need to throw something with code
-// we treat it more as an interface when we use isErrorWithCode
-// that is, the actual object can be instance of something else but we care only about the code and the message
-export class ErrorWithCode extends Error {
-  code: string
+export class ErrorWithName extends Error {
+  name: string
 
-  constructor(message: string, code: string) {
+  constructor(message: string, name: string) {
     super(message)
-    this.code = code
+    this.name = name
   }
 }
 
@@ -16,9 +13,8 @@ export const isError = (error: unknown): error is Error =>
   'message' in error &&
   typeof error.message === 'string'
 
-// we don't need instance of ErrorWithCode - we just need to access the code property
-// i.e. many of errors returned by amplify-js return code on their errors which we match with translation keys
-export const isErrorWithCode = (error: unknown): error is ErrorWithCode =>
-  isError(error) && 'code' in error && typeof error.code === 'string'
+// Amplify errors have a name property that is matched to translation keys
+export const isErrorWithoutName = (error: Error) =>
+  isError(error) && !('name' in error && typeof error.name === 'string')
 
 export const GENERIC_ERROR_MESSAGE = 'UNEXPECTED_ERROR'

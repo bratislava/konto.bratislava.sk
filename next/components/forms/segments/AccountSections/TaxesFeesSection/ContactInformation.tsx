e30285c532989a410145872d3982e3bf9d559f89
Marek Bodinger <marek.bodinger@gmail.com>
2024-03-26 15:29:39 +0100

    Create CorrespondenceAddressModalV2

--- next/components/forms/segments/AccountSections/TaxesFeesSection/ContactInformation.tsx
+++ next/components/forms/segments/AccountSections/TaxesFeesSection/ContactInformation.tsx
@@ -1,18 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ResponseTaxDto } from '@clients/openapi-tax'
-import { updateUserAttributes } from 'aws-amplify/auth'
-import { Address } from 'frontend/dtos/accountDto'
-import useJsonParseMemo from 'frontend/hooks/useJsonParseMemo'
-import { GENERIC_ERROR_MESSAGE, isError } from 'frontend/utils/errors'
-import logger from 'frontend/utils/logger'
 import { useTranslation } from 'next-i18next'
 import { useState } from 'react'
 
-import useSnackbar from '../../../../../frontend/hooks/useSnackbar'
+import { Address } from '../../../../../frontend/dtos/accountDto'
 import { useSsrAuth } from '../../../../../frontend/hooks/useSsrAuth'
 import SummaryRowSimple from '../../../simple-components/SummaryRowSimple'
 import SummaryRow from '../../../steps/Summary/SummaryRow'
-import CorrespondenceAddressModal from '../../CorrespondenceAddressModal/CorrespondenceAddressModal'
+import CorrespondenceAddressModalV2 from '../../CorrespondenceAddressModal/CorrespondenceAddressModalV2'
 
 interface ContactInformationSectionProps {
   tax: ResponseTaxDto
@@ -24,52 +19,26 @@ const postalCodeFormat = (code?: string): string =>
 const ContactInformationSection = ({ tax }: ContactInformationSectionProps) => {
   const { t } = useTranslation('account')
   const { userAttributes } = useSsrAuth()
-  const [showSnackbar] = useSnackbar({ variant: 'success' })
-  const address = userAttributes?.address
-  const parsedAddress = useJsonParseMemo<Address>(address)
+  const [parsedAddress, setParsedAddress] = useState(() => {
+    try {
+      return JSON.parse(userAttributes?.address ?? '{}') as Address
+    } catch {
+      return {} as Address
+    }
+  })
   const postal_code_array = parsedAddress?.postal_code?.replace(/\s/g, '')
   const [correspondenceAddressModalShow, setCorrespondenceAddressModalShow] = useState(false)
 
-  const [correspondenceAddressError, setCorrespondenceAddressError] = useState<Error | null>(null)
-  const resetError = () => {
-    setCorrespondenceAddressError(null)
-  }
-
-  const onSubmitCorrespondenceAddress = async ({ data }: { data?: string }) => {
-    try {
-      const {
-        address: { isUpdated },
-      } = await updateUserAttributes({
-        userAttributes: { address: data },
-      })
-      if (isUpdated) {
-        setCorrespondenceAddressModalShow(false)
-        showSnackbar(t('profile_detail.success_alert'))
-      } else {
-        throw new Error('Unknown error')
-      }
-    } catch (error) {
-      if (isError(error)) {
-        setCorrespondenceAddressError(error)
-      } else {
-        logger.error(
-          `${GENERIC_ERROR_MESSAGE} - unexpected object thrown in onSubmitCorrespondenceAddress:`,
-          error,
-        )
-        setCorrespondenceAddressError(new Error('Unknown error'))
-      }
-    }
-  }
-
   return (
     <>
-      <CorrespondenceAddressModal
-        show={correspondenceAddressModalShow}
-        onClose={() => setCorrespondenceAddressModalShow(false)}
-        onSubmit={onSubmitCorrespondenceAddress}
-        defaultValues={parsedAddress || undefined}
-        error={correspondenceAddressError}
-        onHideError={resetError}
+      <CorrespondenceAddressModalV2
+        parsedAddress={parsedAddress}
+        isOpen={correspondenceAddressModalShow}
+        onOpenChange={setCorrespondenceAddressModalShow}
+        onSuccess={(newAddress) => {
+          setParsedAddress(newAddress)
+          setCorrespondenceAddressModalShow(false)
+        }}
       />
       <div className="flex w-full flex-col items-start gap-6 px-4 sm:gap-8 lg:px-0">
         <div className="flex w-full flex-col items-start gap-2">

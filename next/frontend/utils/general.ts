82f3b33e1604633d51492f5af1b79e960542fc4f
Marek Bodinger <marek.bodinger@gmail.com>
2024-03-28 11:33:47 +0100

    Remove unused utils

--- next/frontend/utils/general.ts
+++ next/frontend/utils/general.ts
@@ -5,61 +5,11 @@ import React from 'react'
 
 import { environment } from '../../environment'
 
-export interface DocumentsWrapper {
-  mainDocument?: {
-    url: string
-  }
-  amedmentDocument?: []
-  cancellationDocument?: []
-  consolidatedText?: unknown
-}
-
-export const arrayify = (input: string | string[] | undefined | null) => {
-  if (input === undefined || input === null) {
-    return [] as undefined[]
-  }
-  if (typeof input === 'string') return [input]
-  return input
-}
-
-// turn unknown values into (empty) strings, return string if it's a string
-export const forceString = (input: unknown) => {
-  if (typeof input === 'string') return input
-  if (typeof input === 'number') return input.toString()
-  if (Array.isArray(input)) return input.join(', ')
-  return ''
-}
-
-export const fileCountVzns = (data: DocumentsWrapper) => {
-  let count = 0
-  if (data?.mainDocument?.url) {
-    count += 1
-  }
-  if (data?.amedmentDocument) {
-    count += data.amedmentDocument.length
-  }
-  if (data?.cancellationDocument) {
-    count += data.cancellationDocument.length
-  }
-  if (data?.consolidatedText) {
-    count += 1
-  }
-  return count
-}
-
-export const isPresent = <U>(a: U | null | undefined | void): a is U => {
-  return !(a === null || a === undefined)
-}
-
 const isServer = () => typeof window === 'undefined'
 
 export const isBrowser = () => !isServer()
 
 export const isProductionDeployment = () => !environment.isStaging
-
-// https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript
-export const isObject = (value: any) =>
-  typeof value === 'object' && value !== null && !Array.isArray(value)
 
 export const getLanguageKey = (currentLanguage?: string) => {
   return currentLanguage === 'sk' ? 'sk' : 'en'

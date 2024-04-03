78ce8f9ff9670d92c7175bedd011a6e891cb4833
Marek Bodinger <marek.bodinger@gmail.com>
2024-04-03 12:39:18 +0200

    Fix error handling in [year].tsx

--- next/pages/dane-a-poplatky/[year].tsx
+++ next/pages/dane-a-poplatky/[year].tsx
@@ -46,8 +46,17 @@ export const getServerSideProps = amplifyGetServerSideProps<AccountTaxesFeesPage
       }
     } catch (error) {
       // TAXYEAR_OR_USER_NOT_FOUND
-      if (isAxiosError(error) && error.response?.status === 422) {
-        return { notFound: true }
+      if (isAxiosError(error)) {
+        const is422Error = error.response?.status === 422
+        // The user is not verified, the BE returns 403, but it means that the tax doesn't exist
+        const isForbiddenTierError =
+          error.response?.status === 403 &&
+          // TODO: This should be replaced with a proper error code (which is not returned)
+          error.response?.data?.message === 'Forbidden tier'
+
+        if (is422Error || isForbiddenTierError) {
+          return { notFound: true }
+        }
       }
 
       throw error

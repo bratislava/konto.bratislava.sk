2beda9c04975b5d598b78e97664054a4a95b455f
Jitka Muravská <44317233+jitka1997@users.noreply.github.com>
2024-04-08 12:49:16 +0200

    #701 unverified profile badge (#925)
    * Show unverified badge after failed verification
    
    * Refactor ternary operator

--- next/components/forms/simple-components/IdentityVerificationStatus.tsx
+++ next/components/forms/simple-components/IdentityVerificationStatus.tsx
@@ -15,29 +15,36 @@ const IdentityVerificationStatus = () => {
   const optionalFormRedirectsContext = useConditionalFormRedirects()
 
   if (!isSignedIn) return null
-
-  return tierStatus.isIdentityVerified ? (
-    <div className="flex rounded bg-success-100 px-2 py-0 lg:px-3 lg:py-1.5">
-      <span className="text-p3-medium text-success-700">{t('verification_status_success')}</span>
-    </div>
-  ) : tierStatus.isIdentityVerificationNotYetAttempted ? (
-    <div className="flex items-center gap-1.5">
-      <div className="flex items-center gap-2 rounded bg-warning-100 px-2 py-0 lg:px-3 lg:py-1.5">
-        <span className="text-p3-medium text-warning-700">{t('verification_status_required')}</span>
+  if (tierStatus.isIdentityVerified)
+    return (
+      <div className="flex rounded bg-success-100 px-2 py-0 lg:px-3 lg:py-1.5">
+        <span className="text-p3-medium text-success-700">{t('verification_status_success')}</span>
       </div>
-      <Button
-        className="hidden lg:flex"
-        size="sm"
-        onPress={() =>
-          optionalFormRedirectsContext
-            ? optionalFormRedirectsContext.verifyIdentity()
-            : router.push(ROUTES.IDENTITY_VERIFICATION)
-        }
-        variant="plain-black"
-        text={t('verification_url_text')}
-      />
-    </div>
-  ) : null
+    )
+
+  if (tierStatus.isIdentityVerificationNotYetAttempted || !tierStatus.isInQueue)
+    return (
+      <div className="flex items-center gap-1.5">
+        <div className="flex items-center gap-2 rounded bg-warning-100 px-2 py-0 lg:px-3 lg:py-1.5">
+          <span className="text-p3-medium text-warning-700">
+            {t('verification_status_required')}
+          </span>
+        </div>
+        <Button
+          className="hidden lg:flex"
+          size="sm"
+          onPress={() =>
+            optionalFormRedirectsContext
+              ? optionalFormRedirectsContext.verifyIdentity()
+              : router.push(ROUTES.IDENTITY_VERIFICATION)
+          }
+          variant="plain-black"
+          text={t('verification_url_text')}
+        />
+      </div>
+    )
+
+  return null
 }
 
 export default IdentityVerificationStatus

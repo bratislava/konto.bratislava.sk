64e37f7800ed934799ebfaa4b33ca5e9211e5f3f
Marek Bodinger <marek.bodinger@gmail.com>
2024-04-19 13:33:43 +0200

    WIP

---
+++ next/clients/graphql-strapi/gql.ts
@@ -0,0 +1,8 @@
+import { GraphQLClient } from 'graphql-request'
+
+import { getSdk } from './api'
+import { environment } from 'environment'
+
+const gql = new GraphQLClient(`${environment.cityAccountStrapiUrl}/graphql`)
+
+export const client = getSdk(gql)
--- next/pages/pomoc.tsx
+++ next/pages/pomoc.tsx
@@ -1,3 +1,5 @@
+import { client } from '@clients/graphql-strapi'
+import { HelpPageFragment } from '@clients/graphql-strapi/api'
 import HelpSection from 'components/forms/segments/AccountSections/HelpSection/HelpSection'
 import AccountPageLayout from 'components/layouts/AccountPageLayout'
 
@@ -5,18 +7,32 @@ import { SsrAuthProviderHOC } from '../components/logic/SsrAuthContext'
 import { amplifyGetServerSideProps } from '../frontend/utils/amplifyServer'
 import { slovakServerSideTranslations } from '../frontend/utils/slovakServerSideTranslations'
 
-export const getServerSideProps = amplifyGetServerSideProps(async () => {
+type HelpPageProps = {
+  helpPage: HelpPageFragment
+}
+
+export const getServerSideProps = amplifyGetServerSideProps<HelpPageProps>(async () => {
+  const helpPageQuery = await client.HelpPage()
+  const helpPage = helpPageQuery?.helpPage?.data?.attributes
+
+  if (!helpPage) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
     props: {
+      helpPage,
       ...(await slovakServerSideTranslations()),
     },
   }
 })
 
-const AccountHelpPage = () => {
+const AccountHelpPage = ({ helpPage }: HelpPageProps) => {
   return (
     <AccountPageLayout>
-      <HelpSection />
+      <HelpSection helpPage={helpPage} />
     </AccountPageLayout>
   )
 }

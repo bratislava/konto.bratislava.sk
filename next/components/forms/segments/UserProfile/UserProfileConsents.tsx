3d04224532427c40a868b0840b421481b24a85cb
Marek Bodinger <marek.bodinger@gmail.com>
2024-04-24 13:25:15 +0200

    Migrate UserProfileConsents to useUserV2

--- next/components/forms/segments/UserProfile/UserProfileConsents.tsx
+++ next/components/forms/segments/UserProfile/UserProfileConsents.tsx
@@ -1,32 +1,57 @@
+import {
+  GdprDataDtoCategoryEnum,
+  GdprDataDtoTypeEnum,
+  ResponseGdprUserDataDtoSubTypeEnum,
+} from '@clients/openapi-city-account'
 import cx from 'classnames'
 import { useTranslation } from 'next-i18next'
 
-import { Gdpr } from '../../../../frontend/dtos/generalApiDto'
 import useSnackbar from '../../../../frontend/hooks/useSnackbar'
-import useUser from '../../../../frontend/hooks/useUser'
+import { useUserV2 } from '../../../../frontend/hooks/useUserV2'
 import UserConsent from './UserConsent'
 import UserProfileSection from './UserProfileSection'
 import UserProfileSectionHeader from './UserProfileSectionHeader'
 
 const UserProfileConsents = () => {
   const { t } = useTranslation('account')
-  const user = useUser()
+  const {
+    userData,
+    taxesMarketingSubscribe,
+    taxesMarketingUnsubscribe,
+    taxesMarketingUnsubscribeIsPending,
+    taxesMarketingSubscribeIsPending,
+  } = useUserV2()
 
   const [openSnackbarSuccess] = useSnackbar({ variant: 'success' })
   const [openSnackbarError] = useSnackbar({ variant: 'error' })
+
+  const isSelected =
+    userData?.gdprData.some(
+      ({ category, type, subType }) =>
+        category === GdprDataDtoCategoryEnum.Taxes &&
+        type === GdprDataDtoTypeEnum.Marketing &&
+        subType === ResponseGdprUserDataDtoSubTypeEnum.Subscribe,
+    ) ?? false
+
+  const isDisabled =
+    !userData || taxesMarketingSubscribeIsPending || taxesMarketingUnsubscribeIsPending
+
   const handleOnChangeConsent = async (isSelected: boolean) => {
-    const gdprData: Gdpr[] = [{ category: 'ESBS', type: 'MARKETING' }]
-    const res = isSelected ? await user.subscribe(gdprData) : await user.unsubscribe(gdprData)
-    if (res) {
-      openSnackbarSuccess(t('profile_detail.success_alert'), 3000)
-    } else {
-      openSnackbarError(t('profile_detail.error_alert'))
+    if (isDisabled) {
+      return
     }
+
+    const mutation = isSelected ? taxesMarketingSubscribe : taxesMarketingUnsubscribe
+    await mutation(undefined, {
+      onSuccess: () => {
+        openSnackbarSuccess(t('profile_detail.success_alert'), 3000)
+      },
+      onError: () => {
+        openSnackbarError(t('profile_detail.error_alert'))
+      },
+    })
   }
 
-  const isSelected = user.data?.gdprData
-    .filter((x) => x.type === 'MARKETING' && x.category === 'ESBS')
-    .some((x) => x.subType === 'subscribe')
   return (
     <UserProfileSection>
       <UserProfileSectionHeader
@@ -41,8 +66,8 @@ const UserProfileConsents = () => {
             id: 'receive_information',
             title: t('consents.receive_information.title'),
             text: t('consents.receive_information.text'),
-            isDisabled: false,
-            isSelected: isSelected ?? false,
+            isDisabled,
+            isSelected,
           }}
           isLast
           onChange={handleOnChangeConsent}

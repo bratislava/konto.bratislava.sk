0b6bf3b59e72d1268c4e1133235caf3c9c7fa7eb
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-05-13 17:59:14 +0200

    Use own runner in amplify-test.tsx (#1010)
    * Use own runner in amplify-test.tsx
    
    * Use own config in amplify-test
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

--- next/pages/amplify-test.tsx
+++ next/pages/amplify-test.tsx
@@ -1,13 +1,33 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { createServerRunner } from '@aws-amplify/adapter-nextjs'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { fetchUserAttributes } from '@aws-amplify/auth/server'
 import type { AmplifyServer } from '@aws-amplify/core/dist/esm/adapterCore'
 import { GetServerSideProps } from 'next'
 
-import { runWithAmplifyServerContext } from '../frontend/utils/amplifyServer'
+import { environment } from '../environment'
 
 type AmplifyTestPageProps = {
   email: string | null | undefined
 }
+
+export const { runWithAmplifyServerContext } = createServerRunner({
+  config: {
+    Auth: {
+      Cognito: {
+        //  Amazon Cognito User Pool ID
+        userPoolId: environment.cognitoUserPoolId,
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolClientId: environment.cognitoClientId,
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: environment.cognitoIdentityPoolId,
+        // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+        // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+        signUpVerificationMethod: 'code', // 'code' | 'link'
+      },
+    },
+  },
+})
 
 function randomDelayPromise() {
   // Generate a random delay between 0 and 2000 milliseconds

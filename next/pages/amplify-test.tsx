4ab1aa4eaa3f2b6838f2b7ab09b9cee02f4d5b9d
Marek Bodinger <marek.bodinger@gmail.com>
2024-05-13 17:48:04 +0200

    Use own config in amplify-test

--- next/pages/amplify-test.tsx
+++ next/pages/amplify-test.tsx
@@ -5,14 +5,28 @@ import { fetchUserAttributes } from '@aws-amplify/auth/server'
 import type { AmplifyServer } from '@aws-amplify/core/dist/esm/adapterCore'
 import { GetServerSideProps } from 'next'
 
-import { amplifyConfig } from '../frontend/utils/amplifyConfig'
+import { environment } from '../environment'
 
 type AmplifyTestPageProps = {
   email: string | null | undefined
 }
 
 export const { runWithAmplifyServerContext } = createServerRunner({
-  config: amplifyConfig,
+  config: {
+    Auth: {
+      Cognito: {
+        //  Amazon Cognito User Pool ID
+        userPoolId: environment.cognitoUserPoolId,
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolClientId: environment.cognitoClientId,
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: environment.cognitoIdentityPoolId,
+        // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+        // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+        signUpVerificationMethod: 'code', // 'code' | 'link'
+      },
+    },
+  },
 })
 
 function randomDelayPromise() {

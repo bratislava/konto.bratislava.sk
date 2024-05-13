1e17af4aa2e95864c037accc61621f9423ef2332
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-05-13 17:01:48 +0200

    Create amplify test route (#1009)
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

---
+++ next/pages/amplify-test.tsx
@@ -0,0 +1,48 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { fetchUserAttributes } from '@aws-amplify/auth/server'
+import type { AmplifyServer } from '@aws-amplify/core/dist/esm/adapterCore'
+import { GetServerSideProps } from 'next'
+
+import { runWithAmplifyServerContext } from '../frontend/utils/amplifyServer'
+
+type AmplifyTestPageProps = {
+  email: string | null | undefined
+}
+
+function randomDelayPromise() {
+  // Generate a random delay between 0 and 2000 milliseconds
+  const delay = Math.floor(Math.random() * 2001)
+
+  // Return a new promise
+  return new Promise<null>((resolve) => {
+    setTimeout(() => {
+      resolve(null) // Resolve the promise with an empty value after the delay
+    }, delay)
+  })
+}
+
+export const getServerSideProps: GetServerSideProps<AmplifyTestPageProps> = async (ctx) => {
+  const email = await runWithAmplifyServerContext({
+    nextServerContext: { request: ctx.req, response: ctx.res },
+    operation: async (contextSpec: AmplifyServer.ContextSpec) => {
+      try {
+        const attributes = await fetchUserAttributes(contextSpec)
+        await randomDelayPromise()
+        return attributes.email
+      } catch (error) {
+        return null
+      }
+    },
+  })
+
+  return {
+    props: {
+      email,
+    },
+  }
+}
+
+// eslint-disable-next-line react/function-component-definition
+export default function AmplifyTest({ email }: AmplifyTestPageProps) {
+  return <div>{email}</div>
+}

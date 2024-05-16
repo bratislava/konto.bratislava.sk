af7058a0c83f2f6f8f0511b5b08a7eaa339bb60d
Martin Pinter <martin.pinter@bratislava.sk>
2024-05-16 12:59:07 +0200

    Use location replace to refresh app state

--- next/pages/force-logout.tsx
+++ next/pages/force-logout.tsx
@@ -1,12 +1,10 @@
 import logger from 'frontend/utils/logger'
-import { useRouter } from 'next/router'
 import { useEffectOnce } from 'usehooks-ts'
 
 import { removeAllCookiesAndClearLocalStorage, useSignOut } from '../frontend/utils/amplifyClient'
 
 const ForceLogoutPage = () => {
   const { signOut } = useSignOut()
-  const { replace } = useRouter()
 
   const logoutHandler = async () => {
     try {
@@ -18,11 +16,12 @@ const ForceLogoutPage = () => {
       // clearing BEFORE signing out, to make sure it happens (singOut redirects)
       removeAllCookiesAndClearLocalStorage()
       // shouldn't be needed, but to be sure that we clear also the in-memory session
-      // in worst case this throws, which is fine - we still cleared everything on the previous line & we'll redirect
+      // in worst case this throws, which is fine - we still cleared everything on the previous line
       await signOut()
     } catch (error) {
       logger.error('[AUTH] Error during forced sign out - redirecting to login', error)
-      replace('/prihlasenie')
+      // this also causes the app to reload, clearing any in memory state if signOut failed
+      window.location.replace('/prihlasenie')
     }
   }
 

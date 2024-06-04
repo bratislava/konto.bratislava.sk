79b9d956d0a30d97fb71c95cba9416fa8b78b4b2
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-06-04 13:43:05 +0200

    wip: remove unnecessary files

--- nest-forms-backend/src/schemas/schemas.errors.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum SchemasErrorsEnum {
-  SCHEMA_NOT_FOUND = 'SCHEMA_NOT_FOUND_ERROR',
-  SCHEMA_VERSION_NOT_FOUND = 'SCHEMA_VERSION_NOT_FOUND_ERROR',
-}
-
-export enum SchemasErrorsResponseEnum {
-  SCHEMA_NOT_FOUND = 'Schema not found.',
-  SCHEMA_VERSION_NOT_FOUND = 'Schema version was not found.',
-}
--- nest-forms-backend/src/utils/types/prisma.ts
+++ nest-forms-backend/src/utils/types/prisma.ts
@@ -1,26 +1,5 @@
 import { Prisma } from '@prisma/client'
 
-export type FormWithSchemaAndVersion = Prisma.FormsGetPayload<{
-  include: {
-    schemaVersion: {
-      include: {
-        schema: true
-      }
-    }
-  }
-}>
-
-export type FormWithSchemaVersionAndFiles = Prisma.FormsGetPayload<{
-  include: {
-    schemaVersion: {
-      include: {
-        schema: true
-      }
-    }
-    files: true
-  }
-}>
-
 export type FormWithSelectedProperties = Prisma.FormsGetPayload<{
   select: {
     id: true

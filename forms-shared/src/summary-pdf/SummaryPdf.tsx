b434c51f808f597c481f2e2ff4f0deef1740d79d
Marek Bodinger <marek.bodinger@gmail.com>
2024-06-04 16:34:41 +0200

    Improve rendering of summary PDF, add terms and conditions, make visually consistent with the app

--- forms-shared/src/summary-pdf/SummaryPdf.tsx
+++ forms-shared/src/summary-pdf/SummaryPdf.tsx
@@ -9,31 +9,42 @@ import SummaryRenderer, {
 } from '../summary-renderer/SummaryRenderer'
 import { SummaryDisplayValueType } from '../summary-json/getSummaryDisplayValue'
 import { SummaryJsonForm } from '../summary-json/summaryJsonTypes'
+import Markdown from 'react-markdown'
+import { generalTermsAndConditions } from '../definitions/terms-and-conditions'
+import cx from 'classnames'
 
 type SummaryPdfProps = {
   cssToInject: string
   summaryJson: SummaryJsonForm
 }
 
-const FormRenderer = ({ children }: SummaryFormRendererProps) => (
-  <div className="flex flex-col gap-8">{children}</div>
+const FormRenderer = ({ form, children }: SummaryFormRendererProps) => (
+  <div className="flex flex-col gap-8">
+    <h1 className="text-2xl font-semibold">{form.title}</h1>
+    {children}
+  </div>
 )
 
 const StepRenderer = ({ step, children }: SummaryStepRendererProps) => (
   <div className="flex flex-col gap-4">
-    <h2 className="text-lg font-bold">{step.title}</h2>
+    <h2 className="text-xl font-semibold">{step.title}</h2>
     <div>{children}</div>
   </div>
 )
 
-const FieldRenderer = ({ field, children }: SummaryFieldRendererProps) => (
-  <div className="flex flex-col py-2.5 border-b-2 border-gray-200">
-    <p className="font-semibold">{field.label}</p>
-    <div className="flex flex-row items-center">
-      <span>{children}</span>
+const FieldRenderer = ({ field, hasError, children }: SummaryFieldRendererProps) => {
+  const wrapperClass = cx('flex flex-row flex-nowrap gap-2 border-b-2 py-2.5', {
+    'border-gray-200': !hasError,
+    'border-red-500': hasError,
+  })
+
+  return (
+    <div className={wrapperClass}>
+      <p className="font-semibold flex-1">{field.label}</p>
+      <div className="flex flex-1 flex-col gap-2">{children}</div>
     </div>
-  </div>
-)
+  )
+}
 
 const DisplayValueRenderer = ({ displayValue }: SummaryDisplayValueRendererProps) => {
   switch (displayValue.type) {
@@ -61,15 +72,21 @@ const getArrayDepth = (id: string): number =>
   id.split('_').filter((part) => !isNaN(parseInt(part))).length
 
 const ArrayItemRenderer = ({ arrayItem, children }: SummaryArrayItemRendererProps) => {
-  const depth = getArrayDepth(arrayItem.id)
-  const depthClass = depth > 1 ? 'pl-4 border-l-4 border-gray-300' : ''
+  const arrayDepth = getArrayDepth(arrayItem.id)
+
+  const wrapperClass = cx('flex flex-col mb-4', {
+    'rounded-xl border border-gray-200 p-6 gap-6': arrayDepth === 1,
+    'gap-2': arrayDepth !== 1,
+  })
+
+  const spanClass = cx('font-semibold', {
+    'inline-block bg-gray-100 px-2 rounded-xl self-start': arrayDepth !== 1,
+  })
 
   return (
-    <div className={`mb-4 ${depthClass}`}>
-      <div className="mb-2 font-semibold bg-gray-100 px-2 inline-block rounded-full">
-        {arrayItem.title}
-      </div>
-      {children}
+    <div className={wrapperClass}>
+      <span className={spanClass}>{arrayItem.title}</span>
+      <div>{children}</div>
     </div>
   )
 }
@@ -78,19 +95,37 @@ export const SummaryPdf = ({ cssToInject, summaryJson }: SummaryPdfProps) => {
   return (
     <html>
       <head>
-        <title>Summary PDF</title>
+        <title>{summaryJson.title}</title>
         <style type="text/css" dangerouslySetInnerHTML={{ __html: cssToInject }} />
       </head>
-      <body className="font-sans">
-        <SummaryRenderer
-          summaryJson={summaryJson}
-          renderForm={FormRenderer}
-          renderStep={StepRenderer}
-          renderField={FieldRenderer}
-          renderArray={ArrayRenderer}
-          renderArrayItem={ArrayItemRenderer}
-          renderDisplayValue={DisplayValueRenderer}
-        />
+      <body className="font-sans text-[#333]">
+        <div className="flex flex-col gap-8">
+          <SummaryRenderer
+            summaryJson={summaryJson}
+            renderForm={FormRenderer}
+            renderStep={StepRenderer}
+            renderField={FieldRenderer}
+            renderArray={ArrayRenderer}
+            renderArrayItem={ArrayItemRenderer}
+            renderDisplayValue={DisplayValueRenderer}
+          />
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold">Ochrana osobných údajov</h2>
+            <Markdown
+              className="rounded-xl bg-gray-50 p-6"
+              components={{
+                a: ({ children, href }) => (
+                  <a href={href} className="font-semibold underline" target="_blank">
+                    {children}
+                  </a>
+                ),
+              }}
+            >
+              {/* TODO: Use from form definition. */}
+              {generalTermsAndConditions}
+            </Markdown>
+          </div>
+        </div>
       </body>
     </html>
   )

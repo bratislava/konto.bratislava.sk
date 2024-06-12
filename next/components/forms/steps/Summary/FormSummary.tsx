8e7c9f6caf166a2aaee042a4227171d71db8d879
Marek Bodinger <marek.bodinger@gmail.com>
2024-06-12 18:18:19 +0200

    Rewrite form summary to use new ValidatedSummary, creates multiple optimizations that were needed to have seamless experience, moves the provider to all other form providers, as it is used in useFormSend and is consistent with others

--- next/components/forms/steps/Summary/FormSummary.tsx
+++ next/components/forms/steps/Summary/FormSummary.tsx
@@ -6,21 +6,18 @@ import SummaryForm from './SummaryForm'
 import SummaryFormControls from './SummaryFormControls'
 import SummaryFormLegalText from './SummaryFormLegalText'
 import SummaryFormSignature from './SummaryFormSignature'
-import { FormSummaryProvider } from './useFormSummary'
 
 const FormSummary = () => {
   const { isSigned } = useFormContext()
 
   return (
-    <FormSummaryProvider>
-      <div className="flex flex-col gap-4">
-        <SummaryHeader />
-        {environment.featureToggles.newSummaryDetails ? <SummaryDetails /> : <SummaryForm />}
-        {isSigned && <SummaryFormSignature />}
-        <SummaryFormLegalText />
-        <SummaryFormControls />
-      </div>
-    </FormSummaryProvider>
+    <div className="flex flex-col gap-4">
+      <SummaryHeader />
+      {environment.featureToggles.newSummaryDetails ? <SummaryDetails /> : <SummaryForm />}
+      {isSigned && <SummaryFormSignature />}
+      <SummaryFormLegalText />
+      <SummaryFormControls />
+    </div>
   )
 }
 

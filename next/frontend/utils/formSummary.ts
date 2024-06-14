90aba9becfdf1f54b287ce0ff892fe2d1ed964b4
Marek Bodinger <marek.bodinger@gmail.com>
2024-06-14 16:25:44 +0200

    Disable form submit when files are being uploaded

--- next/frontend/utils/formSummary.ts
+++ next/frontend/utils/formSummary.ts
@@ -1,3 +1,4 @@
+import { isUploadFileStatusType } from '@forms-shared/form-files/fileStatus'
 import { ValidatedSummary } from '@forms-shared/summary-renderer/validateSummary'
 
 export const isFormSigningDisabled = ({ hasErrors }: ValidatedSummary) => hasErrors
@@ -6,5 +7,10 @@ export const isFormSigningDisabled = ({ hasErrors }: ValidatedSummary) => hasErr
  * We want to disable submit button only in those cases, technically it is possible to send form with other errors,
  * but they are displayed in modal instead.
  */
-export const isFormSubmitDisabled = ({ hasErrors }: ValidatedSummary, isValidSignature: boolean) =>
-  hasErrors || !isValidSignature
+export const isFormSubmitDisabled = (
+  { hasErrors, filesInFormData }: ValidatedSummary,
+  isValidSignature: boolean,
+) =>
+  hasErrors ||
+  !isValidSignature ||
+  filesInFormData.some((file) => isUploadFileStatusType(file.statusType))

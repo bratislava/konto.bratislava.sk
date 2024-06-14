b3e6a99d3b13749e21e4b261bdce8baf4cf343ce
Marek Bodinger <marek.bodinger@gmail.com>
2024-06-14 16:37:10 +0200

    Icon in SummaryFile needs to be memoized, otherwise it rerenders the spinner to default position all the time

--- next/components/forms/steps/Summary/SummaryFile.tsx
+++ next/components/forms/steps/Summary/SummaryFile.tsx
@@ -7,7 +7,7 @@ import {
   isUploadFileStatusType,
 } from '@forms-shared/form-files/fileStatus'
 import cx from 'classnames'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import Spinner from '../../simple-components/Spinner'
 
@@ -20,26 +20,27 @@ const SummaryFile = ({ fileInfo }: SummaryFileProps) => {
   const isScanningStyle = isScanFileStatusType(fileInfo.statusType)
   const isDoneStyle = isDoneFileStatusType(fileInfo.statusType)
   const isUploadingStyle = isUploadFileStatusType(fileInfo.statusType)
-  const isDefaultStyle = !isErrorStyle && !isDoneStyle && !isScanningStyle
 
-  const Icon = (props: { 'data-cy': string }) =>
-    isErrorStyle ? (
-      <AlertIcon className="text-error" {...props} />
-    ) : isScanningStyle ? (
-      <ScanningIcon {...props} />
-    ) : isDoneStyle ? (
-      <CheckInCircleIcon className="text-success-700" {...props} />
-    ) : isUploadingStyle ? (
-      <Spinner size="sm" className="size-6" {...props} />
-    ) : isDefaultStyle ? (
-      <AttachmentIcon {...props} />
-    ) : null
+  const Icon = useMemo(() => {
+    const props = { 'data-cy': 'summary-row-icon' }
+    if (isErrorStyle) {
+      return <AlertIcon className="text-error" {...props} />
+    }
+    if (isScanningStyle) {
+      return <ScanningIcon {...props} />
+    }
+    if (isDoneStyle) {
+      return <CheckInCircleIcon className="text-success-700" {...props} />
+    }
+    if (isUploadingStyle) {
+      return <Spinner size="sm" className="size-6" {...props} />
+    }
+    return <AttachmentIcon {...props} />
+  }, [isErrorStyle, isScanningStyle, isDoneStyle, isUploadingStyle])
 
   return (
     <div className={cx('flex items-center gap-3', { 'text-error': isErrorStyle })}>
-      <div className="shrink-0">
-        <Icon data-cy="summary-row-icon" />
-      </div>
+      <div className="shrink-0">{Icon}</div>
       {fileInfo.fileName}
     </div>
   )

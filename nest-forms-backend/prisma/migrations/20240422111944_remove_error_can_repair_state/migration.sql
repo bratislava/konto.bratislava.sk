165c7dec685f9a3e55cc5e8f6836dfeaf598c7ce
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-06-20 10:48:13 +0200

    fix: add missing migrations

---
+++ nest-forms-backend/prisma/migrations/20240422111944_remove_error_can_repair_state/migration.sql
@@ -0,0 +1,10 @@
+BEGIN;
+UPDATE "Forms" SET "state" = 'ERROR' WHERE "state" = 'ERROR_USER_CAN_REPAIR';
+CREATE TYPE "FormState_new" AS ENUM ('DRAFT', 'QUEUED', 'SENDING_TO_NASES', 'DELIVERED_NASES', 'DELIVERED_GINIS', 'PROCESSING', 'FINISHED', 'REJECTED', 'ERROR');
+ALTER TABLE "Forms" ALTER COLUMN "state" DROP DEFAULT;
+ALTER TABLE "Forms" ALTER COLUMN "state" TYPE "FormState_new" USING ("state"::text::"FormState_new");
+ALTER TYPE "FormState" RENAME TO "FormState_old";
+ALTER TYPE "FormState_new" RENAME TO "FormState";
+DROP TYPE "FormState_old";
+ALTER TABLE "Forms" ALTER COLUMN "state" SET DEFAULT 'DRAFT';
+COMMIT;

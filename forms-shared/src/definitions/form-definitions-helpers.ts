15b47f86ce5f19e0a97f8c1b25921d8290ab075d
Marek Bodinger <marek.bodinger@gmail.com>
2024-06-24 13:49:37 +0200

    Move type helpers to formDefinitionTypes

--- forms-shared/src/definitions/form-definitions-helpers.ts
+++ forms-shared/src/definitions/form-definitions-helpers.ts
@@ -1,32 +1,5 @@
-import {
-  FormDefinition,
-  FormDefinitionEmail,
-  formDefinitions,
-  FormDefinitionSlovenskoSk,
-  FormDefinitionSlovenskoSkGeneric,
-  FormDefinitionSlovenskoSkTax,
-  FormDefinitionType,
-} from './form-definitions'
-
-export const isSlovenskoSkGenericFormDefinition = (
-  formDefinition: FormDefinition,
-): formDefinition is FormDefinitionSlovenskoSkGeneric =>
-  formDefinition.type === FormDefinitionType.SlovenskoSkGeneric
-
-export const isSlovenskoSkTaxFormDefinition = (
-  formDefinition: FormDefinition,
-): formDefinition is FormDefinitionSlovenskoSkTax =>
-  formDefinition.type === FormDefinitionType.SlovenskoSkTax
-
-export const isSlovenskoSkFormDefinition = (
-  formDefinition: FormDefinition,
-): formDefinition is FormDefinitionSlovenskoSk =>
-  isSlovenskoSkGenericFormDefinition(formDefinition) ||
-  isSlovenskoSkTaxFormDefinition(formDefinition)
-
-export const isEmailFormDefinition = (
-  formDefinition: FormDefinition,
-): formDefinition is FormDefinitionEmail => formDefinition.type === FormDefinitionType.Email
+import { formDefinitions } from './form-definitions'
+import { FormDefinition } from './formDefinitionTypes'
 
 export const getFormDefinitionBySlug = (slug: string): FormDefinition | null => {
   const formDefinition = formDefinitions.find((formDefinition) => formDefinition.slug === slug)
--- forms-shared/src/definitions/formDefinitionTypes.ts
+++ forms-shared/src/definitions/formDefinitionTypes.ts
@@ -47,3 +47,23 @@ export type FormDefinition =
   | FormDefinitionSlovenskoSkGeneric
   | FormDefinitionSlovenskoSkTax
   | FormDefinitionEmail
+
+export const isSlovenskoSkGenericFormDefinition = (
+  formDefinition: FormDefinition,
+): formDefinition is FormDefinitionSlovenskoSkGeneric =>
+  formDefinition.type === FormDefinitionType.SlovenskoSkGeneric
+
+export const isSlovenskoSkTaxFormDefinition = (
+  formDefinition: FormDefinition,
+): formDefinition is FormDefinitionSlovenskoSkTax =>
+  formDefinition.type === FormDefinitionType.SlovenskoSkTax
+
+export const isSlovenskoSkFormDefinition = (
+  formDefinition: FormDefinition,
+): formDefinition is FormDefinitionSlovenskoSk =>
+  isSlovenskoSkGenericFormDefinition(formDefinition) ||
+  isSlovenskoSkTaxFormDefinition(formDefinition)
+
+export const isEmailFormDefinition = (
+  formDefinition: FormDefinition,
+): formDefinition is FormDefinitionEmail => formDefinition.type === FormDefinitionType.Email

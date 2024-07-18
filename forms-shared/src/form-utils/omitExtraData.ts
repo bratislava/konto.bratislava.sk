5835b88c2da06417eca52385aaf7eb5822738c08
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-07-18 17:27:26 +0200

    Omit extra data util (#1189)
    Create omitExtraData util
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

---
+++ forms-shared/src/form-utils/omitExtraData.ts
@@ -0,0 +1,21 @@
+import { createSchemaUtils, GenericObjectType, RJSFSchema } from '@rjsf/utils'
+import Form from '@rjsf/core'
+import { baRjsfValidator } from './validators'
+import { baDefaultFormStateBehavior } from './defaultFormState'
+
+/**
+ * Omits extra data from form data.
+ *
+ * Until https://github.com/rjsf-team/react-jsonschema-form/issues/4081 is resolved this is the only way how to omit
+ * extra data from form data.
+ */
+export function omitExtraData(schema: RJSFSchema, formData: GenericObjectType) {
+  const schemaUtils = createSchemaUtils(baRjsfValidator, schema, baDefaultFormStateBehavior)
+  const formInstance = new Form({ schema, validator: baRjsfValidator })
+
+  const retrievedSchema = schemaUtils.retrieveSchema(schema, formData)
+  const pathSchema = schemaUtils.toPathSchema(retrievedSchema, undefined, formData)
+  const fieldNames = formInstance.getFieldNames(pathSchema, formData)
+
+  return formInstance.getUsedFormData(formData, fieldNames)
+}

fa0cdef85406643962c8247424816a90d3cb738b
Marek Bodinger <marek.bodinger@gmail.com>
2024-07-22 14:12:47 +0200

    Both MenuList and FormHeader contained the list of menu items, created a new useFormMenuItems that returns the list for both, use new permissions from useFormContext

--- next/components/forms/steps/MenuList.tsx
+++ next/components/forms/steps/MenuList.tsx
@@ -1,97 +1,16 @@
-import { BinIcon, ConnectionIcon, DiscIcon, DownloadIcon, PdfIcon } from '@assets/ui-icons'
 import cx from 'classnames'
 import Link from 'next/link'
-import { useTranslation } from 'next-i18next'
-import { ReactNode } from 'react'
 
-import { useFormExportImport } from '../../../frontend/hooks/useFormExportImport'
-import { useFormContext } from '../useFormContext'
-import { useFormModals } from '../useFormModals'
-
-type MenuItem = {
-  title: string
-  icon: ReactNode
-  url?: string
-  onPress?: () => void
-  className?: string
-  dataCy?: string
-}
+import { useFormMenuItems } from '../useFormMenuItems'
 
 const MenuList = () => {
-  const { isTaxForm, isReadonly, isDeletable } = useFormContext()
-  const {
-    exportXml,
-    exportPdf,
-    importXml,
-    saveConcept,
-    deleteConcept,
-    showImportExportJson,
-    exportJson,
-    importJson,
-  } = useFormExportImport()
-  const { t } = useTranslation('forms')
-  const { setDeleteConceptModal } = useFormModals()
-
-  const menuList = [
-    isReadonly
-      ? null
-      : {
-          title: t('menu_list.save_concept'),
-          icon: <DiscIcon className="size-6" />,
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onPress: () => saveConcept(),
-          dataCy: 'save-concept-mobile',
-        },
-    {
-      title: t('menu_list.download_xml'),
-      icon: <DownloadIcon className="size-6" />,
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      onPress: () => exportXml(),
-    },
-    isTaxForm
-      ? null
-      : {
-          title: t('menu_list.pdf'),
-          icon: <PdfIcon className="size-6" />,
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onPress: () => exportPdf(),
-        },
-    isReadonly
-      ? null
-      : {
-          title: t('menu_list.upload_xml'),
-          icon: <ConnectionIcon className="size-6" />,
-          onPress: importXml,
-        },
-    showImportExportJson
-      ? {
-          title: t('menu_list.download_json'),
-          icon: <DownloadIcon className="size-6" />,
-          onPress: exportJson,
-        }
-      : null,
-    showImportExportJson
-      ? {
-          title: t('menu_list.upload_json'),
-          icon: <ConnectionIcon className="size-6" />,
-          onPress: importJson,
-        }
-      : null,
-    isDeletable
-      ? null
-      : {
-          title: t('menu_list.delete'),
-          icon: <BinIcon className="size-6" />,
-          onPress: () => setDeleteConceptModal({ isOpen: true, sendCallback: deleteConcept }),
-          className: 'text-negative-700',
-        },
-  ].filter(Boolean) as MenuItem[]
+  const menuItems = useFormMenuItems()
 
   return (
     <ul className="mt-4 flex flex-col gap-3 border-t-2 border-gray-200 pt-4 lg:hidden">
-      {menuList.map((menuItem, i) =>
+      {menuItems.map((menuItem, index) =>
         menuItem.url ? (
-          <li className="w-max" key={i}>
+          <li className="w-max" key={index}>
             <Link href={menuItem.url}>
               <div className={cx('flex items-center gap-3', menuItem.className)}>
                 {menuItem.icon}
@@ -100,7 +19,7 @@ const MenuList = () => {
             </Link>
           </li>
         ) : (
-          <li className="w-max" key={i}>
+          <li className="w-max" key={index}>
             <button type="button" onClick={menuItem.onPress} data-cy={menuItem.dataCy ?? ''}>
               <div className={cx('flex items-center gap-3', menuItem.className)}>
                 {menuItem.icon}
---
+++ next/components/forms/useFormMenuItems.tsx
@@ -0,0 +1,84 @@
+import { BinIcon, ConnectionIcon, DiscIcon, DownloadIcon, PdfIcon } from '@assets/ui-icons'
+import { useTranslation } from 'next-i18next'
+
+import { useFormExportImport } from '../../frontend/hooks/useFormExportImport'
+import { isDefined } from '../../frontend/utils/general'
+import { MenuItemBase } from './simple-components/MenuDropdown/MenuDropdown'
+import { useFormContext } from './useFormContext'
+import { useFormModals } from './useFormModals'
+
+type FormMenuItem = MenuItemBase & {
+  dataCy?: string
+  className?: string
+}
+
+export const useFormMenuItems = (): FormMenuItem[] => {
+  const {
+    isReadonly,
+    isDeletable,
+    xmlImportExportAllowed,
+    jsonImportExportAllowed,
+    pdfDownloadInMenuAllowed,
+  } = useFormContext()
+  const { exportXml, exportPdf, importXml, saveConcept, deleteConcept, exportJson, importJson } =
+    useFormExportImport()
+  const { t } = useTranslation('forms')
+  const { setDeleteConceptModal } = useFormModals()
+
+  return [
+    isReadonly
+      ? null
+      : {
+          title: t('menu_list.save_concept'),
+          icon: <DiscIcon className="size-6" />,
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onPress: () => saveConcept(),
+          dataCy: 'save-concept-mobile',
+        },
+    xmlImportExportAllowed
+      ? {
+          title: t('menu_list.download_xml'),
+          icon: <DownloadIcon className="size-6" />,
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onPress: () => exportXml(),
+        }
+      : null,
+    pdfDownloadInMenuAllowed
+      ? null
+      : {
+          title: t('menu_list.pdf'),
+          icon: <PdfIcon className="size-6" />,
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onPress: () => exportPdf(),
+        },
+    !isReadonly && xmlImportExportAllowed
+      ? {
+          title: t('menu_list.upload_xml'),
+          icon: <ConnectionIcon className="size-6" />,
+          onPress: importXml,
+        }
+      : null,
+    jsonImportExportAllowed
+      ? {
+          title: t('menu_list.download_json'),
+          icon: <DownloadIcon className="size-6" />,
+          onPress: exportJson,
+        }
+      : null,
+    !isReadonly && jsonImportExportAllowed
+      ? {
+          title: t('menu_list.upload_json'),
+          icon: <ConnectionIcon className="size-6" />,
+          onPress: importJson,
+        }
+      : null,
+    isDeletable
+      ? null
+      : {
+          title: t('menu_list.delete'),
+          icon: <BinIcon className="size-6" />,
+          onPress: () => setDeleteConceptModal({ isOpen: true, sendCallback: deleteConcept }),
+          className: 'text-negative-700',
+        },
+  ].filter(isDefined)
+}

6f8259756a77c616943a0d5372269362157ba90f
Marek Bodinger <marek.bodinger@gmail.com>
2024-07-23 13:21:44 +0200

    Add whitespace-pre-wrap to string value in summary PDF to accomodate multiline values

--- forms-shared/src/summary-pdf/SummaryPdf.tsx
+++ forms-shared/src/summary-pdf/SummaryPdf.tsx
@@ -50,7 +50,7 @@ const FieldRenderer = ({ field, hasError, children }: SummaryFieldRendererProps)
 }
 
 const StringValueRenderer = ({ value }: SummaryStringValueRendererProps) => {
-  return <span>{value}</span>
+  return <span className="whitespace-pre-wrap">{value}</span>
 }
 
 const FileIcon = () => (

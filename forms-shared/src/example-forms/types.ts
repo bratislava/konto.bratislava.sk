f40d22df0efc71fe15a1999788224237dc697baa
Marek Bodinger <marek.bodinger@gmail.com>
2024-07-23 13:42:53 +0200

    Create more advanced example forms (with named examples, possibility to add files) + getExampleFormPairs helper function

---
+++ forms-shared/src/example-forms/types.ts
@@ -0,0 +1,10 @@
+import { GenericObjectType } from '@rjsf/utils'
+import { FormsBackendFile } from '../form-files/serverFilesTypes'
+import { ClientFileInfo } from '../form-files/fileStatus'
+
+export type ExampleForm<FormData = GenericObjectType> = {
+  name: string
+  formData: FormData
+  serverFiles?: FormsBackendFile[]
+  clientFiles?: ClientFileInfo[]
+}

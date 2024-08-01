ae73212372a2ed4816aa48a19c8795dbea1dc0ec
Marek Bodinger <marek.bodinger@gmail.com>
2024-08-01 14:28:16 +0200

    wip

--- forms-shared/tests/slovensko-sk-xml/generateSlovenskoSkXml.ts
+++ forms-shared/tests/slovensko-sk-xml/generateSlovenskoSkXml.ts
@@ -1,26 +1,30 @@
 import { getExampleFormPairs } from '../../src/example-forms/getExampleFormPairs'
 import { generateSlovenskoSkXml } from '../../src/slovensko-sk/generateSlovenskoSkXml'
-import {
-  FormDefinitionSlovenskoSk,
-  isSlovenskoSkTaxFormDefinition,
-} from '../../src/definitions/formDefinitionTypes'
+import { FormDefinitionSlovenskoSk } from '../../src/definitions/formDefinitionTypes'
 import { renderApacheFopPdf } from '../../test-utils/apache-fop/renderApacheFopPdf'
 import { pdfFop } from './pdffop'
+import fs from 'node:fs'
+import * as path from 'node:path'
 import { expectPdfToMatchSnapshot } from '../../test-utils/expectPdfToMatchSnapshot'
 
 describe('generateSlovenskoSkXml', () => {
-  getExampleFormPairs({ formDefinitionFilterFn: isSlovenskoSkTaxFormDefinition }).forEach(
-    ({ formDefinition, exampleForm }) => {
-      it(`${exampleForm.name} summary JSON should match snapshot`, async () => {
-        const result = await generateSlovenskoSkXml(
-          formDefinition as FormDefinitionSlovenskoSk,
-          exampleForm.formData,
-          exampleForm.serverFiles,
-        )
-        const pdf = await renderApacheFopPdf(result, pdfFop)
-        await expectPdfToMatchSnapshot(pdf)
-        // expect(result).toMatchSnapshot?()
-      }, 15000)
-    },
-  )
+  getExampleFormPairs().forEach(({ formDefinition, exampleForm }) => {
+    it(`${exampleForm.name} summary JSON should match snapshot`, async () => {
+      const result = await generateSlovenskoSkXml(
+        formDefinition as FormDefinitionSlovenskoSk,
+        exampleForm.formData,
+        exampleForm.serverFiles,
+      )
+      console.log(result)
+      const pdf = await renderApacheFopPdf(
+        result,
+        pdfFop({
+          formDefinition: formDefinition as FormDefinitionSlovenskoSk,
+          testEnvironment: true,
+        }),
+      )
+      fs.writeFileSync(path.join(__dirname, `${exampleForm.name}.pdf`), pdf)
+      await expectPdfToMatchSnapshot(pdf)
+    }, 15000)
+  })
 })
--- forms-shared/tests/slovensko-sk-xml/pdffop.ts
+++ forms-shared/tests/slovensko-sk-xml/pdffop.ts
@@ -1,7 +1,17 @@
-export const pdfFop = `<?xml version="1.0" encoding="UTF-8"?>
+import { FormDefinitionSlovenskoSk } from '../../src/definitions/formDefinitionTypes'
+
+export type PdfFopPayload = {
+  formDefinition: FormDefinitionSlovenskoSk
+  testEnvironment: boolean
+}
+
+export const pdfFop = ({
+  formDefinition,
+  testEnvironment = false,
+}: PdfFopPayload) => `<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:fo="http://www.w3.org/1999/XSL/Format"
-                xmlns:e="http://schemas.gov.sk/form/esmao.eforms.bratislava.obec_024/201501.2">
+                xmlns:e="http://schemas.gov.sk/form/${formDefinition.pospID}/${formDefinition.pospVersion}">
 
   <xsl:output method="xml" indent="yes"/>
 
@@ -9,20 +19,28 @@ export const pdfFop = `<?xml version="1.0" encoding="UTF-8"?>
     <fo:root>
       <fo:layout-master-set>
         <fo:simple-page-master master-name="A4" page-height="29.7cm" page-width="21cm" margin="2cm">
-          <fo:region-body/>
+          <fo:region-body margin-bottom="1.5cm"/>
+          <fo:region-after extent="1.5cm"/>
         </fo:simple-page-master>
       </fo:layout-master-set>
-      <fo:page-sequence master-reference="A4">
+      <fo:page-sequence master-reference="A4" font-family="${testEnvironment ? 'Liberation Sans' : 'Arial'}">
+        <fo:static-content flow-name="xsl-region-after">
+          <fo:block text-align="center" padding-top="1cm">
+            Strana <fo:page-number/> z <fo:page-number-citation ref-id="last-page"/>
+          </fo:block>
+        </fo:static-content>
         <fo:flow flow-name="xsl-region-body">
+          <fo:block font-size="24pt" font-weight="bold" space-after="20pt">
+            <xsl:value-of select="//e:E-form/e:Meta/e:Name"/>
+          </fo:block>
           <xsl:apply-templates select="//e:E-form/e:Body/e:Summary/e:Form/*"/>
-          
-          <!-- Add the "Ochrana osobných údajov" section -->
           <fo:block font-size="16pt" font-weight="bold" space-before="20pt" space-after="10pt">
             Ochrana osobných údajov
           </fo:block>
           <fo:block>
             <xsl:value-of select="//e:E-form/e:Body/e:TermsAndConditions"/>
           </fo:block>
+          <fo:block id="last-page"/>
         </fo:flow>
       </fo:page-sequence>
     </fo:root>
@@ -37,12 +55,16 @@ export const pdfFop = `<?xml version="1.0" encoding="UTF-8"?>
 
   <xsl:template match="e:Field">
     <fo:table width="100%" space-before="3pt" space-after="3pt" border-bottom="1pt solid #CCCCCC">
-      <fo:table-column column-width="50%"/>
-      <fo:table-column column-width="50%"/>
+      <fo:table-column column-width="48%"/>
+      <fo:table-column column-width="4%"/>
+      <fo:table-column column-width="48%"/>
       <fo:table-body>
         <fo:table-row>
           <fo:table-cell padding-top="3pt" padding-bottom="3pt" start-indent="0pt">
             <fo:block><xsl:value-of select="@label"/></fo:block>
+          </fo:table-cell>
+          <fo:table-cell>
+            <fo:block></fo:block>
           </fo:table-cell>
           <fo:table-cell padding-top="3pt" padding-bottom="3pt">
             <fo:block>
@@ -77,7 +99,15 @@ export const pdfFop = `<?xml version="1.0" encoding="UTF-8"?>
   <xsl:template match="e:StringValue">
     <fo:block space-after="3pt"><xsl:value-of select="."/></fo:block>
   </xsl:template>
+  
+  <xsl:template match="e:FileValue">
+    <fo:block space-after="3pt"><xsl:value-of select="."/></fo:block>
+  </xsl:template>
 
+  <xsl:template match="e:InvalidValue">
+    <fo:block space-after="3pt">Neznáma hodnota</fo:block>
+  </xsl:template>
+  
   <xsl:template match="e:NoneValue">
     <fo:block space-after="3pt">-</fo:block>
   </xsl:template>

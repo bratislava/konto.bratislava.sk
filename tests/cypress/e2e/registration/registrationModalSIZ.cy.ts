e5f494b077afa4b796bee42e6fd4fd2296c59cc9
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-08-19 11:18:14 +0200

    Allow cypress video tests + prettier reformat (#1273)
    * Generate video artifacts for Cypress based on https://docs.cypress.io/guides/guides/screenshots-and-videos#Delete-videos-for-specs-without-failing-or-retried-tests
    
    * Prettier reformat cypress files
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>
    Co-authored-by: Richi <richard.dvorsky@bratislava.sk>

--- tests/cypress/e2e/registration/registrationModalSIZ.cy.ts
+++ tests/cypress/e2e/registration/registrationModalSIZ.cy.ts
@@ -7,7 +7,6 @@ describe('RF03 -', { testIsolation: false }, () => {
     .filter((device) => Cypress.env('devices')[`${device}`])
     .forEach((device) => {
       context(device, Cypress.env('resolution')[`${device}`], () => {
-
         beforeEach(() => {
           cy.visit('/mestske-sluzby/stanovisko-k-investicnemu-zameru')
           cy.hideNavbar(device)
@@ -15,14 +14,14 @@ describe('RF03 -', { testIsolation: false }, () => {
 
         it('1. Registration modal is redirecting to registration page.', () => {
           cy.dataCy('registration-modal-button').click()
-          cy.url().should("include", "/registracia");
+          cy.url().should('include', '/registracia')
         })
 
         it(`2. Reopening registration modal with save as a concept button.`, () => {
           cy.dataCy('close-modal').click()
 
           cy.dataCy(`save-concept-${device}`).should('be.visible').click()
-          cy.dataCy('registration-modal').should('be.visible')//.matchImage()
+          cy.dataCy('registration-modal').should('be.visible') //.matchImage()
         })
       })
     })
--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -4,12 +4,11 @@ import './commands/account'
 import './commands/form'
 import './triggers/before'
 import '@frsource/cypress-plugin-visual-regression-diff'
-import 'cypress-file-upload';
+import 'cypress-file-upload'
 
-const app = window.top;
+const app = window.top
 if (!app.document.head.querySelector('[data-testid="snapshot-controls]')) {
-  const style = app.document.createElement('style');
-  style.innerHTML =
-    '[data-testid="snapshot-controls"] { display: none }';
-  app.document.head.appendChild(style);
+  const style = app.document.createElement('style')
+  style.innerHTML = '[data-testid="snapshot-controls"] { display: none }'
+  app.document.head.appendChild(style)
 }

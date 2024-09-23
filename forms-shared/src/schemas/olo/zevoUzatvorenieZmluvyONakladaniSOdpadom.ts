1a56f8c39f91f6ed169cb939f43d36612b2f8d56
Marek Bodinger <marek.bodinger@gmail.com>
2024-09-23 09:08:47 +0200

    OLO forms v1

--- forms-shared/src/schemas/olo/zevoUzatvorenieZmluvyONakladaniSOdpadom.ts
+++
@@ -1,5 +0,0 @@
-import { schema, step } from '../../generator/functions'
-
-export default schema({ title: 'Uzatvorenie zmluvy o nakladaní s odpadom' }, {}, [
-  step('ziadatel', { title: 'Žiadateľ' }, []),
-])

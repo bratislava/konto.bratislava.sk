e09b4adce61a7ffe1ab8d54e94709d2ef4187782
Erik Rehulka <erik.rehulka@bratislava.sk>
2024-10-14 13:01:26 +0200

    send olo mails through their smtp server

---
+++ nest-forms-backend/src/utils/global-enums/mailgun.errors.enum.ts
@@ -0,0 +1,7 @@
+export enum MailgunErrorsEnum {
+  SEND_OLO_MAIL_ERROR = 'SEND_OLO_MAIL_ERROR',
+}
+
+export enum MailgunErrorsResponseEnum {
+  SEND_OLO_MAIL_ERROR = 'Error when sending olo mail',
+}

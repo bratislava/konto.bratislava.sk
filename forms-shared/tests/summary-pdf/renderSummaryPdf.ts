9e9411e766faea2aa654bb1f179b329215db6a85
Marek Bodinger <marek.bodinger@gmail.com>
2024-10-31 12:58:54 +0100

    Create screenshotTestTimeout const and add it to renderSummaryEmail

--- forms-shared/tests/summary-pdf/renderSummaryPdf.ts
+++ forms-shared/tests/summary-pdf/renderSummaryPdf.ts
@@ -3,27 +3,32 @@ import { filterConsole } from '../../test-utils/filterConsole'
 import { renderSummaryPdf } from '../../src/summary-pdf/renderSummaryPdf'
 import { launchPlaywrightTest } from '../../test-utils/launchPlaywright'
 import { expectPdfToMatchSnapshot } from '../../test-utils/expectPdfToMatchSnapshot'
+import { screenshotTestTimeout } from '../../test-utils/consts'
 
 describe('getSummaryJson', () => {
   getExampleFormPairs().forEach(({ formDefinition, exampleForm }) => {
-    it(`${exampleForm.name} summary PDF should match snapshot`, async () => {
-      filterConsole(
-        'error',
-        (message) =>
-          typeof message === 'string' &&
-          message.includes(
-            'Support for defaultProps will be removed from function components in a future major release.',
-          ),
-      )
+    it(
+      `${exampleForm.name} summary PDF should match snapshot`,
+      async () => {
+        filterConsole(
+          'error',
+          (message) =>
+            typeof message === 'string' &&
+            message.includes(
+              'Support for defaultProps will be removed from function components in a future major release.',
+            ),
+        )
 
-      const pdfBuffer = await renderSummaryPdf({
-        formDefinition,
-        formData: exampleForm.formData,
-        launchBrowser: launchPlaywrightTest,
-        serverFiles: exampleForm.serverFiles,
-      })
+        const pdfBuffer = await renderSummaryPdf({
+          formDefinition,
+          formData: exampleForm.formData,
+          launchBrowser: launchPlaywrightTest,
+          serverFiles: exampleForm.serverFiles,
+        })
 
-      await expectPdfToMatchSnapshot(pdfBuffer)
-    }, /* The PDFs take a while to generate, so they need an increased timeout. */ 30000)
+        await expectPdfToMatchSnapshot(pdfBuffer)
+      },
+      screenshotTestTimeout,
+    )
   })
 })

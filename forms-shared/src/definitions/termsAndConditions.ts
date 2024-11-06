59407f363d024941a3281d21775378dd748ca737
Marek Bodinger <marek.bodinger@gmail.com>
2024-11-06 14:17:24 +0100

    OLO terms and conditions

--- forms-shared/src/definitions/termsAndConditions.ts
+++ forms-shared/src/definitions/termsAndConditions.ts
@@ -1,3 +1,5 @@
 export const generalTermsAndConditions = `Stiahnutím, uložením a/alebo odoslaním Žiadosti výslovne vyhlasujem, že som sa oboznámil s [podmienkami spracúvania osobných údajov Hlavného mesta](https://bratislava.sk/konto/vyhlasenie-o-spracovani-osobnych-udajov) ako prevádzkovateľa osobných údajov a beriem na vedomie, že ich spracúvanie je nevyhnutné na splnenie zákonných povinností Hlavného mesta v súvislosti s vybavením tejto žiadosti.`
 
 export const taxTermsAndConditions = `Uložením a/alebo odoslaním daňového priznania výslovne vyhlasujem, že som sa oboznámil s [podmienkami spracúvania osobných údajov Hlavného mesta](https://bratislava.sk/konto/vyhlasenie-o-spracovani-osobnych-udajov) ako prevádzkovateľa osobných údajov a beriem na vedomie, že ich spracúvanie je nevyhnutné na správne vyrubenie dane a/ alebo splnenie ďalších súvisiacich zákonných povinností.`
+
+export const oloTermsAndConditions = `Vaše osobné údaje používame na vybavenie vašej požiadavky. Viac informácií nájdete na stránke [Ochrana osobných údajov](https://olo.sk/ochrana-osobnych-udajov).`

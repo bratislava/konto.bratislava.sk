e3b47cc263fbc9423d95d43d27bc16f8ac6be1f2
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2024-11-08 12:42:59 +0100

    OLO disable Hotjar (#1707)
    * Create GlobalAppProps
    
    * Don't display Hotjar on externally embedded sites
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

--- next/pages/mestske-sluzby/[slug]/[id].tsx
+++ next/pages/mestske-sluzby/[slug]/[id].tsx
@@ -15,6 +15,7 @@ import { getDefaultFormDataForFormDefinition } from '../../../frontend/utils/get
 import { getInitialFormSignature } from '../../../frontend/utils/getInitialFormSignature'
 import { redirectQueryParam } from '../../../frontend/utils/queryParamRedirect'
 import { slovakServerSideTranslations } from '../../../frontend/utils/slovakServerSideTranslations'
+import type { GlobalAppProps } from '../../_app'
 
 const fetchStrapiForm = async (slug: string): Promise<FormBaseFragment | null | undefined> => {
   const result = await strapiClient.FormBaseBySlug({ slug })
@@ -27,96 +28,100 @@ type Params = {
   id: string
 }
 
-export const getServerSideProps = amplifyGetServerSideProps<FormPageWrapperProps, Params>(
-  async ({ context, getAccessToken, isSignedIn }) => {
-    if (!context.params) {
+export const getServerSideProps = amplifyGetServerSideProps<
+  FormPageWrapperProps & GlobalAppProps,
+  Params
+>(async ({ context, getAccessToken, isSignedIn }) => {
+  if (!context.params) {
+    return { notFound: true }
+  }
+
+  const { slug, id: formId } = context.params
+  const formDefinition = getFormDefinitionBySlug(slug)
+  if (!formDefinition) {
+    return { notFound: true }
+  }
+
+  try {
+    // These promises cannot be run in parallel because the redirects in catch blocks depends on the error response of the first promise.
+    const { data: form } = await formsApi.nasesControllerGetForm(formId, {
+      accessToken: 'onlyAuthenticated',
+      accessTokenSsrGetFn: getAccessToken,
+    })
+    if (
+      !form ||
+      /* If there wouldn't be this check it would be possible to open the page with any slug in the URL. */
+      form.formDefinitionSlug !== slug
+    ) {
       return { notFound: true }
     }
 
-    const { slug, id: formId } = context.params
-    const formDefinition = getFormDefinitionBySlug(slug)
-    if (!formDefinition) {
-      return { notFound: true }
-    }
-
-    try {
-      // These promises cannot be run in parallel because the redirects in catch blocks depends on the error response of the first promise.
-      const { data: form } = await formsApi.nasesControllerGetForm(formId, {
+    const [{ data: files }, initialSignature, strapiForm] = await Promise.all([
+      formsApi.filesControllerGetFilesStatusByForm(formId, {
         accessToken: 'onlyAuthenticated',
         accessTokenSsrGetFn: getAccessToken,
-      })
-      if (
-        !form ||
-        /* If there wouldn't be this check it would be possible to open the page with any slug in the URL. */
-        form.formDefinitionSlug !== slug
-      ) {
-        return { notFound: true }
-      }
+      }),
+      getInitialFormSignature(form.formDataBase64),
+      fetchStrapiForm(slug),
+    ])
 
-      const [{ data: files }, initialSignature, strapiForm] = await Promise.all([
-        formsApi.filesControllerGetFilesStatusByForm(formId, {
-          accessToken: 'onlyAuthenticated',
-          accessTokenSsrGetFn: getAccessToken,
-        }),
-        getInitialFormSignature(form.formDataBase64),
-        fetchStrapiForm(slug),
-      ])
+    const formSent = form.state !== GetFormResponseDtoStateEnum.Draft
+    // If the form was created by an unauthenticated user, a migration modal is displayed and form is not editable.
+    const formMigrationRequired = Boolean(isSignedIn && !form.userExternalId)
 
-      const formSent = form.state !== GetFormResponseDtoStateEnum.Draft
-      // If the form was created by an unauthenticated user, a migration modal is displayed and form is not editable.
-      const formMigrationRequired = Boolean(isSignedIn && !form.userExternalId)
-
-      const { success: embeddedSuccess, isEmbedded } = handleEmbeddedFormRequest(
-        formDefinition,
-        context,
-      )
-      if (!embeddedSuccess) {
-        return { notFound: true }
-      }
-
-      return {
-        props: {
-          formServerContext: {
-            formDefinition: makeSerializableFormDefinition(formDefinition),
-            formId,
-            initialFormDataJson:
-              form.formDataJson ?? getDefaultFormDataForFormDefinition(formDefinition),
-            initialServerFiles: files,
-            initialSignature,
-            formSent,
-            formMigrationRequired,
-            isEmbedded,
-            strapiForm: strapiForm ?? null,
-          },
-          ...(await slovakServerSideTranslations()),
-        } satisfies FormPageWrapperProps,
-      }
-    } catch (error) {
-      if (isAxiosError(error)) {
-        const is401 = error.response?.status === 401
-        const is403 = error.response?.status === 403
-        const is404 = error.response?.status === 404
-
-        // If logged in user receives 403 for his/her form it's not theirs.
-        if (is404 || (is403 && isSignedIn)) {
-          return { notFound: true }
-        }
-        // If logged out user receives 403 for his/her form it might be theirs.
-        if (is401 || (is403 && !isSignedIn)) {
-          return {
-            redirect: {
-              destination: `${ROUTES.LOGIN}?${redirectQueryParam}=${encodeURIComponent(
-                context.resolvedUrl,
-              )}`,
-              permanent: false,
-            },
-          }
-        }
-      }
-
-      throw error
+    const { success: embeddedSuccess, isEmbedded } = handleEmbeddedFormRequest(
+      formDefinition,
+      context,
+    )
+    if (!embeddedSuccess) {
+      return { notFound: true }
     }
-  },
-)
+
+    return {
+      props: {
+        formServerContext: {
+          formDefinition: makeSerializableFormDefinition(formDefinition),
+          formId,
+          initialFormDataJson:
+            form.formDataJson ?? getDefaultFormDataForFormDefinition(formDefinition),
+          initialServerFiles: files,
+          initialSignature,
+          formSent,
+          formMigrationRequired,
+          isEmbedded,
+          strapiForm: strapiForm ?? null,
+        },
+        appProps: {
+          externallyEmbedded: isEmbedded,
+        },
+        ...(await slovakServerSideTranslations()),
+      },
+    }
+  } catch (error) {
+    if (isAxiosError(error)) {
+      const is401 = error.response?.status === 401
+      const is403 = error.response?.status === 403
+      const is404 = error.response?.status === 404
+
+      // If logged in user receives 403 for his/her form it's not theirs.
+      if (is404 || (is403 && isSignedIn)) {
+        return { notFound: true }
+      }
+      // If logged out user receives 403 for his/her form it might be theirs.
+      if (is401 || (is403 && !isSignedIn)) {
+        return {
+          redirect: {
+            destination: `${ROUTES.LOGIN}?${redirectQueryParam}=${encodeURIComponent(
+              context.resolvedUrl,
+            )}`,
+            permanent: false,
+          },
+        }
+      }
+    }
+
+    throw error
+  }
+})
 
 export default SsrAuthProviderHOC(FormPageWrapper)

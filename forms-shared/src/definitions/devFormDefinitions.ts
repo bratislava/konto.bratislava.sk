f2d365630d5c94eb6532fee0f14a6f4ed02bb3bd
Marek Bodinger <marek.bodinger@gmail.com>
2024-11-13 13:17:45 +0100

    Showcase form

--- forms-shared/src/definitions/devFormDefinitions.ts
+++ forms-shared/src/definitions/devFormDefinitions.ts
@@ -1,3 +1,23 @@
-import { FormDefinition } from './formDefinitionTypes'
+import { FormDefinition, FormDefinitionType } from './formDefinitionTypes'
+import { generalTermsAndConditions } from './termsAndConditions'
+import showcase from '../schemas/showcase'
 
-export const devFormDefinitions: FormDefinition[] = []
+export const devFormDefinitions: FormDefinition[] = [
+  {
+    type: FormDefinitionType.SlovenskoSkGeneric,
+    slug: 'showcase',
+    title: 'Showcase',
+    schemas: showcase,
+    pospID: '',
+    pospVersion: '',
+    publisher: '',
+    gestor: '',
+    termsAndConditions: generalTermsAndConditions,
+    messageSubjectDefault: 'Showcase',
+    ginisAssignment: {
+      ginisOrganizationName: '',
+      ginisPersonName: '',
+    },
+    isSigned: false,
+  },
+]

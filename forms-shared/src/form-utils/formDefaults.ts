42ea6cf42861a895940a08db37feb5bf16f034a7
Marek Bodinger <marek.bodinger@gmail.com>
2024-12-06 16:24:38 +0100

    Create defaultUiSchema

--- forms-shared/src/form-utils/formDefaults.ts
+++ forms-shared/src/form-utils/formDefaults.ts
@@ -4,6 +4,12 @@ import { BAJSONSchema7 } from './ajvKeywords'
 import { BaRjsfValidatorRegistry } from './validatorRegistry'
 
 /**
+ * This is a default UI schema for all user-facing forms. All the UI schemas are now in the schema itself.
+ * As we handle displaying errors in our own way, we hide the default error messages.
+ */
+export const defaultUiSchema = { 'ui:options': { hideError: true } }
+
+/**
  * Default RJSF props that should be used for all forms to work consistently.
  */
 export const getBaFormDefaults = (

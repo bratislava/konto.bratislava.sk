7dfc7ffb7f1bbedbef319384348655aa9871ad9f
Jakub Hvolka <63741979+JHvolka@users.noreply.github.com>
2024-12-19 10:19:35 +0100

    Delete rfo table from database (#2031)
    * delete or move rfo-by-birthnumber files
    
    * remove rfo from database schema
    
    * migration: remove RfoByBirthnumber from DB
    
    * refactor: replace RfoByBirthnumber.create() by MagproxyService.rfoBirthNumberList()
    
    * refactor: move dto to magproxy
    
    * refactor: remove rfo updater (cron job)

--- nest-city-account/src/physical-entity/physical-entity.module.ts
+++ nest-city-account/src/physical-entity/physical-entity.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/prisma/prisma.module'
 
-import { RfoByBirthnumberModule } from 'src/rfo-by-birthnumber/rfo-by-birthnumber.module'
 import { UpvsIdentityByUriModule } from 'src/upvs-identity-by-uri/upvs-identity-by-uri.module'
 import ThrowerErrorGuard from '../utils/guards/errors.guard'
 import { PhysicalEntityService } from './physical-entity.service'
 import { MagproxyModule } from '../magproxy/magproxy.module'
-import { PhysicalEntityCronSubservice } from './subservices/physical-entity-cron.subservice'
 
 @Module({
-  imports: [PrismaModule, RfoByBirthnumberModule, UpvsIdentityByUriModule, MagproxyModule],
-  providers: [PhysicalEntityService, ThrowerErrorGuard, PhysicalEntityCronSubservice],
+  imports: [PrismaModule, UpvsIdentityByUriModule, MagproxyModule],
+  providers: [PhysicalEntityService, ThrowerErrorGuard],
   exports: [PhysicalEntityService],
   controllers: [],
 })

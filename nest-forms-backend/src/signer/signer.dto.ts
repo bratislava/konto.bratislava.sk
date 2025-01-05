083257d173a4390e9e6cfd87de4bc1ea44c33c1a
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-01-05 17:50:12 +0100

    Create new SignerModule that returns signer data for all forms instead of TaxModule

---
+++ nest-forms-backend/src/signer/signer.dto.ts
@@ -0,0 +1,106 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Prisma } from '@prisma/client'
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator'
+
+import { JSON_FORM_EXAMPLE } from '../utils/constants'
+
+export class SignerDataRequestDto {
+  @ApiProperty({
+    description: 'Form id',
+    example: 'f69559da-5eca-4ed7-80fd-370d09dc3632',
+  })
+  @IsUUID()
+  formId: string
+
+  @IsObject()
+  @ApiProperty({
+    description: 'Form values in JSON',
+    example: JSON_FORM_EXAMPLE,
+  })
+  @IsNotEmpty()
+  @IsOptional()
+  formDataJson!: Prisma.JsonObject
+}
+
+/**
+ * Response DTO for signer data. The implementation and generation of these values can be found in:
+ * - forms-shared/src/signer/signerData.ts
+ */
+export class SignerDataResponseDto {
+  @ApiProperty()
+  @IsString()
+  signatureId!: string
+
+  @ApiProperty({ example: 'Vseobecna_agenda.xml' })
+  @IsString()
+  objectId!: string
+
+  @ApiProperty({ example: 'Všeobecná agenda' })
+  @IsString()
+  objectDescription!: string
+
+  @ApiProperty({ example: 'http://schemas.gov.sk/form/Doc.GeneralAgenda/1.2' })
+  @IsString()
+  objectFormatIdentifier!: string
+
+  @ApiProperty()
+  @IsString()
+  xdcXMLData!: string
+
+  @ApiProperty({
+    example: 'http://data.gov.sk/doc/eform/Doc.GeneralAgenda/1.2',
+  })
+  @IsString()
+  xdcIdentifier!: string
+
+  @ApiProperty({ example: '1.2' })
+  @IsString()
+  xdcVersion!: string
+
+  @ApiProperty()
+  @IsString()
+  xdcUsedXSD!: string
+
+  @ApiProperty({ example: 'http://schemas.gov.sk/form/Doc.GeneralAgenda/1.2' })
+  @IsString()
+  xsdReferenceURI!: string
+
+  @ApiProperty()
+  @IsString()
+  xdcUsedXSLT!: string
+
+  @ApiProperty({
+    example: 'http://schemas.gov.sk/form/Doc.GeneralAgenda/1.2/form.xslt',
+  })
+  @IsString()
+  xslReferenceURI!: string
+
+  @ApiProperty({ example: 'HTML' })
+  @IsString()
+  xslMediaDestinationTypeDescription!: string
+
+  @ApiProperty({ example: 'sk' })
+  @IsString()
+  xslXSLTLanguage!: string
+
+  @ApiProperty({ example: '' })
+  @IsString()
+  xslTargetEnvironment!: string
+
+  @ApiProperty({ example: true })
+  @IsBoolean()
+  xdcIncludeRefs!: boolean
+
+  @ApiProperty({
+    example: 'http://data.gov.sk/def/container/xmldatacontainer+xml/1.1',
+  })
+  @IsString()
+  xdcNamespaceURI!: string
+}
---
+++ nest-forms-backend/src/signer/signer.service.ts
@@ -0,0 +1,97 @@
+import { Injectable, Logger } from '@nestjs/common'
+import { GenericObjectType } from '@rjsf/utils'
+import { isSlovenskoSkFormDefinition } from 'forms-shared/definitions/formDefinitionTypes'
+import { getFormDefinitionBySlug } from 'forms-shared/definitions/getFormDefinitionBySlug'
+import { getSignerData } from 'forms-shared/signer/signerData'
+import validateSchema from 'xsd-validator'
+
+import { CognitoGetUserData } from '../auth/dtos/cognito.dto'
+import FormValidatorRegistryService from '../form-validator-registry/form-validator-registry.service'
+import {
+  FormsErrorsEnum,
+  FormsErrorsResponseEnum,
+} from '../forms/forms.errors.enum'
+import FormsService from '../forms/forms.service'
+import PrismaService from '../prisma/prisma.service'
+import ThrowerErrorGuard from '../utils/guards/thrower-error.guard'
+import { FormWithFiles } from '../utils/types/prisma'
+import { SignerDataRequestDto, SignerDataResponseDto } from './signer.dto'
+import {
+  SignerErrorsEnum,
+  SignerErrorsResponseEnum,
+} from './signer.errors.enum'
+
+@Injectable()
+export default class SignerService {
+  private readonly logger: Logger
+
+  constructor(
+    private readonly throwerErrorGuard: ThrowerErrorGuard,
+    private readonly formsService: FormsService,
+    private readonly prismaService: PrismaService,
+    private readonly formValidatorRegistryService: FormValidatorRegistryService,
+  ) {
+    this.logger = new Logger('SignerService')
+  }
+
+  private validateXml(xmlData: string, xsd: string): void {
+    const result = validateSchema(xmlData, xsd)
+    if (result === true) {
+      return
+    }
+
+    throw this.throwerErrorGuard.BadRequestException(
+      SignerErrorsEnum.XML_VALIDATION_ERROR,
+      `${SignerErrorsResponseEnum.XML_VALIDATION_ERROR} Errors: ${result
+        .map((e) => e.message)
+        .join(', ')}`,
+    )
+  }
+
+  async getSignerData(
+    data: SignerDataRequestDto,
+    ico: string | null,
+    user?: CognitoGetUserData,
+  ): Promise<SignerDataResponseDto> {
+    const form = await this.formsService.getFormWithAccessCheck(
+      data.formId,
+      user?.sub ?? null,
+      ico,
+    )
+
+    const formDefinition = getFormDefinitionBySlug(form.formDefinitionSlug)
+    if (formDefinition === null) {
+      throw this.throwerErrorGuard.NotFoundException(
+        FormsErrorsEnum.FORM_DEFINITION_NOT_FOUND,
+        `getSignerData: ${FormsErrorsResponseEnum.FORM_DEFINITION_NOT_FOUND} ${form.formDefinitionSlug}`,
+      )
+    }
+    if (!isSlovenskoSkFormDefinition(formDefinition)) {
+      throw this.throwerErrorGuard.UnprocessableEntityException(
+        FormsErrorsEnum.FORM_DEFINITION_NOT_SUPPORTED_TYPE,
+        `getSignerData: ${FormsErrorsResponseEnum.FORM_DEFINITION_NOT_SUPPORTED_TYPE}: ${formDefinition.type}, slug: ${form.formDefinitionSlug}`,
+      )
+    }
+
+    const formWithFiles = (await this.prismaService.forms.findUnique({
+      where: {
+        id: form.id,
+      },
+      include: {
+        files: true,
+      },
+    })) as FormWithFiles
+
+    const signerData = await getSignerData({
+      formDefinition,
+      formId: data.formId,
+      formData: data.formDataJson as GenericObjectType,
+      validatorRegistry: this.formValidatorRegistryService.getRegistry(),
+      serverFiles: formWithFiles.files,
+    })
+
+    this.validateXml(signerData.xdcXMLData, signerData.xdcUsedXSD)
+
+    return signerData
+  }
+}

ef1db8075f9a9a4e898a3f02fe81a3451bd94696
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-01-23 11:03:53 +0100

    Create tests createFormSignature and verifyFormSignature

---
+++ forms-shared/tests/signer/signature.ts
@@ -0,0 +1,101 @@
+import {
+  FormDefinitionSlovenskoSk,
+  FormDefinitionType,
+} from '../../src/definitions/formDefinitionTypes'
+import {
+  createFormSignature,
+  verifyFormSignature,
+  VerifyFormSignatureError,
+  VerifyFormSignatureErrorType,
+} from '../../src/signer/signature'
+
+describe('signature', () => {
+  const mockFormDefinition = {
+    type: FormDefinitionType.SlovenskoSkGeneric,
+    pospID: 'test.form',
+    pospVersion: '1.0',
+    jsonVersion: '1.0',
+  } as FormDefinitionSlovenskoSk
+
+  const mockFormData = {
+    field1: 'value1',
+    field2: 'value2',
+  }
+
+  const mockFormDataHash = '47e22393eae489a887dc05e62b1bf8fc5285f869'
+
+  describe('createFormSignature', () => {
+    it('should create a valid form signature', () => {
+      const signatureBase64 = 'test-signature'
+      const result = createFormSignature(mockFormDefinition, signatureBase64, mockFormData)
+
+      expect(result).toEqual({
+        signatureBase64,
+        pospID: mockFormDefinition.pospID,
+        pospVersion: mockFormDefinition.pospVersion,
+        jsonVersion: mockFormDefinition.jsonVersion,
+        formDataHash: mockFormDataHash,
+      })
+    })
+  })
+
+  describe('verifyFormSignature', () => {
+    const validSignature = {
+      signatureBase64: 'test-signature',
+      pospID: mockFormDefinition.pospID,
+      pospVersion: mockFormDefinition.pospVersion,
+      jsonVersion: mockFormDefinition.jsonVersion,
+      formDataHash: mockFormDataHash,
+    }
+
+    it('should not throw for valid signature', () => {
+      expect(() => {
+        verifyFormSignature(mockFormDefinition, mockFormData, validSignature)
+      }).not.toThrow()
+    })
+
+    it('should throw FormDefinitionMismatch when pospID differs', () => {
+      const invalidSignature = {
+        ...validSignature,
+        pospID: 'different.form',
+      }
+
+      expect(() => {
+        verifyFormSignature(mockFormDefinition, mockFormData, invalidSignature)
+      }).toThrow(new VerifyFormSignatureError(VerifyFormSignatureErrorType.FormDefinitionMismatch))
+    })
+
+    it('should throw FormDefinitionMismatch when pospVersion differs', () => {
+      const invalidSignature = {
+        ...validSignature,
+        pospVersion: '2.0',
+      }
+
+      expect(() => {
+        verifyFormSignature(mockFormDefinition, mockFormData, invalidSignature)
+      }).toThrow(new VerifyFormSignatureError(VerifyFormSignatureErrorType.FormDefinitionMismatch))
+    })
+
+    it('should throw FormDefinitionMismatch when jsonVersion differs', () => {
+      const invalidSignature = {
+        ...validSignature,
+        jsonVersion: '2.0',
+      }
+
+      expect(() => {
+        verifyFormSignature(mockFormDefinition, mockFormData, invalidSignature)
+      }).toThrow(new VerifyFormSignatureError(VerifyFormSignatureErrorType.FormDefinitionMismatch))
+    })
+
+    it('should throw FormDataHashMismatch when form data has been modified', () => {
+      const modifiedFormData = {
+        ...mockFormData,
+        field1: 'modified value',
+      }
+
+      expect(() => {
+        verifyFormSignature(mockFormDefinition, modifiedFormData, validSignature)
+      }).toThrow(new VerifyFormSignatureError(VerifyFormSignatureErrorType.FormDataHashMismatch))
+    })
+  })
+})

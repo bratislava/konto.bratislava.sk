98078d4d2217196836e44a2922ff87619fb13472
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2025-01-28 13:07:00 +0100

    Redirect to ?krok=sumar after successful eID login (#2181)

--- next/pages/nases/login.tsx
+++ next/pages/nases/login.tsx
@@ -3,8 +3,10 @@ import { useRouter } from 'next/router'
 import { useRef } from 'react'
 import { useEffectOnce } from 'usehooks-ts'
 
+import { STEP_QUERY_PARAM_KEY } from '../../components/forms/useFormCurrentStepIndex'
 import { ROUTES } from '../../frontend/api/constants'
 import { FORM_SEND_EID_TOKEN_QUERY_KEY, popSendEidMetadata } from '../../frontend/utils/formSend'
+import { STEP_QUERY_PARAM_VALUE_SUMMARY } from '../../frontend/utils/formState'
 
 export const getServerSideProps: GetServerSideProps = async () => {
   return { props: {} }
@@ -29,9 +31,12 @@ const NasesLoginPage = () => {
     }
 
     const { token } = router.query
-    const query = typeof token === 'string' ? { [FORM_SEND_EID_TOKEN_QUERY_KEY]: token } : {}
+    const query = {
+      [STEP_QUERY_PARAM_KEY]: STEP_QUERY_PARAM_VALUE_SUMMARY,
+      ...(typeof token === 'string' ? { [FORM_SEND_EID_TOKEN_QUERY_KEY]: token } : {}),
+    }
 
-    const url = `${ROUTES.MUNICIPAL_SERVICES}/${metadata.formSlug}/${metadata.formId}`
+    const url = ROUTES.MUNICIPAL_SERVICES_FORM_WITH_ID(metadata.formSlug, metadata.formId)
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     router.push(
       {

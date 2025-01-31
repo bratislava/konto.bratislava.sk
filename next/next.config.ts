9f483ca8be1e7e24411fd428f1a2689018237834
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-01-31 16:44:01 +0100

    Migrate next.config to TypeScript (newly supported in Next 15)

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,11 +1,12 @@
-const { withPlausibleProxy } = require('next-plausible')
-const { i18n } = require('./next-i18next.config')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { join } = require('node:path')
-const fs = require('node:fs')
-const path = require('node:path')
+import { withPlausibleProxy } from 'next-plausible'
+import { i18n } from './next-i18next.config'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import path, { join } from 'node:path'
+import fs from 'node:fs'
+import type { NextConfig } from 'next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
+const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
@@ -17,10 +18,7 @@ const iframeResizerPublicPath = (() => {
   return `/scripts/iframe-resizer-child-${version}.js`
 })()
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+const nextConfig: NextConfig = {
   env: {
     IFRAME_RESIZER_PUBLIC_PATH: iframeResizerPublicPath,
   },
@@ -175,11 +173,6 @@ const nextConfig = {
     // https://github.com/konvajs/konva/issues/1458#issuecomment-1356122802
     config.externals = [...config.externals, { canvas: 'canvas' }]
 
-    // In `forms-shared` we have React, and it acts as a duplicate of the React in the app in local development
-    // (because the package is symlinked).
-    // It causes the error: `You might have more than one copy of React in the same app`.
-    // This is a temporary solution until the packages are installed using npm workspaces.
-    // https://blog.elantha.com/more-than-one-copy-of-react-in-the-same-app/
     config.resolve.alias = {
       ...config.resolve.alias,
       react: path.resolve('./node_modules/react'),
@@ -191,6 +184,6 @@ const nextConfig = {
 }
 
 // https://github.com/4lejandrito/next-plausible#proxy-the-analytics-script
-module.exports = withPlausibleProxy()({
-  ...withBundleAnalyzer(nextConfig),
+export default withPlausibleProxy()({
+  ...bundleAnalyzer(nextConfig),
 })

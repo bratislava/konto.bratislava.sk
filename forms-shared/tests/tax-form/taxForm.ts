3b1b2028415a4af118c78d9ccf7f0a26e955cabb
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-02-03 13:04:53 +0100

    Fix tax form timers

--- forms-shared/tests/tax-form/taxForm.ts
+++ forms-shared/tests/tax-form/taxForm.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeAll, afterAll, vi } from 'vitest'
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
 import { getTaxFormPdfMapping } from '../../src/tax-form/mapping/pdf/pdf'
 import { generateTaxPdf } from '../../src/tax-form/generateTaxPdf'
 import { generateTaxXml } from '../../src/tax-form/generateTaxXml'
@@ -13,46 +13,58 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot'
 expect.extend({ toMatchImageSnapshot })
 
 describe('tax-form', () => {
-  beforeAll(() => {
-    // Without `shouldAdvanceTime` the PDF generation would hang indefinitely.
-    vi.useFakeTimers({ now: new Date('2024-01-01'), shouldAdvanceTime: true })
-  })
-
-  afterAll(() => {
-    vi.useRealTimers()
-  })
-
   getExampleFormPairs({ formDefinitionFilterFn: isSlovenskoSkTaxFormDefinition }).forEach(
     ({ exampleForm }) => {
-      test(`should return correct PDF mapping for ${exampleForm.name}`, () => {
-        expect(getTaxFormPdfMapping(exampleForm.formData, undefined)).toMatchSnapshot()
+      describe('PDF', () => {
+        beforeEach(() => {
+          // Without `shouldAdvanceTime` the PDF generation would hang indefinitely.
+          vi.useFakeTimers({ now: new Date('2024-01-01'), shouldAdvanceTime: true })
+        })
+
+        afterEach(() => {
+          vi.useRealTimers()
+        })
+
+        test(`should return correct PDF mapping for ${exampleForm.name}`, () => {
+          expect(getTaxFormPdfMapping(exampleForm.formData, undefined)).toMatchSnapshot()
+        })
+
+        test(
+          `should match snapshot for generated PDF ${exampleForm.name}`,
+          async () => {
+            filterConsole(
+              'log',
+              (message) =>
+                message ===
+                'Warning: _getAppearance: OffscreenCanvas is not supported, annotation may not render correctly.',
+            )
+
+            const base64Pdf = await generateTaxPdf({ formData: exampleForm.formData })
+
+            await expectPdfToMatchSnapshot(`data:application/pdf;base64,${base64Pdf}`)
+          },
+          screenshotTestTimeout,
+        )
       })
 
-      test(`should return correct XML for ${exampleForm.name}`, () => {
-        const formDefinition = getFormDefinitionBySlug('priznanie-k-dani-z-nehnutelnosti')
-        if (!formDefinition || !isSlovenskoSkTaxFormDefinition(formDefinition)) {
-          throw new Error('Form definition not found')
-        }
+      describe('XML', () => {
+        beforeEach(() => {
+          vi.useFakeTimers({ now: new Date('2024-01-01') })
+        })
 
-        expect(generateTaxXml(exampleForm.formData, true, formDefinition)).toMatchSnapshot()
+        afterEach(() => {
+          vi.useRealTimers()
+        })
+
+        test(`should return correct XML for ${exampleForm.name}`, () => {
+          const formDefinition = getFormDefinitionBySlug('priznanie-k-dani-z-nehnutelnosti')
+          if (!formDefinition || !isSlovenskoSkTaxFormDefinition(formDefinition)) {
+            throw new Error('Form definition not found')
+          }
+
+          expect(generateTaxXml(exampleForm.formData, true, formDefinition)).toMatchSnapshot()
+        })
       })
-
-      test(
-        `should match snapshot for generated PDF ${exampleForm.name}`,
-        async () => {
-          filterConsole(
-            'log',
-            (message) =>
-              message ===
-              'Warning: _getAppearance: OffscreenCanvas is not supported, annotation may not render correctly.',
-          )
-
-          const base64Pdf = await generateTaxPdf({ formData: exampleForm.formData })
-
-          await expectPdfToMatchSnapshot(`data:application/pdf;base64,${base64Pdf}`)
-        },
-        screenshotTestTimeout,
-      )
     },
   )
 })

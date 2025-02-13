ef726739e92461c2540fc4e1f156ddbf2cef4823
Erik Rehulka <erik.rehulka@bratislava.sk>
2025-02-13 10:00:42 +0100

    fix the tax deadlien

--- nest-city-account/src/utils/constants/__tests__/tax-deadline.spec.ts
+++ nest-city-account/src/utils/constants/__tests__/tax-deadline.spec.ts
@@ -3,20 +3,20 @@ import { getTaxDeadlineDateString } from '../tax-deadline'
 describe('tax-deadline', () => {
   it('should return the correct tax deadline for the given year', () => {
     const year = new Date().getFullYear()
-    expect(getTaxDeadlineDateString()).toBe(`${year}-03-02 00:00:00`)
+    expect(getTaxDeadlineDateString()).toBe(`${year}-04-01 00:00:00`)
   })
 
   it('each date from 1st of March should be before deadline, each date from 2nd should be after', () => {
     const year = new Date().getFullYear()
     const afterDeadline = [
-      new Date(`${year}-03-02 00:00:10`),
-      new Date(`${year}-03-02 00:00:00`),
-      new Date(`${year}-03-03 10:00:00`),
+      new Date(`${year}-04-01 00:00:10`),
+      new Date(`${year}-04-01 00:00:00`),
+      new Date(`${year}-04-01 10:00:00`),
     ]
     const beforeDeadline = [
-      new Date(`${year}-03-01 00:00:10`),
-      new Date(`${year}-03-01 23:59:59`),
-      new Date(`${year}-02-03 10:00:00`),
+      new Date(`${year}-03-31 00:00:10`),
+      new Date(`${year}-03-31 23:59:59`),
+      new Date(`${year}-01-04 10:00:00`),
     ]
     afterDeadline.forEach((date) => {
       expect(date < new Date(getTaxDeadlineDateString())).toBe(false)
--- nest-city-account/src/utils/constants/tax-deadline.ts
+++ nest-city-account/src/utils/constants/tax-deadline.ts
@@ -1,4 +1,4 @@
 export function getTaxDeadlineDateString(): string {
   const year = new Date().getFullYear()
-  return `${year}-03-02 00:00:00`
+  return `${year}-04-01 00:00:00`
 }

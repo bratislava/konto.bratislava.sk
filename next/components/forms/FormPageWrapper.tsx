323c5e2ec4d2cf49fbe0f9ede15817bdaea19972
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-02-19 15:37:22 +0100

    Rework FormContextProvider to be wrapped around whole FormPageWrapper

--- next/components/forms/FormPageWrapper.tsx
+++ next/components/forms/FormPageWrapper.tsx
@@ -9,12 +9,8 @@ import FormProviders from './FormProviders'
 import IframeResizerChild from './IframeResizerChild'
 import ThankYouFormSection from './segments/AccountSections/ThankYouSection/ThankYouFormSection'
 import ConditionalWrap from './simple-components/ConditionalWrap'
-import { FormContextProvider, FormServerContext } from './useFormContext'
+import { FormContextProvider, FormServerContext, useFormContext } from './useFormContext'
 import { FormSentRenderer } from './useFormSent'
-
-export type FormPageWrapperProps = {
-  formServerContext: FormServerContext
-}
 
 // custom plausible tracking - we exclude '/mestske-sluzby/*/*' in top level plausible provider
 // instead, we track these as custom events: formSlug#hash
@@ -42,18 +38,18 @@ const useCustomPlausibleFormPagesTracking = (formSlug: string) => {
   })
 }
 
-const FormPageWrapper = ({ formServerContext }: FormPageWrapperProps) => {
-  const { formDefinition, isEmbedded } = formServerContext
+const FormPageWrapper = () => {
+  const { formDefinition, isEmbedded } = useFormContext()
   useCustomPlausibleFormPagesTracking(formDefinition.slug)
 
   return (
-    <IframeResizerChild enabled={formServerContext.isEmbedded}>
+    <IframeResizerChild enabled={isEmbedded}>
       <FormSentRenderer
         // TODO today it does not make sense to have anything else than false in initialFormSent - otherwise we just display "thank you" page on each revisit
         // if it stays this way remove the prop completely
         initialFormSent={false}
         notSentChildren={
-          <FormProviders formServerContext={formServerContext}>
+          <FormProviders>
             <ConditionalWrap
               condition={!isEmbedded}
               wrap={(children) => <AccountPageLayout>{children}</AccountPageLayout>}
@@ -63,20 +59,30 @@ const FormPageWrapper = ({ formServerContext }: FormPageWrapperProps) => {
           </FormProviders>
         }
         sentChildren={
-          <FormContextProvider formServerContext={formServerContext}>
-            <ConditionalWrap
-              condition={!isEmbedded}
-              wrap={(children) => (
-                <AccountPageLayout className="bg-gray-50">{children}</AccountPageLayout>
-              )}
-            >
-              <ThankYouFormSection />
-            </ConditionalWrap>
-          </FormContextProvider>
+          <ConditionalWrap
+            condition={!isEmbedded}
+            wrap={(children) => (
+              <AccountPageLayout className="bg-gray-50">{children}</AccountPageLayout>
+            )}
+          >
+            <ThankYouFormSection />
+          </ConditionalWrap>
         }
       />
     </IframeResizerChild>
   )
 }
 
-export default FormPageWrapper
+export type FormPageWrapperWithContextProps = {
+  formServerContext: FormServerContext
+}
+
+const FormPageWrapperWithContext = ({ formServerContext }: FormPageWrapperWithContextProps) => {
+  return (
+    <FormContextProvider formServerContext={formServerContext}>
+      <FormPageWrapper />
+    </FormContextProvider>
+  )
+}
+
+export default FormPageWrapperWithContext

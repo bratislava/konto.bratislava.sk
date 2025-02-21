e571f3790aad0b1b97793d66277f0bc41f8b87fe
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-02-21 15:13:34 +0100

    Remove useCustomPlausibleFormPagesTracking as form steps are not implemented with hashes anymore

--- next/components/forms/FormPageWrapper.tsx
+++ next/components/forms/FormPageWrapper.tsx
@@ -1,8 +1,5 @@
 import { VersionCompareContinueAction } from 'forms-shared/versioning/version-compare'
-import { useRouter } from 'next/router'
-import { usePlausible } from 'next-plausible'
-import React, { useEffect } from 'react'
-import { useEffectOnce } from 'usehooks-ts'
+import React from 'react'
 
 import AccountPageLayout from '../layouts/AccountPageLayout'
 import FormPage from './FormPage'
@@ -13,32 +10,6 @@ import ThankYouFormSection from './segments/AccountSections/ThankYouSection/Than
 import ConditionalWrap from './simple-components/ConditionalWrap'
 import { FormContextProvider, FormServerContext, useFormContext } from './useFormContext'
 import { FormSentRenderer } from './useFormSent'
-
-// custom plausible tracking - we exclude '/mestske-sluzby/*/*' in top level plausible provider
-// instead, we track these as custom events: formSlug#hash
-const useCustomPlausibleFormPagesTracking = (formSlug: string) => {
-  const router = useRouter()
-  const plausible = usePlausible()
-
-  useEffect(() => {
-    const onHashChangeStart = (url: string) => {
-      const hash = url.split('#')[1]
-      plausible(`${formSlug}#${hash}`)
-    }
-
-    router.events.on('hashChangeStart', onHashChangeStart)
-
-    return () => {
-      router.events.off('hashChangeStart', onHashChangeStart)
-    }
-  }, [formSlug, plausible, router.events])
-
-  // track initial pageview
-  useEffectOnce(() => {
-    const hash = router.asPath.split('#')[1]
-    plausible(`${formSlug}#${hash}`)
-  })
-}
 
 const FormPageWrapper = () => {
   const { isEmbedded, versionCompareContinueAction } = useFormContext()

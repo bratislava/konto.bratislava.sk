3f54853f18de714f9e0ef1bd7e69fecd5546becb
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-02-25 17:56:08 +0100

    Remove legacy ResponseGdprDataDto

--- nest-forms-backend/src/nases/dtos/responses.dto.ts
+++ nest-forms-backend/src/nases/dtos/responses.dto.ts
@@ -1,7 +1,7 @@
 /* eslint-disable pii/no-phone-number */
 /* eslint-disable pii/no-email */
 import { HttpStatus } from '@nestjs/common'
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsBoolean, IsUUID } from 'class-validator'
 
 import { GetFormResponseDto } from './requests.dto'
@@ -9,51 +9,6 @@ import { GetFormResponseDto } from './requests.dto'
 export interface NasesSendResponse {
   status: HttpStatus
   data: unknown
-}
-
-export class ResponseGdprDataDto {
-  @ApiProperty({
-    description: 'Local ID of user',
-    default: '133e0473-44da-407a-b24f-12da343e808d',
-  })
-  declare id: string
-
-  @ApiProperty({
-    description: 'Created timestamp',
-    default: '2023-02-10T10:31:49.247Z',
-  })
-  declare createdAt: Date
-
-  @ApiProperty({
-    description: 'Last updated timestamp',
-    default: '2023-02-10T10:31:49.247Z',
-  })
-  declare updatedAt: Date
-
-  @ApiPropertyOptional({
-    description:
-      'Id from cognito, it is not required. We can have also only subscribed user, who are not city account users',
-    default: 'e51754f2-3367-43f6-b9bc-b5c6131b041a',
-  })
-  declare externalId?: string
-
-  @ApiProperty({
-    description: 'Email',
-    default: 'test@bratislava.sk',
-  })
-  declare email: string
-
-  @ApiPropertyOptional({
-    description: 'Ico of company, which this user represents',
-    default: '000000',
-  })
-  ico?: string
-
-  @ApiProperty({
-    description: 'Birth number',
-    default: '9909090000',
-  })
-  declare birthNumber: string | null
 }
 
 export class CanSendResponseDto {

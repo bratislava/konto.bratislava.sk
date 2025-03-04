572ebb272eef36c68468395f3a40b24eae6893df
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-03-04 13:37:49 +0100

    Implement twMerge in legacy button (it broke with new Tailwind because the classes priority is different)

--- next/components/forms/simple-components/Button.tsx
+++ next/components/forms/simple-components/Button.tsx
@@ -7,6 +7,7 @@ import { AriaButtonProps, useButton } from 'react-aria'
 
 import MLink from './MLink'
 import Spinner from './Spinner'
+import { twMerge } from 'tailwind-merge'
 
 type ButtonBase = {
   variant?:
@@ -89,158 +90,161 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProp
       ref,
     )
 
-    const style = cx(
-      'inline-flex items-center',
-      rest.href
-        ? 'underline underline-offset-4 focus-visible:outline-hidden'
-        : 'h-fit justify-center rounded-lg text-center align-middle focus:outline-hidden',
-      className,
-      {
-        'w-full': fullWidth,
-        'w-fit': !fullWidth,
-      },
-      {
-        // text for lg button
-        'text-16-semibold px-4 py-2.5':
-          size === 'lg' &&
-          !icon &&
-          text &&
-          (variant === 'black' ||
-            variant === 'negative' ||
-            variant === 'black-outline' ||
-            variant === 'category' ||
-            variant === 'category-outline'),
-        // text for sm button
-        'text-16-semibold px-4 py-1.5':
-          size === 'sm' &&
-          !icon &&
-          text &&
-          (variant === 'black' ||
-            variant === 'negative' ||
-            variant === 'black-outline' ||
-            variant === 'category' ||
-            variant === 'category-outline'),
-        // icon for lg button
-        'px-2.5 py-2.5':
-          size === 'lg' &&
-          icon &&
-          !text &&
-          (variant === 'black' ||
-            variant === 'negative' ||
-            variant === 'black-outline' ||
-            variant === 'category' ||
-            variant === 'category-outline'),
-        // icon for sm button
-        'px-2 py-2':
-          (size === 'sm' &&
+    const style = twMerge(
+      cx(
+        'inline-flex items-center',
+        rest.href
+          ? 'underline underline-offset-4 focus-visible:outline-hidden'
+          : 'h-fit justify-center rounded-lg text-center align-middle focus:outline-hidden',
+        {
+          'w-full': fullWidth,
+          'w-fit': !fullWidth,
+        },
+        {
+          // text for lg button
+          'text-16-semibold px-4 py-2.5':
+            size === 'lg' &&
+            !icon &&
+            text &&
+            (variant === 'black' ||
+              variant === 'negative' ||
+              variant === 'black-outline' ||
+              variant === 'category' ||
+              variant === 'category-outline'),
+          // text for sm button
+          'text-16-semibold px-4 py-1.5':
+            size === 'sm' &&
+            !icon &&
+            text &&
+            (variant === 'black' ||
+              variant === 'negative' ||
+              variant === 'black-outline' ||
+              variant === 'category' ||
+              variant === 'category-outline'),
+          // icon for lg button
+          'px-2.5 py-2.5':
+            size === 'lg' &&
             icon &&
             !text &&
             (variant === 'black' ||
               variant === 'negative' ||
               variant === 'black-outline' ||
               variant === 'category' ||
-              variant === 'category-outline')) ||
-          (size === 'lg' &&
+              variant === 'category-outline'),
+          // icon for sm button
+          'px-2 py-2':
+            (size === 'sm' &&
+              icon &&
+              !text &&
+              (variant === 'black' ||
+                variant === 'negative' ||
+                variant === 'black-outline' ||
+                variant === 'category' ||
+                variant === 'category-outline')) ||
+            (size === 'lg' &&
+              icon &&
+              !text &&
+              (variant === 'plain-category' ||
+                variant === 'plain-black' ||
+                variant === 'plain-negative')),
+
+          // icon for sm button plain variant
+          'px-1.5 py-1.5':
+            size === 'sm' &&
             icon &&
             !text &&
             (variant === 'plain-category' ||
               variant === 'plain-black' ||
-              variant === 'plain-negative')),
+              variant === 'plain-negative'),
 
-        // icon for sm button plain variant
-        'px-1.5 py-1.5':
-          size === 'sm' &&
-          icon &&
-          !text &&
-          (variant === 'plain-category' ||
-            variant === 'plain-black' ||
-            variant === 'plain-negative'),
+          // text for lg button plain variant
+          'text-16-semibold px-3 py-2':
+            size === 'lg' &&
+            !icon &&
+            text &&
+            (variant === 'plain-category' ||
+              variant === 'plain-black' ||
+              variant === 'plain-negative'),
 
-        // text for lg button plain variant
-        'text-16-semibold px-3 py-2':
-          size === 'lg' &&
-          !icon &&
-          text &&
-          (variant === 'plain-category' ||
-            variant === 'plain-black' ||
-            variant === 'plain-negative'),
+          // text for sm button plain variant
+          'text-16-semibold px-2 py-1':
+            size === 'sm' &&
+            !icon &&
+            text &&
+            (variant === 'plain-category' ||
+              variant === 'plain-black' ||
+              variant === 'plain-negative'),
 
-        // text for sm button plain variant
-        'text-16-semibold px-2 py-1':
-          size === 'sm' &&
-          !icon &&
-          text &&
-          (variant === 'plain-category' ||
-            variant === 'plain-black' ||
-            variant === 'plain-negative'),
+          // text for lg link button
+          'text-20-medium':
+            size === 'lg' && (variant === 'link-category' || variant === 'link-black'),
+          // text for sm link button
+          'text-16-medium':
+            size === 'sm' && (variant === 'link-category' || variant === 'link-black'),
 
-        // text for lg link button
-        'text-20-medium':
-          size === 'lg' && (variant === 'link-category' || variant === 'link-black'),
-        // text for sm link button
-        'text-16-medium':
-          size === 'sm' && (variant === 'link-category' || variant === 'link-black'),
+          'border-2':
+            variant === 'black' ||
+            variant === 'negative' ||
+            variant === 'black-outline' ||
+            variant === 'category' ||
+            variant === 'category-outline',
 
-        'border-2':
-          variant === 'black' ||
-          variant === 'negative' ||
-          variant === 'black-outline' ||
-          variant === 'category' ||
-          variant === 'category-outline',
+          'focus:ring-2':
+            variant === 'black' ||
+            variant === 'negative' ||
+            variant === 'black-outline' ||
+            variant === 'category' ||
+            variant === 'category-outline',
 
-        'focus:ring-2':
-          variant === 'black' ||
-          variant === 'negative' ||
-          variant === 'black-outline' ||
-          variant === 'category' ||
-          variant === 'category-outline',
+          // bg and border color
+          'border-gray-700 bg-gray-700 focus:border-gray-800 focus:bg-gray-800':
+            variant === 'black',
+          'border-gray-200 bg-transparent text-gray-700 focus:border-gray-300 focus:text-gray-800':
+            variant === 'black-outline',
+          'border-negative-700 bg-negative-700 focus:border-negative-800 focus:bg-negative-800':
+            variant === 'negative',
+          'border-category-700 bg-category-700 focus:border-category-800 focus:bg-category-800':
+            variant === 'category',
+          'border-category-700 text-category-700 focus:border-category-800 focus:text-category-800 bg-transparent':
+            variant === 'category-outline',
 
-        // bg and border color
-        'border-gray-700 bg-gray-700 focus:border-gray-800 focus:bg-gray-800': variant === 'black',
-        'border-gray-200 bg-transparent text-gray-700 focus:border-gray-300 focus:text-gray-800':
-          variant === 'black-outline',
-        'border-negative-700 bg-negative-700 focus:border-negative-800 focus:bg-negative-800':
-          variant === 'negative',
-        'border-category-700 bg-category-700 focus:border-category-800 focus:bg-category-800':
-          variant === 'category',
-        'border-category-700 bg-transparent text-category-700 focus:border-category-800 focus:text-category-800':
-          variant === 'category-outline',
+          'text-category-700 focus:bg-category-200 focus:text-category-800':
+            variant === 'plain-category',
+          'text-gray-700 focus:bg-gray-200 focus:text-gray-800': variant === 'plain-black',
+          'text-negative-700 focus:bg-negative-200 focus:text-negative-800':
+            variant === 'plain-negative',
 
-        'text-category-700 focus:bg-category-200 focus:text-category-800':
-          variant === 'plain-category',
-        'text-gray-700 focus:bg-gray-200 focus:text-gray-800': variant === 'plain-black',
-        'text-negative-700 focus:bg-negative-200 focus:text-negative-800':
-          variant === 'plain-negative',
+          'text-category-700 focus:text-category-800': variant === 'link-category',
+          'text-gray-700 focus:text-gray-800': variant === 'link-black',
 
-        'text-category-700 focus:text-category-800': variant === 'link-category',
-        'text-gray-700 focus:text-gray-800': variant === 'link-black',
+          // hover
+          'hover:border-gray-600 hover:bg-gray-600': variant === 'black' && !disabledStyling,
+          'hover:border-gray-200 hover:text-gray-600':
+            variant === 'black-outline' && !disabledStyling,
+          'hover:border-negative-600 hover:bg-negative-600':
+            variant === 'negative' && !disabledStyling,
 
-        // hover
-        'hover:border-gray-600 hover:bg-gray-600': variant === 'black' && !disabledStyling,
-        'hover:border-gray-200 hover:text-gray-600':
-          variant === 'black-outline' && !disabledStyling,
-        'hover:border-negative-600 hover:bg-negative-600':
-          variant === 'negative' && !disabledStyling,
+          'hover:border-category-600 hover:bg-category-600':
+            variant === 'category' && !disabledStyling,
+          'hover:border-category-600 hover:text-category-600':
+            variant === 'category-outline' && !disabledStyling,
+          'hover:bg-category-100 hover:text-category-600':
+            variant === 'plain-category' && !disabledStyling,
+          'hover:bg-gray-100 hover:text-gray-600': variant === 'plain-black' && !disabledStyling,
+          'hover:bg-negative-100 hover:text-negative-600':
+            variant === 'plain-negative' && !disabledStyling,
 
-        'hover:border-category-600 hover:bg-category-600':
-          variant === 'category' && !disabledStyling,
-        'hover:border-category-600 hover:text-category-600':
-          variant === 'category-outline' && !disabledStyling,
-        'hover:bg-category-100 hover:text-category-600':
-          variant === 'plain-category' && !disabledStyling,
-        'hover:bg-gray-100 hover:text-gray-600': variant === 'plain-black' && !disabledStyling,
-        'hover:bg-negative-100 hover:text-negative-600':
-          variant === 'plain-negative' && !disabledStyling,
+          'hover:text-category-600': variant === 'link-category' && !disabledStyling,
+          'hover:text-gray-600': variant === 'link-black' && !disabledStyling,
 
-        'hover:text-category-600': variant === 'link-category' && !disabledStyling,
-        'hover:text-gray-600': variant === 'link-black' && !disabledStyling,
+          // text color
+          'text-white': variant === 'negative' || variant === 'black' || variant === 'category',
 
-        // text color
-        'text-white': variant === 'negative' || variant === 'black' || variant === 'category',
-
-        // opacity lowered only when explicitly disabled, not when loading
-        'opacity-50': disabled,
-      },
+          // opacity lowered only when explicitly disabled, not when loading
+          'opacity-50': disabled,
+        },
+      ),
+      className,
     )
 
     if (rest.href) {

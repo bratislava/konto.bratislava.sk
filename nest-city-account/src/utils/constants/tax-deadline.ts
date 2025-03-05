5fdfc028ee3d211c6cc3190191e89b3a3f5c462b
Erik Rehulka <erik.rehulka@bratislava.sk>
2025-03-05 17:08:01 +0100

    fix failing tests

--- nest-city-account/src/utils/constants/tax-deadline.ts
+++ nest-city-account/src/utils/constants/tax-deadline.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon'
 
 export function getTaxDeadlineDate(): Date {
   return DateTime.now()
-    .set({ month: 4, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 })
     .setZone('Europe/Bratislava')
+    .set({ month: 4, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 })
     .toJSDate()
 }

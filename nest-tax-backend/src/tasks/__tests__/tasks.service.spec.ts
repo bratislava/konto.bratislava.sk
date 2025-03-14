1c8b5efd71980853aea2cbb8f0c68330fae9a080
Jakub Hvolka <jhvolka@gmail.com>
2025-03-14 10:54:36 +0100

    fix test

--- nest-tax-backend/src/tasks/__tests__/tasks.service.spec.ts
+++ nest-tax-backend/src/tasks/__tests__/tasks.service.spec.ts
@@ -5,6 +5,7 @@ import { AdminService } from '../../admin/admin.service'
 import { PrismaService } from '../../prisma/prisma.service'
 import ThrowerErrorGuard from '../../utils/guards/errors.guard'
 import { TasksService } from '../tasks.service'
+import { CardPaymentReportingService } from '../../card-payment-reporting/card-payment-reporting.service'
 
 describe('TasksService', () => {
   let service: TasksService
@@ -16,6 +17,7 @@ describe('TasksService', () => {
         ThrowerErrorGuard,
         { provide: AdminService, useValue: createMock<AdminService>() },
         { provide: PrismaService, useValue: createMock<PrismaService>() },
+        { provide: CardPaymentReportingService, useValue: createMock<CardPaymentReportingService>() },
       ],
     }).compile()
 

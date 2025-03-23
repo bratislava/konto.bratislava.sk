983abbfe21da526d4326d943e37ece529c638ca6
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-03-23 17:16:21 +0100

    Use JSON endpoint to fetch Slovensko.sk metadata

--- forms-shared/test-utils/fetchSlovenskoSkFormMetadata.ts
+++ forms-shared/test-utils/fetchSlovenskoSkFormMetadata.ts
@@ -1,67 +1,49 @@
-import { JSDOM } from 'jsdom'
-import { parseStringPromise } from 'xml2js'
 import { FormDefinitionSlovenskoSk } from '../src/definitions/formDefinitionTypes'
 
-interface SlovenskoSkMetadataXml {
-  'meta:metadata': {
-    $: {
-      'xmlns:attachment': string
-      'xmlns:manifest': string
-      'xmlns:attachmentfile': string
-      'xmlns:xs': string
-      'xmlns:setting': string
-      'xmlns:content': string
-      'xmlns:dc': string
-      'xmlns:presentation': string
-      'xmlns:data': string
-      'xmlns:meta': string
-    }
-    'dc:title': string[]
-    'dc:identifier': string[]
-    'dc:description': string[]
-    'dc:creator': string[]
-    'dc:publisher': string[]
-    'meta:version': string[]
-    'meta:validDateFrom': string[]
-    'meta:inForceFrom': string[]
-    'meta:inForceTo'?: string[]
-  }
+export interface SlovenskoSkMetadataJson {
+  XSDtargetNamespace: string
+  title: string
+  identifier: string
+  shortIdentifier: string
+  eformURI: string
+  eformPartsURI: string
+  description: string
+  creator: string
+  publisherURI: string
+  publisherUpvsURI: string
+  language: string | null
+  version: string
+  validDateFrom: string
+  validDateTo: string | null
+  inForceFrom: string
+  inForceTo: string | null
+  changeDate: string
 }
 
-const getSlovenskoSkUrl = (formDefinition: FormDefinitionSlovenskoSk) => {
-  const versionRegex = /^\d+\.\d+$/
-  if (!versionRegex.test(formDefinition.pospVersion)) {
-    throw new Error(
-      'Invalid pospVersion format. Expected format: "x.y" where x and y are integers.',
-    )
-  }
-  const [vh, vl] = formDefinition.pospVersion.split('.').map(Number)
-
-  return `https://formulare.slovensko.sk/_layouts/eFLCM/DetailVzoruEFormulara.aspx?vid=${formDefinition.pospID}&vh=${vh}&vl=${vl}`
-}
-
-/**
- * Fetches the form metadata XML and converts it to JSON, e.g.:
- * https://formulare.slovensko.sk/_layouts/eFLCM/DetailVzoruEFormulara.aspx?vid=00603481.predzahradky&vh=1&vl=0
- */
-export async function fetchSlovenskoSkFormMetadata(formDefintion: FormDefinitionSlovenskoSk) {
-  const url = getSlovenskoSkUrl(formDefintion)
+export async function fetchSlovenskoSkMetadata() {
+  const url = 'https://www.slovensko.sk/static/eForm/datasetexport/json/mef.json'
   const response = await fetch(url)
   if (!response.ok) {
-    throw new Error(`Failed to fetch the URL: ${response.statusText}`)
+    throw new Error(`Failed to fetch metadata: ${response.statusText}`)
   }
-  const htmlText = await response.text()
 
-  const dom = new JSDOM(htmlText)
-  const document = dom.window.document
+  return (await response.json()) as SlovenskoSkMetadataJson[]
+}
 
-  const textarea = document.querySelector(
-    'textarea#PlaceHolderMain_metadataTextBox',
-  ) as HTMLTextAreaElement
-  if (!textarea) {
-    throw new Error('Textarea with ID "PlaceHolderMain_metadataTextBox" not found')
+export function findSlovenskoSkFormMetadata(
+  allMetadata: SlovenskoSkMetadataJson[],
+  formDefinition: FormDefinitionSlovenskoSk,
+) {
+  const formMetadata = allMetadata.find(
+    (item) =>
+      item.shortIdentifier === formDefinition.pospID && item.version === formDefinition.pospVersion,
+  )
+
+  if (!formMetadata) {
+    throw new Error(
+      `Form metadata not found for ID ${formDefinition.pospID} version ${formDefinition.pospVersion}`,
+    )
   }
-  const xmlContent = textarea.value
-  const parsedXml = (await parseStringPromise(xmlContent)) as SlovenskoSkMetadataXml
-  return parsedXml['meta:metadata']
+
+  return formMetadata
 }

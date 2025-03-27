36fa75e430919f7e864cc0bcebc755845bd2b3ac
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2025-03-27 13:33:53 +0100

    803 Žiadosť o sprístupnenie informácií podľa zákona č. 211/2000 Z.z. form (#2530)
    * Create new form
    
    * Update snapshots

--- forms-shared/src/example-forms/exampleForms.ts
+++ forms-shared/src/example-forms/exampleForms.ts
@@ -21,6 +21,7 @@ import oloUzatvorenieZmluvyONakladaniSOdpadomExample from './examples/oloUzatvor
 import oloDocistenieStanovistaZbernychNadobExample from './examples/oloDocistenieStanovistaZbernychNadobExample'
 import oloOdvozOdpaduVelkokapacitnymAleboLisovacimKontajneromExample from './examples/oloOdvozOdpaduVelkokapacitnymAleboLisovacimKontajneromExample'
 import oznamenieOPoplatkovejPovinnostiZaKomunalneOdpadyExample from './examples/oznamenieOPoplatkovejPovinnostiZaKomunalneOdpadyExample'
+import ziadostOSlobodnyPristupKInformaciamExample from './examples/ziadostOSlobodnyPristupKInformaciamExample'
 
 export const exampleForms: Record<string, ExampleForm[]> = {
   'stanovisko-k-investicnemu-zameru': [stanoviskoKInvesticnemuZameruExample],
@@ -55,6 +56,7 @@ export const exampleForms: Record<string, ExampleForm[]> = {
   'oznamenie-o-poplatkovej-povinnosti-za-komunalne-odpady': [
     oznamenieOPoplatkovejPovinnostiZaKomunalneOdpadyExample,
   ],
+  'ziadost-o-slobodny-pristup-k-informaciam': [ziadostOSlobodnyPristupKInformaciamExample],
 }
 
 export const exampleDevForms: Record<string, ExampleForm[]> = {}

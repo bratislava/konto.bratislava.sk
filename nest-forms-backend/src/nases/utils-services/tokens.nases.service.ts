72c46a717758ffcbedab16c8e5172f0563521f67
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-11 12:45:09 +0200

    User ClientsService instead of clients directly

--- nest-forms-backend/src/nases/utils-services/tokens.nases.service.ts
+++ nest-forms-backend/src/nases/utils-services/tokens.nases.service.ts
@@ -16,9 +16,11 @@ import { getFormDefinitionBySlug } from 'forms-shared/definitions/getFormDefinit
 import { buildSlovenskoSkXml } from 'forms-shared/slovensko-sk/xmlBuilder'
 import jwt from 'jsonwebtoken'
 import mime from 'mime-types'
+import { UserControllerGetOrCreateUser200Response } from 'openapi-clients/city-account'
 import { v1 as uuidv1, v4 as uuidv4 } from 'uuid'
 
 import { CognitoGetUserData } from '../../auth/dtos/cognito.dto'
+import ClientsService from '../../clients/clients.service'
 import ConvertService from '../../convert/convert.service'
 import {
   FormsErrorsEnum,
@@ -26,9 +28,6 @@ import {
 } from '../../forms/forms.errors.enum'
 import PrismaService from '../../prisma/prisma.service'
 import TaxService from '../../tax/tax.service'
-import { cityAccountApi } from '../../utils/clients/cityAccountApi'
-import { UserControllerGetOrCreateUser200Response } from '../../utils/clients/openapi-city-account'
-import { slovenskoSkApi } from '../../utils/clients/slovenskoSkApi'
 import { ErrorsEnum } from '../../utils/global-enums/errors.enum'
 import ThrowerErrorGuard from '../../utils/guards/thrower-error.guard'
 import alertError, {
@@ -55,6 +54,7 @@ export default class NasesUtilsService {
     private minioClientSubservice: MinioClientSubservice,
     private taxService: TaxService,
     private configService: ConfigService,
+    private readonly clientsService: ClientsService,
   ) {
     this.logger = new LineLoggerSubservice('NasesUtilsService')
   }
@@ -252,7 +252,7 @@ export default class NasesUtilsService {
   async getUserInfo(
     bearerToken: string,
   ): Promise<UserControllerGetOrCreateUser200Response> {
-    return cityAccountApi
+    return this.clientsService.cityAccountApi
       .userControllerGetOrCreateUser({
         headers: {
           Authorization: bearerToken,
@@ -518,16 +518,17 @@ export default class NasesUtilsService {
       }
     }
     try {
-      const response = await slovenskoSkApi.apiSktalkReceiveAndSaveToOutboxPost(
-        {
-          message,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${jwtToken}`,
+      const response =
+        await this.clientsService.slovenskoSkApi.apiSktalkReceiveAndSaveToOutboxPost(
+          {
+            message,
           },
-        },
-      )
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          },
+        )
 
       if (!response.data) {
         // TODO temp SEND_TO_NASES_ERROR log, remove
@@ -582,7 +583,7 @@ export default class NasesUtilsService {
 
   async isNasesMessageDelivered(formId: string): Promise<boolean> {
     const jwtToken = this.createTechnicalAccountJwtToken()
-    const result = await slovenskoSkApi
+    const result = await this.clientsService.slovenskoSkApi
       .apiEdeskMessagesSearchGet(formId, undefined, undefined, {
         headers: {
           Authorization: `Bearer ${jwtToken}`,

b1881cf641cc91e17e613c5162ad9bccf42d3b4d
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-11 13:55:14 +0200

    Adjust validateXml to return errors

--- forms-shared/src/slovensko-sk/validateXml.ts
+++ forms-shared/src/slovensko-sk/validateXml.ts
@@ -1,21 +1,58 @@
-export async function validateXml(xmlString: string, xsdString: string) {
-  try {
-    const { XmlDocument, XsdValidator } = await import('libxml2-wasm')
-    const xmlDoc = XmlDocument.fromString(xmlString)
-    const xsdDoc = XmlDocument.fromString(xsdString)
-    const xsdValidator = XsdValidator.fromDoc(xsdDoc)
+export type ValidateXmlResultError = {
+  message: string
+  line: number
+  col: number
+}
 
-    try {
-      xsdValidator.validate(xmlDoc)
-      return true
-    } catch (e) {
-      return false
-    } finally {
-      xsdValidator.dispose()
-      xmlDoc.dispose()
-      xsdDoc.dispose()
+export type ValidateXmlResult =
+  | {
+      success: true
+    }
+  | {
+      success: false
+      errors?: ValidateXmlResultError[]
+    }
+
+export async function validateXml(
+  xmlString: string,
+  xsdString: string,
+): Promise<ValidateXmlResult> {
+  const { XmlDocument, XsdValidator, XmlLibError } = await import('libxml2-wasm')
+
+  let xmlDoc: InstanceType<typeof XmlDocument> | null = null
+  let xsdDoc: InstanceType<typeof XmlDocument> | null = null
+  let xsdValidator: InstanceType<typeof XsdValidator> | null = null
+
+  try {
+    xmlDoc = XmlDocument.fromString(xmlString)
+    xsdDoc = XmlDocument.fromString(xsdString)
+    xsdValidator = XsdValidator.fromDoc(xsdDoc)
+
+    xsdValidator.validate(xmlDoc)
+
+    return {
+      success: true,
     }
   } catch (error) {
-    return false
+    if (error instanceof XmlLibError) {
+      return {
+        success: false,
+        errors: error.details,
+      }
+    }
+
+    return {
+      success: false,
+    }
+  } finally {
+    xsdValidator?.dispose()
+    xmlDoc?.dispose()
+    xsdDoc?.dispose()
   }
+}
+
+export const formatValidateXmlResultErrors = (errors: ValidateXmlResultError[]) => {
+  return errors
+    .map((error) => `${error.message} (line ${error.line}, column ${error.col})`)
+    .join('\n')
 }

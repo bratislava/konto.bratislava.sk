95804ab916b993356be3dc8412f4ede65f9df83e
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-24 16:45:43 +0200

    Create tests that both versions produce the same PDF mapping/XML

--- forms-shared/tests/tax-form/taxForm.ts
+++ forms-shared/tests/tax-form/taxForm.ts
@@ -13,68 +13,101 @@ import { formatValidateXmlResultErrors, validateXml } from '../../src/slovensko-
 
 expect.extend({ toMatchImageSnapshot })
 
-describe('tax-form', () => {
-  getExampleFormPairs({ formDefinitionFilterFn: isSlovenskoSkTaxFormDefinition }).forEach(
-    ({ exampleForm, formDefinition }) => {
-      describe('PDF', () => {
-        beforeEach(() => {
-          // Without `shouldAdvanceTime` the PDF generation would hang indefinitely.
-          vi.useFakeTimers({ now: new Date('2024-01-01'), shouldAdvanceTime: true })
-        })
-
-        afterEach(() => {
-          vi.useRealTimers()
-        })
-
-        test(`should return correct PDF mapping for ${exampleForm.name}`, () => {
-          expect(getTaxFormPdfMapping(exampleForm.formData, undefined)).toMatchSnapshot()
-        })
-
-        test(
-          `should match snapshot for generated PDF ${exampleForm.name}`,
-          async () => {
-            filterConsole(
-              'log',
-              (message) =>
-                message ===
-                'Warning: _getAppearance: OffscreenCanvas is not supported, annotation may not render correctly.',
-            )
-
-            const base64Pdf = await generateTaxPdf({ formData: exampleForm.formData })
-
-            await expectPdfToMatchSnapshot(`data:application/pdf;base64,${base64Pdf}`)
-          },
-          screenshotTestTimeout,
-        )
-      })
-
-      describe('XML', () => {
-        let xmlString: string
-
-        beforeEach(() => {
-          vi.useFakeTimers({ now: new Date('2024-01-01') })
-          xmlString = generateTaxXml(exampleForm.formData, true, formDefinition)
-        })
-
-        afterEach(() => {
-          vi.useRealTimers()
-        })
-
-        test(`should return correct XML for ${exampleForm.name}`, () => {
-          expect(xmlString).toMatchSnapshot()
-        })
-
-        test(`should produce valid XML for ${exampleForm.name}`, async () => {
-          const result = await validateXml(xmlString, getTaxXsd(formDefinition))
-          if (!result.success && result.errors) {
-            throw new Error(
-              `XML validation errors: ${formatValidateXmlResultErrors(result.errors)}`,
-            )
-          }
-
-          expect(result.success).toBe(true)
-        })
-      })
-    },
+describe('taxForm', () => {
+  const exampleFormPairs = getExampleFormPairs({
+    formDefinitionFilterFn: isSlovenskoSkTaxFormDefinition,
+  })
+  const example5 = exampleFormPairs.find(
+    ({ exampleForm }) => exampleForm.name === 'priznanieKDaniZNehnutelnostiExample5',
   )
+  const example5NoCalculators = exampleFormPairs.find(
+    ({ exampleForm }) => exampleForm.name === 'priznanieKDaniZNehnutelnostiExample5NoCalculators',
+  )
+  if (!example5 || !example5NoCalculators) {
+    throw new Error('example5 or example5NoCalculators is undefined')
+  }
+
+  describe('PDF', () => {
+    beforeEach(() => {
+      // Without `shouldAdvanceTime` the PDF generation would hang indefinitely.
+      vi.useFakeTimers({ now: new Date('2024-01-01'), shouldAdvanceTime: true })
+    })
+
+    afterEach(() => {
+      vi.useRealTimers()
+    })
+
+    exampleFormPairs.forEach(({ exampleForm }) => {
+      test(`should return correct PDF mapping for ${exampleForm.name}`, () => {
+        expect(getTaxFormPdfMapping(exampleForm.formData, undefined)).toMatchSnapshot()
+      })
+
+      test(
+        `should match snapshot for generated PDF ${exampleForm.name}`,
+        async () => {
+          filterConsole(
+            'log',
+            (message) =>
+              message ===
+              'Warning: _getAppearance: OffscreenCanvas is not supported, annotation may not render correctly.',
+          )
+
+          const base64Pdf = await generateTaxPdf({ formData: exampleForm.formData })
+
+          await expectPdfToMatchSnapshot(`data:application/pdf;base64,${base64Pdf}`)
+        },
+        screenshotTestTimeout,
+      )
+    })
+
+    test('example5 and example5NoCalculators should produce the same PDF mapping', async () => {
+      const example5PdfMapping = getTaxFormPdfMapping(example5.exampleForm.formData, undefined)
+      const example5NoCalculatorsPdfMapping = getTaxFormPdfMapping(
+        example5NoCalculators.exampleForm.formData,
+        undefined,
+      )
+      expect(example5PdfMapping).toEqual(example5NoCalculatorsPdfMapping)
+    })
+  })
+
+  describe('XML', () => {
+    beforeEach(() => {
+      vi.useFakeTimers({ now: new Date('2024-01-01') })
+    })
+
+    afterEach(() => {
+      vi.useRealTimers()
+    })
+
+    exampleFormPairs.forEach(({ exampleForm, formDefinition }) => {
+      test(`should match snapshot for generated XML for ${exampleForm.name}`, () => {
+        const xmlString = generateTaxXml(exampleForm.formData, true, formDefinition)
+        expect(xmlString).toMatchSnapshot()
+      })
+
+      test(`should produce valid XML for ${exampleForm.name}`, async () => {
+        const xmlString = generateTaxXml(exampleForm.formData, true, formDefinition)
+        const result = await validateXml(xmlString, getTaxXsd(formDefinition))
+        if (!result.success && result.errors) {
+          throw new Error(`XML validation errors: ${formatValidateXmlResultErrors(result.errors)}`)
+        }
+
+        expect(result.success).toBe(true)
+      })
+    })
+
+    test('example5 and example5NoCalculators should produce the same XML', () => {
+      const example5Xml = generateTaxXml(
+        example5.exampleForm.formData,
+        true,
+        example5.formDefinition,
+      )
+      const example5NoCalculatorsXml = generateTaxXml(
+        example5NoCalculators.exampleForm.formData,
+        true,
+        example5NoCalculators.formDefinition,
+      )
+      expect(example5Xml).toEqual(example5NoCalculatorsXml)
+    })
+  })
 })

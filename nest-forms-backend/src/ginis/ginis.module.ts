6473d8dd5a2139a52c12f39bc33c74c2eca172df
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-28 19:42:20 +0200

    Put SharepointSubservice into correct module

--- nest-forms-backend/src/ginis/ginis.module.ts
+++ nest-forms-backend/src/ginis/ginis.module.ts
@@ -6,6 +6,7 @@ import ClientsModule from '../clients/clients.module'
 import ConvertModule from '../convert/convert.module'
 import ConvertPdfModule from '../convert-pdf/convert-pdf.module'
 import FilesModule from '../files/files.module'
+import FormValidatorRegistryModule from '../form-validator-registry/form-validator-registry.module'
 import FormsModule from '../forms/forms.module'
 import NasesUtilsService from '../nases/utils-services/tokens.nases.service'
 import RabbitmqClientModule from '../rabbitmq-client/rabbitmq-client.module'
@@ -13,6 +14,7 @@ import TaxModule from '../tax/tax.module'
 import MailgunService from '../utils/global-services/mailer/mailgun.service'
 import ThrowerErrorGuard from '../utils/guards/thrower-error.guard'
 import MinioClientSubservice from '../utils/subservices/minio-client.subservice'
+import SharepointSubservice from '../utils/subservices/sharepoint.subservice'
 import GinisController from './ginis.controller'
 import GinisService from './ginis.service'
 import GinisHelper from './subservices/ginis.helper'
@@ -32,6 +34,7 @@ import GinisTasksSubservice from './subservices/ginis-tasks.subservice'
     }),
     ClientsModule,
     UserInfoPipeModule,
+    FormValidatorRegistryModule,
   ],
   providers: [
     GinisService,
@@ -42,6 +45,7 @@ import GinisTasksSubservice from './subservices/ginis-tasks.subservice'
     MailgunService,
     MinioClientSubservice,
     GinisTasksSubservice,
+    SharepointSubservice,
   ],
   exports: [GinisService, GinisHelper],
   controllers: [GinisController],

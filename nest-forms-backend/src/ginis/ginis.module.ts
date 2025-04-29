9049a0ed9623a17e225e9666dca943390dfc114a
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-29 13:34:29 +0200

    Revert "Put SharepointSubservice into correct module (#2716)"
    This reverts commit 9d9d3545a64cfe3237de6ce20cf37e4b3f30d2bf.

--- nest-forms-backend/src/ginis/ginis.module.ts
+++ nest-forms-backend/src/ginis/ginis.module.ts
@@ -6,7 +6,6 @@ import ClientsModule from '../clients/clients.module'
 import ConvertModule from '../convert/convert.module'
 import ConvertPdfModule from '../convert-pdf/convert-pdf.module'
 import FilesModule from '../files/files.module'
-import FormValidatorRegistryModule from '../form-validator-registry/form-validator-registry.module'
 import FormsModule from '../forms/forms.module'
 import NasesUtilsService from '../nases/utils-services/tokens.nases.service'
 import RabbitmqClientModule from '../rabbitmq-client/rabbitmq-client.module'
@@ -14,7 +13,6 @@ import TaxModule from '../tax/tax.module'
 import MailgunService from '../utils/global-services/mailer/mailgun.service'
 import ThrowerErrorGuard from '../utils/guards/thrower-error.guard'
 import MinioClientSubservice from '../utils/subservices/minio-client.subservice'
-import SharepointSubservice from '../utils/subservices/sharepoint.subservice'
 import GinisController from './ginis.controller'
 import GinisService from './ginis.service'
 import GinisHelper from './subservices/ginis.helper'
@@ -34,7 +32,6 @@ import GinisTasksSubservice from './subservices/ginis-tasks.subservice'
     }),
     ClientsModule,
     UserInfoPipeModule,
-    FormValidatorRegistryModule,
   ],
   providers: [
     GinisService,
@@ -45,7 +42,6 @@ import GinisTasksSubservice from './subservices/ginis-tasks.subservice'
     MailgunService,
     MinioClientSubservice,
     GinisTasksSubservice,
-    SharepointSubservice,
   ],
   exports: [GinisService, GinisHelper],
   controllers: [GinisController],

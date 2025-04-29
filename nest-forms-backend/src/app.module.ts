fc530d195e2f5ec574cddf3e8fff4a381a8d67bc
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-04-29 13:34:24 +0200

    Revert "Move BullModule above GinisModule (#2719)"
    This reverts commit 100ef275f88628ca3ec072e5bb3a8530188edb87.

--- nest-forms-backend/src/app.module.ts
+++ nest-forms-backend/src/app.module.ts
@@ -42,18 +42,6 @@ import AppLoggerMiddleware from './utils/middlewares/logger.service'
         secretKey: baConfigService.minio.secretKey,
       }),
     }),
-    BullModule.forRootAsync({
-      imports: [BaConfigModule],
-      inject: [BaConfigService],
-      useFactory: async (baConfigService: BaConfigService) => ({
-        redis: {
-          host: baConfigService.redis.service,
-          port: baConfigService.redis.port,
-          username: baConfigService.redis.username,
-          password: baConfigService.redis.password,
-        },
-      }),
-    }),
     PrismaModule,
     AuthModule,
     AdminModule,
@@ -69,6 +57,20 @@ import AppLoggerMiddleware from './utils/middlewares/logger.service'
     TaxModule,
     FormValidatorRegistryModule,
     SignerModule,
+    // BEWARE: If Bull doesn't connect to Redis successfully, it will silently fail!
+    // https://github.com/nestjs/bull/issues/1076
+    BullModule.forRootAsync({
+      imports: [BaConfigModule],
+      inject: [BaConfigService],
+      useFactory: async (baConfigService: BaConfigService) => ({
+        redis: {
+          host: baConfigService.redis.service,
+          port: baConfigService.redis.port,
+          username: baConfigService.redis.username,
+          password: baConfigService.redis.password,
+        },
+      }),
+    }),
     ScheduleModule.forRoot(),
   ],
   controllers: [AppController],

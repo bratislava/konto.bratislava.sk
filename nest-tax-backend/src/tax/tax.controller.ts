b3efcb7ddbe4630b3fc17ca6c110135ea2c53d24
Erik Řehulka <erik.rehulka@bratislava.sk>
2025-05-05 10:34:20 +0200

    Use openapi-clients in tax backend (#2717)
    * Use shared openapi clients in tax backend
    
    * try and fix build issues
    
    * revert commits
    
    * Fix openapi-clients path for build in City account / Tax BE
    
    ---------
    
    Co-authored-by: Marek Bodinger <marek.bodinger@bratislava.sk>

--- nest-tax-backend/src/tax/tax.controller.ts
+++ nest-tax-backend/src/tax/tax.controller.ts
@@ -14,7 +14,6 @@ import {
 } from '@nestjs/swagger'
 import { AuthenticationGuard } from '@nestjs-cognito/auth'
 import pdf, { CreateOptions } from 'html-pdf'
-import { BratislavaUser } from 'src/auth/guards/cognito.guard'
 import { TiersGuard } from 'src/auth/guards/tiers.guard'
 import { Tiers } from 'src/utils/decorators/tier.decorator'
 import { CognitoTiersEnum } from 'src/utils/global-dtos/cognito.dto'
@@ -24,6 +23,7 @@ import {
 } from 'src/utils/guards/dtos/error.dto'
 import ThrowerErrorGuard from 'src/utils/guards/errors.guard'
 
+import { BratislavaUser } from '../auth/decorators/user-info.decorator'
 import { BratislavaUserDto } from '../utils/global-dtos/city-account.dto'
 import { LineLoggerSubservice } from '../utils/subservices/line-logger.subservice'
 import { CustomErrorPdfCreateTypesEnum } from './dtos/error.dto'

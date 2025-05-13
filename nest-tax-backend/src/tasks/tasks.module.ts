b441c0dd2261b64eaa6edec78693fefac0140327
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2025-05-13 15:03:39 +0200

    Fix tax BE build (#2805)

--- nest-tax-backend/src/tasks/tasks.module.ts
+++ nest-tax-backend/src/tasks/tasks.module.ts
@@ -3,13 +3,19 @@ import { Module } from '@nestjs/common'
 import { AdminModule } from '../admin/admin.module'
 import { BloomreachModule } from '../bloomreach/bloomreach.module'
 import { CardPaymentReportingModule } from '../card-payment-reporting/card-payment-reporting.module'
+import ClientsModule from '../clients/clients.module'
 import ThrowerErrorGuard from '../utils/guards/errors.guard'
 import { CityAccountSubservice } from '../utils/subservices/cityaccount.subservice'
 import DatabaseSubservice from '../utils/subservices/database.subservice'
 import { TasksService } from './tasks.service'
 
 @Module({
-  imports: [AdminModule, CardPaymentReportingModule, BloomreachModule],
+  imports: [
+    AdminModule,
+    CardPaymentReportingModule,
+    BloomreachModule,
+    ClientsModule,
+  ],
   providers: [
     TasksService,
     ThrowerErrorGuard,

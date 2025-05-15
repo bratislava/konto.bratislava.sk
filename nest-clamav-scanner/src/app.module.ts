788595d070f07f543124226af68e2264f75dadb9
Lukáš Babula <lukas.babula@gmail.com>
2025-05-15 23:35:58 +0200

    795 Use openapi-clients in virus scanner (#2584)
    * Fix for local virus scanner configuration
    
    * Unify prettier style in virus scanner with other backends
    
    * Upgrade scanner dependencies
    
    * Create openapi-clients module and service
    
    * Upgrade axios version throughout the repository
    
    * Propagate new clients module usage in scanner
    
    * Include openapi-clients in local scanner setup
    
    * Fix missing clients usage in scanner
    
    * Fix import order in scanner clients
    
    * Fix scanner dockerfile for openapi-clients build

--- nest-clamav-scanner/src/app.module.ts
+++ nest-clamav-scanner/src/app.module.ts
@@ -1,23 +1,24 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { ScheduleModule } from '@nestjs/schedule'
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ClamavClientModule } from './clamav-client/clamav-client.module';
-import { FormsClientModule } from './forms-client/forms-client.module';
-import { MinioClientModule } from './minio-client/minio-client.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ScannerModule } from './scanner/scanner.module';
-import { ScannerCronModule } from './scanner-cron/scanner-cron.module';
-import { StatusModule } from './status/status.module';
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { ClamavClientModule } from './clamav-client/clamav-client.module'
+import ClientsModule from './clients/clients.module'
+import { MinioClientModule } from './minio-client/minio-client.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { ScannerModule } from './scanner/scanner.module'
+import { ScannerCronModule } from './scanner-cron/scanner-cron.module'
+import { StatusModule } from './status/status.module'
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ClientsModule,
     StatusModule,
     ScannerModule,
     PrismaModule,
@@ -26,7 +27,6 @@ import { StatusModule } from './status/status.module';
     ClamavClientModule,
     ScheduleModule.forRoot(),
     ScannerCronModule,
-    FormsClientModule,
   ],
 })
 export class AppModule {}

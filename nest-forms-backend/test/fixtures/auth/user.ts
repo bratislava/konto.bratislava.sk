7a028880e42b0625b75e2404e04eca6034d2ecd7
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-05-20 10:53:16 +0200

    Add tests for UserAuthStrategy

---
+++ nest-forms-backend/test/fixtures/auth/user.ts
@@ -0,0 +1,109 @@
+import {
+  UserOfficialCorrespondenceChannelEnum,
+  UserVerifyStateCognitoTierEnum,
+  UserVerifyStateTypeEnum,
+} from 'openapi-clients/city-account'
+import supertest from 'supertest'
+
+import { User, UserType } from '../../../src/auth-v2/types/user'
+
+type FixtureUser = {
+  headers: Record<string, string>
+  user: User | null
+}
+
+export const fixtureGuestUserIdentityId = '2b6fe2b9-5754-4dc9-b653-d96b5d13c03d'
+export const fixtureInvalidGuestUserIdentityId =
+  '98eba9d0-7bb4-4c63-8f1c-9ff427538325'
+
+export const fixtureGuestUser: FixtureUser = {
+  headers: {
+    'X-Cognito-Guest-Identity-Id': fixtureGuestUserIdentityId,
+  },
+  user: {
+    type: UserType.Guest,
+    cognitoIdentityId: fixtureGuestUserIdentityId,
+  },
+}
+
+export const fixtureInvalidGuestUser: FixtureUser = {
+  headers: {
+    'X-Cognito-Guest-Identity-Id': fixtureInvalidGuestUserIdentityId,
+  },
+  user: null,
+}
+
+export const fixtureAuthUserSub = '6070fed1-2b24-498d-9012-815a2ce2c817'
+export const fixtureAuthUserBearerToken = 'authUserBearerToken'
+export const fixtureInvalidAuthUserBearerToken = 'authUserWrongBearerToken'
+
+export const fixtureAuthUser: FixtureUser = {
+  headers: {
+    Authorization: `Bearer ${fixtureAuthUserBearerToken}`,
+  },
+  user: {
+    type: UserType.Auth,
+    cognitoJwtPayload: {
+      version: 1,
+      sub: fixtureAuthUserSub,
+      iss: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_FZDV0j2ZK',
+      client_id: 'exampleclientid',
+      origin_jti: '83c48067-c002-4ce3-9f64-8d039b177913',
+      event_id: '3514487d-f4e3-488b-b1f5-754f29a11f79',
+      token_use: 'access',
+      scope: 'aws.cognito.signin.user.admin',
+      auth_time: 1_700_000_000,
+      exp: 1_700_000_100,
+      iat: 1_700_000_000,
+      jti: '893d2e7c-0fe8-46c7-ad46-4b774d0c8126',
+      username: fixtureAuthUserSub,
+    },
+    cognitoUser: {
+      userAttributes: {
+        sub: fixtureAuthUserSub,
+        'custom:account_type': UserVerifyStateTypeEnum.Fo,
+        'custom:tier': UserVerifyStateCognitoTierEnum.IdentityCard,
+        given_name: 'John',
+        family_name: 'Doe',
+        // eslint-disable-next-line pii/no-email
+        email: 'user@example.com',
+      },
+      userCreateDate: new Date('2024-01-01T00:00:00.000Z'),
+      userLastModifiedDate: new Date('2024-01-02T00:00:00.000Z'),
+      userStatus: 'CONFIRMED',
+      username: fixtureAuthUserSub,
+    },
+    cityAccountUser: {
+      id: 'cb827c6a-9f6e-48e5-b9bb-2e49ce008576',
+      createdAt: '2024-01-01T00:00:00.000Z',
+      updatedAt: '2024-01-02T00:00:00.000Z',
+      externalId: fixtureAuthUserSub,
+      // eslint-disable-next-line pii/no-email
+      email: 'user@example.com',
+      birthNumber: null,
+      wasVerifiedBeforeTaxDeadline: false,
+      officialCorrespondenceChannel:
+        UserOfficialCorrespondenceChannelEnum.Postal,
+      showEmailCommunicationBanner: false,
+      gdprData: [],
+    },
+  },
+}
+
+export const fixtureInvalidAuthUser: FixtureUser = {
+  headers: {
+    Authorization: `Bearer ${fixtureInvalidAuthUserBearerToken}`,
+  },
+  user: null,
+}
+
+export function withUser<Request extends supertest.Test>(
+  req: Request,
+  user: FixtureUser,
+) {
+  let newReq = req
+  Object.entries(user.headers).forEach(([key, value]) => {
+    newReq = newReq.set(key, value)
+  })
+  return newReq
+}

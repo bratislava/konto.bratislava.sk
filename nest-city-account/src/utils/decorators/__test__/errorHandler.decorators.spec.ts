9f6e0e37c2c33f31a4420cf45d6688e17999c9d2
Jakub Hvolka <jhvolka@gmail.com>
2025-05-27 14:58:51 +0200

    Update datetime format in test regex to ISO 8601 standard

--- nest-city-account/src/utils/decorators/__test__/errorHandler.decorators.spec.ts
+++ nest-city-account/src/utils/decorators/__test__/errorHandler.decorators.spec.ts
@@ -27,7 +27,7 @@ describe('HandleErrors', () => {
     await expect(t.testMethod()).resolves.toBeNull()
 
     const regex =
-      /process="\[Nest\]" processPID="\d+" datetime="\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}" severity="ERROR" context="Test error handler" errorType="Error" message="This is a test error" method="undefined" stack="Error: This is a test error.*"/
+      /process="\[Nest\]" processPID="\d+" datetime="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z" severity="ERROR" context="Test error handler" errorType="Error" message="This is a test error" method="undefined" stack="Error: This is a test error.*"/
 
     expect(consoleErrorMock).toHaveBeenCalledTimes(1)
     expect(consoleErrorMock).toHaveBeenCalledWith(expect.stringMatching(regex))
@@ -54,7 +54,7 @@ describe('HandleErrors', () => {
     await expect(t.testMethod()).resolves.toBeNull()
 
     const regex =
-      /process="\[Nest\]" processPID="\d+" datetime="\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}" severity="ERROR" context="Test error handler" errorType="HttpException" statusCode="400" status="Bad Request" errorName="INTERNAL_SERVER_ERROR" message="Error message" alert="0" errorCause="Error" causedByMessage="Caused by error message test" console="Console error" method="undefined" stack="HttpException:.*Was directly caused by:.*"/
+      /process="\[Nest\]" processPID="\d+" datetime="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z" severity="ERROR" context="Test error handler" errorType="HttpException" statusCode="400" status="Bad Request" errorName="INTERNAL_SERVER_ERROR" message="Error message" alert="0" errorCause="Error" causedByMessage="Caused by error message test" console="Console error" method="undefined" stack="HttpException:.*Was directly caused by:.*"/
 
     expect(consoleErrorMock).toHaveBeenCalledTimes(1)
     expect(consoleErrorMock).toHaveBeenCalledWith(expect.stringMatching(regex))
--- nest-city-account/src/utils/subservices/__tests__/line-logger.subservice.spec.ts
+++ nest-city-account/src/utils/subservices/__tests__/line-logger.subservice.spec.ts
@@ -26,7 +26,7 @@ describe('LineLoggerSubservice', () => {
     service[method]('test message')
 
     const regex = new RegExp(
-      `process="\\[Nest\\]" processPID="\\d+" datetime="\\d{2}\\/\\d{2}\\/\\d{4}, \\d{2}:\\d{2}:\\d{2}" severity="${severity}" context="LineLogger TEST" message="test message"`
+      `process="\\[Nest\\]" processPID="\\d+" datetime="\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z" severity="${severity}" context="LineLogger TEST" message="test message"`
     )
 
     expect(consoleSpy).toHaveBeenCalledWith(expect.stringMatching(regex))
@@ -38,7 +38,7 @@ describe('LineLoggerSubservice', () => {
     service.log({ foo: 'string' })
 
     const regex =
-      /process="\[Nest]" processPID="\d+" datetime="\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}" severity="LOG" context="LineLogger TEST" foo="string"/
+      /process="\[Nest]" processPID="\d+" datetime="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z" severity="LOG" context="LineLogger TEST" foo="string"/
 
     expect(consoleSpy).toHaveBeenCalledWith(expect.stringMatching(regex))
     expect(consoleSpy).toHaveBeenCalledTimes(1)

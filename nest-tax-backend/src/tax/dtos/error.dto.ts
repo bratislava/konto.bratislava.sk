9aae5c253f53af526691f81c0a41ea49059cb2ed
Jakub Hvolka <jhvolka@gmail.com>
2025-06-12 10:37:17 +0200

    Add new error types for installment validation

--- nest-tax-backend/src/tax/dtos/error.dto.ts
+++ nest-tax-backend/src/tax/dtos/error.dto.ts
@@ -3,6 +3,8 @@ export enum CustomErrorTaxTypesEnum {
   TAX_USER_NOT_FOUND = 'TAX_USER_NOT_FOUND',
   BIRTHNUMBER_NOT_EXISTS = 'BIRTHNUMBER_NOT_EXISTS',
   MISSING_INSTALLMENT_AMOUNTS = 'MISSING_INSTALLMENT_AMOUNTS',
+  INSTALLMENT_INCORRECT_COUNT = 'INSTALLMENT_INCORRECT_COUNT',
+  INSTALLMENT_UNEXPECTED_ERROR = 'INSTALLMENT_UNEXPECTED_ERROR',
   STATE_HOLIDAY_NOT_EXISTS = 'STATE_HOLIDAY_NOT_EXISTS',
 }
 
@@ -11,6 +13,8 @@ export enum CustomErrorTaxTypesResponseEnum {
   TAX_USER_NOT_FOUND = 'Tax user was not found',
   BIRTHNUMBER_NOT_EXISTS = 'Birthnumber not exists',
   MISSING_INSTALLMENT_AMOUNTS = 'Missing one or more installment amount',
+  INSTALLMENT_INCORRECT_COUNT = 'Number of installments does not equal 3.',
+  INSTALLMENT_UNEXPECTED_ERROR = 'An unexpected error occurred while calculating installments.',
   STATE_HOLIDAY_NOT_EXISTS = 'State holidays are not filled out for this year.',
 }
 

a160c22a2288d952f77bd0e9fef8ed21d724b72f
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-06-13 15:10:48 +0200

    Add providers / exports to FormsV2Module

--- nest-forms-backend/src/forms-v2/forms-v2.module.ts
+++ nest-forms-backend/src/forms-v2/forms-v2.module.ts
@@ -5,12 +5,21 @@ import PrismaModule from '../prisma/prisma.module'
 import ThrowerErrorGuard from '../utils/guards/thrower-error.guard'
 import { FormMigrationsController } from './controllers/form-migrations.controller'
 import { FormsV2Controller } from './controllers/forms-v2.controller'
+import { FormAccessGuard } from './guards/form-access.guard'
 import { CreateFormService } from './services/create-form.service'
+import { FormAccessService } from './services/form-access.service'
 import { FormMigrationsService } from './services/form-migrations.service'
 
 @Module({
   imports: [PrismaModule, AuthV2Module],
   controllers: [FormMigrationsController, FormsV2Controller],
-  providers: [FormMigrationsService, CreateFormService, ThrowerErrorGuard],
+  providers: [
+    FormMigrationsService,
+    FormAccessService,
+    FormAccessGuard,
+    CreateFormService,
+    ThrowerErrorGuard,
+  ],
+  exports: [FormAccessService, FormAccessGuard],
 })
 export class FormsV2Module {}

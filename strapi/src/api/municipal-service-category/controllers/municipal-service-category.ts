99acd017f720afae6622dca8da0758d291d3f302
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-06-23 14:32:30 +0200

    Strapi types for municipal services and announcements

---
+++ strapi/src/api/municipal-service-category/controllers/municipal-service-category.ts
@@ -0,0 +1,7 @@
+/**
+ * municipal-service-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::municipal-service-category.municipal-service-category');

e831cc6d018b81264001d8784a7787445cec46c3
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-07-02 15:41:59 +0200

    Fetch announcements serverside

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,5 +1,8 @@
 import { strapiClient } from '@clients/graphql-strapi'
-import { MunicipalServiceCardEntityFragment } from '@clients/graphql-strapi/api'
+import {
+  HomepageAnnouncementEntityFragment,
+  MunicipalServiceCardEntityFragment,
+} from '@clients/graphql-strapi/api'
 import IntroSection from 'components/forms/segments/AccountSections/IntroSection/IntroSection'
 import AccountPageLayout from 'components/layouts/AccountPageLayout'
 import { isDefined } from 'frontend/utils/general'
@@ -8,16 +11,49 @@ import { SsrAuthProviderHOC } from '../components/logic/SsrAuthContext'
 import { amplifyGetServerSideProps } from '../frontend/utils/amplifyServer'
 import { slovakServerSideTranslations } from '../frontend/utils/slovakServerSideTranslations'
 
+const filterValidAnnouncements = (
+  announcements: HomepageAnnouncementEntityFragment[],
+): HomepageAnnouncementEntityFragment[] => {
+  const now = new Date()
+
+  return announcements
+    .filter((announcement) => {
+      if (!announcement.attributes) {
+        return false
+      }
+
+      const { dateFrom, dateTo } = announcement.attributes
+      const fromDate = dateFrom ? new Date(dateFrom) : null
+      const toDate = dateTo ? new Date(dateTo) : null
+
+      const isAfterFromDate = !fromDate || now >= fromDate
+      const isBeforeToDate = !toDate || now <= toDate
+
+      return isAfterFromDate && isBeforeToDate
+    })
+    .slice(0, 3)
+}
+
 export const getServerSideProps = amplifyGetServerSideProps<AccountIntroPageProps>(async () => {
   const homepageQuery = await strapiClient.Homepage()
   const services = homepageQuery.homepage?.data?.attributes?.services?.data.filter(isDefined) ?? []
   const servicesLegalPerson =
     homepageQuery.homepage?.data?.attributes?.servicesLegalPerson?.data.filter(isDefined) ?? []
 
+  const allAnnouncements =
+    homepageQuery.homepage?.data?.attributes?.announcements?.data.filter(isDefined) ?? []
+  const allAnnouncementsLegalPerson =
+    homepageQuery.homepage?.data?.attributes?.announcementsLegalPerson?.data.filter(isDefined) ?? []
+
+  const announcements = filterValidAnnouncements(allAnnouncements)
+  const announcementsLegalPerson = filterValidAnnouncements(allAnnouncementsLegalPerson)
+
   return {
     props: {
       services,
       servicesLegalPerson,
+      announcements,
+      announcementsLegalPerson,
       ...(await slovakServerSideTranslations()),
     },
   }
@@ -26,12 +62,24 @@ export const getServerSideProps = amplifyGetServerSideProps<AccountIntroPageProp
 type AccountIntroPageProps = {
   services: MunicipalServiceCardEntityFragment[]
   servicesLegalPerson: MunicipalServiceCardEntityFragment[]
+  announcements: HomepageAnnouncementEntityFragment[]
+  announcementsLegalPerson: HomepageAnnouncementEntityFragment[]
 }
 
-const AccountIntroPage = ({ services, servicesLegalPerson }: AccountIntroPageProps) => {
+const AccountIntroPage = ({
+  services,
+  servicesLegalPerson,
+  announcements,
+  announcementsLegalPerson,
+}: AccountIntroPageProps) => {
   return (
     <AccountPageLayout>
-      <IntroSection services={services} servicesLegalPerson={servicesLegalPerson} />
+      <IntroSection
+        services={services}
+        servicesLegalPerson={servicesLegalPerson}
+        announcements={announcements}
+        announcementsLegalPerson={announcementsLegalPerson}
+      />
     </AccountPageLayout>
   )
 }

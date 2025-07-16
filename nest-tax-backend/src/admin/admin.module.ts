ecee4512e6fa020b07725f77f5e44323a5a11739
Adam Grund <adam.grund@gmail.com>
2025-07-16 17:11:33 +0200

    remove forgotten import

--- nest-tax-backend/src/admin/admin.module.ts
+++ nest-tax-backend/src/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { BloomreachModule } from 'src/bloomreach/bloomreach.module'
-import ClientsModule from 'src/clients/clients.module'
 import { NorisModule } from 'src/noris/noris.module'
 import { CityAccountSubservice } from 'src/utils/subservices/cityaccount.subservice'
 import { QrCodeSubservice } from 'src/utils/subservices/qrcode.subservice'

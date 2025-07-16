746b34a10a12f8edbd892260adf27d530819d95e
Erik Rehulka <erik.rehulka@bratislava.sk>
2025-07-16 13:14:35 +0200

    test of rfoResponse zod schema parsing

---
+++ nest-city-account/src/magproxy/__test__/magproxy.service.spec.ts
@@ -0,0 +1,71 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import ThrowerErrorGuard, { ErrorMessengerGuard } from '../../utils/guards/errors.guard'
+import { MagproxyService } from '../magproxy.service'
+import ClientsService from '../../clients/clients.service'
+import { createMock } from '@golevelup/ts-jest'
+import { mockRfoResponseListOneItems } from '../../rfo-by-birthnumber/dtos/__test__/rfoResponse.test'
+import { ResponseRfoPersonDto } from 'openapi-clients/magproxy'
+
+describe('MagproxyService', () => {
+  let service: MagproxyService
+
+  beforeAll(() => {
+    process.env = {
+      ...process.env,
+      MAGPROXY_URL: 'https://mock-new-magproxy.bratislava.sk',
+      MAGPROXY_AZURE_AD_URL:
+        'https://mock-login.microsoftonline.com/mock-azure-ad-id/oauth2/v2.0/token',
+      MAGPROXY_AZURE_CLIENT_ID: 'mock-magproxy-azure-client-id',
+      MAGPROXY_AZURE_CLIENT_SECRET: 'mock-magproxy-azure-secret',
+      MAGPROXY_AZURE_SCOPE: 'api://mock-azure-scope/.default',
+    }
+  })
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        MagproxyService,
+        ThrowerErrorGuard,
+        ErrorMessengerGuard,
+        { provide: ClientsService, useValue: createMock<ClientsService>() },
+      ],
+    }).compile()
+
+    service = module.get<MagproxyService>(MagproxyService)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
+
+  describe('validateRfoDataFormat', ()=> {
+    it('should return result for valid RFO data', () => {
+      const errorLogSpy = jest.spyOn(service['logger'], 'error').mockImplementation(() => {})
+      const response = service['validateRfoDataFormat'](mockRfoResponseListOneItems as unknown as ResponseRfoPersonDto[])
+
+      expect(errorLogSpy).not.toHaveBeenCalled()
+      expect(response).toEqual(mockRfoResponseListOneItems as unknown as ResponseRfoPersonDto[])
+    })
+
+    it('should log error for invalid RFO data, however still return', () => {
+      const errorLogSpy = jest.spyOn(service['logger'], 'error').mockImplementation(() => {})
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const mockRfoResponseListOneItemsInvalid = mockRfoResponseListOneItems as any
+      mockRfoResponseListOneItemsInvalid[0].rodnePriezviskaOsoby[0].meno = 1222 // Invalid data - name as number
+      const response = service['validateRfoDataFormat'](mockRfoResponseListOneItemsInvalid as unknown as ResponseRfoPersonDto[])
+
+      expect(errorLogSpy).toHaveBeenCalled()
+      expect(response).toEqual(mockRfoResponseListOneItemsInvalid)
+    })
+
+    it('should throw error if the data is not an array', () => {
+      const errorLogSpy = jest.spyOn(service['logger'], 'error').mockImplementation(() => {})
+      expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        service['validateRfoDataFormat']({} as any)
+      }).toThrow()
+
+      expect(errorLogSpy).toHaveBeenCalled()
+    })
+  })
+})
---
+++ nest-city-account/src/rfo-by-birthnumber/dtos/__test__/rfoResponse.test.ts
@@ -0,0 +1,144 @@
+export const mockRfoResponseListOneItems = [
+    {
+      "ifo": "26111333",
+      "rodneCislo": "010101/1111",
+      "datumNarodenia": "2000-01-01",
+      "rokNarodenia": 2000,
+      "miestoNarodeniaKod": 119777,
+      "miestoNarodenia": "Bratislava",
+      "statNarodeniaKod": 111,
+      "statNarodenia": "Slovenská republika",
+      "okresNarodeniaKod": 1111,
+      "okresNarodenia": "Bratislava I",
+      "rodnaMatrikaKod": null,
+      "rodnaMatrika": null,
+      "datumUmrtia": null,
+      "miestoUmrtiaKod": null,
+      "miestoUmrtia": null,
+      "statUmrtiaKod": null,
+      "statUmrtia": null,
+      "okresUmrtiaKod": null,
+      "okresUmrtia": null,
+      "umrtnaMatrikaKod": null,
+      "umrtnaMatrika": null,
+      "pohlavieOsobyKod": 1,
+      "pohlavieOsoby": "mužské",
+      "rodinnyStavKod": 2,
+      "rodinnyStav": "slobodný / slobodná",
+      "narodnostKod": null,
+      "narodnost": null,
+      "stupenZverejneniaUdajovKod": 0,
+      "stupenZverejneniaUdajov": "0",
+      "typOsobyKod": 1,
+      "typOsoby": "Občan s trvalým pobytom na území SR",
+      "menaOsoby": [
+        {
+          "meno": "Meno",
+          "poradieMena": 1
+        }
+      ],
+      "priezviskaOsoby": [
+        {
+          "meno": "Priezvisko",
+          "poradiePriezviska": 1
+        }
+      ],
+      "rodnePriezviskaOsoby": [
+        {
+          "meno": "Priezvisko",
+          "poradieRodnehoPriezviska": 1
+        }
+      ],
+      "titulyOsoby": {},
+      "statnePrislusnosti": [
+        {
+          "hodnotaKod": 211,
+          "hodnota": "Slovenská republika"
+        }
+      ],
+      "rodinniPrislusnici": [
+        {
+          "identifikatorVztahovejOsoby": "1112223",
+          "typRodinnehoVztahuKod": 1,
+          "typRodinnehoVztahu": "Rodičovstvo",
+          "datumVznikuVztahu": null,
+          "miestoVydaniaSobasnehoListuKod": null,
+          "miestoVydaniaSobasnehoListu": null,
+          "vztahRolaVztahovejOsobyKod": 1,
+          "vztahRolaVztahovejOsoby": "Otec",
+          "vztahRolaPrimarnejOsobyKod": 5,
+          "vztahRolaPrimarnejOsoby": "Dieťa",
+          "sobasnaMatrikaKod": null,
+          "sobasnaMatrika": null
+        },
+        {
+          "identifikatorVztahovejOsoby": "2223334",
+          "typRodinnehoVztahuKod": 1,
+          "typRodinnehoVztahu": "Rodičovstvo",
+          "datumVznikuVztahu": null,
+          "miestoVydaniaSobasnehoListuKod": null,
+          "miestoVydaniaSobasnehoListu": null,
+          "vztahRolaVztahovejOsobyKod": 2,
+          "vztahRolaVztahovejOsoby": "Matka",
+          "vztahRolaPrimarnejOsobyKod": 5,
+          "vztahRolaPrimarnejOsoby": "Dieťa",
+          "sobasnaMatrikaKod": null,
+          "sobasnaMatrika": null
+        }
+      ],
+      "pobytyOsoby": [
+        {
+          "typPobytu": 2,
+          "typPobytuMimoCiselnik": "Trvalý pobyt",
+          "datumPrihlaseniaPobytu": "2022-01-01",
+          "datumUkonceniaPobytu": null,
+          "nazovObce": "Bratislava-Nové Mesto",
+          "supisneCislo": 10,
+          "nazovCastiObce": null,
+          "nazovOkresu": "Bratislava III",
+          "nazovUlice": "Ulica",
+          "statMimoSr": null,
+          "nazovStatu": null,
+          "adresaMimoSr": null,
+          "okresMimoSr": null,
+          "obecMimoSr": null,
+          "castObceMimoSr": null,
+          "ulicaMimoSr": null,
+          "orientacneCisloMimoSr": null,
+          "supisneCisloMimoSr": null,
+          "urcenieMiestaVRamciBudovyMimoSr": null,
+          "pobytMimoSr": false,
+          "indexDomu": "611111     ",
+          "urcenieMiestaVRamciBudovy": "10",
+          "identifikatorAdresyRa": 10,
+          "vchodDomu": 10,
+          "dom": 10,
+          "ulica": 10,
+          "castObce": null,
+          "obec": 10,
+          "okres": 10,
+          "orientacneCislo": 10
+        }
+      ],
+      "zakazyPobytu": {},
+      "zruseniaVyhlaseniaOsobyZaMrtvu": {},
+      "doklady": [
+        {
+          "druhDokladuKod": 1,
+          "druhDokladu": "Cestovný pas",
+          "jednoznacnyIdentifikator": "AA0000000",
+          "udrzitela": true
+        },
+        {
+          "druhDokladuKod": 34,
+          "druhDokladu": "Občiansky preukaz",
+          "jednoznacnyIdentifikator": "555555 BB",
+          "udrzitela": true
+        }
+      ],
+      "statneObcianstva": {},
+      "obmedzeniaPravnejSposobilosti": {},
+      "systemyModifikujuceUdajeOsoby": {},
+      "zaujmovaOsoba": true
+    }
+  ]

2a756db94049f85bba5580b9e5a7388977e1c7b2
Marek Bodinger <marek.bodinger@bratislava.sk>
2025-07-18 15:22:54 +0200

    Display BE tax administrator when provided

--- next/components/forms/segments/AccountSections/TaxesFeesSection/TaxesFeesCards.tsx
+++ next/components/forms/segments/AccountSections/TaxesFeesSection/TaxesFeesCards.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 import cn from '../../../../../frontend/cn'
-import { AccountType } from '../../../../../frontend/dtos/accountDto'
-import { useSsrAuth } from '../../../../../frontend/hooks/useSsrAuth'
 import TaxesChannelChangeEffectiveNextYearAlert from './TaxesChannelChangeEffectiveNextYearAlert'
 import TaxesFeesDeliveryMethodBanner from './TaxesFeesDeliveryMethodBanner'
 import TaxesFeesDeliveryMethodCard from './TaxesFeesDeliveryMethodCard'
@@ -12,11 +10,9 @@ import { useTaxChannel } from './useTaxChannel'
 import { useTaxFeesSection } from './useTaxFeesSection'
 
 const TaxesFeesCards = () => {
-  const { accountType } = useSsrAuth()
-  const { strapiTaxAdministrator, setOfficialCorrespondenceChannelModalOpen } = useTaxFeesSection()
+  const { taxesData, strapiTaxAdministrator, setOfficialCorrespondenceChannelModalOpen } =
+    useTaxFeesSection()
   const strapiTax = useStrapiTax()
-  const displayTaxAdministratorCard =
-    strapiTaxAdministrator !== null && accountType === AccountType.FyzickaOsoba
   const { showEmailCommunicationBanner, channelChangeEffectiveNextYear } = useTaxChannel()
 
   const wrapperStyle = cn('flex flex-col gap-4', {
@@ -36,9 +32,10 @@ const TaxesFeesCards = () => {
             onDeliveryMethodChange={() => setOfficialCorrespondenceChannelModalOpen(true)}
           />
         )}
-        {displayTaxAdministratorCard && (
-          <TaxesFeesTaxAdministratorCard strapiTaxAdministrator={strapiTaxAdministrator} />
-        )}
+        <TaxesFeesTaxAdministratorCard
+          beTaxAdministrator={taxesData.taxAdministrator}
+          strapiTaxAdministrator={strapiTaxAdministrator}
+        />
       </div>
       {channelChangeEffectiveNextYear && (
         <TaxesChannelChangeEffectiveNextYearAlert strapiTax={strapiTax} />

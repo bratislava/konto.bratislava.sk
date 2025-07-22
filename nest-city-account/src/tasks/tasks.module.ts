afa1eb28998ba335d8750ef185c743284c607f04
Jakub Hvolka <jhvolka@gmail.com>
2025-07-22 11:21:51 +0200

    Add `TasksService` to providers array in `TasksModule`

--- nest-city-account/src/tasks/tasks.module.ts
+++ nest-city-account/src/tasks/tasks.module.ts
@@ -4,10 +4,11 @@ import { PhysicalEntityModule } from '../physical-entity/physical-entity.module'
 import ThrowerErrorGuard from '../utils/guards/errors.guard'
 import { TaxSubservice } from '../utils/subservices/tax.subservice'
 import ClientsModule from '../clients/clients.module'
+import { TasksService } from './tasks.service'
 
 @Module({
   imports: [PrismaModule, PhysicalEntityModule, ClientsModule],
-  providers: [ThrowerErrorGuard, TaxSubservice],
+  providers: [ThrowerErrorGuard, TaxSubservice, TasksService],
   exports: [],
   controllers: [],
 })

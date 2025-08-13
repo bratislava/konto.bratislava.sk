a6594d25cd07b8b34f7a81d53118d01f89bcea33
Jakub Hvolka <jhvolka@gmail.com>
2025-08-13 16:45:38 +0200

    Add method to retrieve taxes by birth number and status helper function

--- nest-tax-backend/src/tax/tax.service.ts
+++ nest-tax-backend/src/tax/tax.service.ts
@@ -18,11 +18,15 @@ import {
 import {
   ResponseGetTaxesBodyDto,
   ResponseGetTaxesDto,
+  ResponseGetTaxesListBodyDto,
+  ResponseGetTaxesListDto,
   ResponseInstallmentPaymentDetailDto,
   ResponseOneTimePaymentDetailsDto,
   ResponseTaxDto,
   ResponseTaxPayerReducedDto,
   ResponseTaxSummaryDetailDto,
+  TaxAvailabilityStatus,
+  TaxStatusEnum,
 } from './dtos/response.tax.dto'
 import { taxDetailsToPdf, taxTotalsToPdf } from './utils/helpers/pdf.helper'
 import { fixInstallmentTexts, getTaxStatus } from './utils/helpers/tax.helper'
@@ -35,6 +39,9 @@ import {
 const paymentCalendarThreshold = 6600
 
 const specificSymbol = '2025200000'
+
+const waitForTaxFromMonth = 3
+const waitForTaxTilMonth = 4
 
 @Injectable()
 export class TaxService {
@@ -235,6 +242,112 @@ export class TaxService {
       isInNoris: items.length > 0,
       items,
       taxAdministrator: taxPayer ? taxPayer.taxAdministrator : null,
+    }
+  }
+
+  getExistingTaxStatus = (
+    taxAmount: number,
+    paidAmount: number,
+  ): TaxStatusEnum => {
+    if (paidAmount === 0) {
+      return TaxStatusEnum.NOT_PAID
+    }
+
+    if (paidAmount === taxAmount) {
+      return TaxStatusEnum.PAID
+    }
+
+    if (paidAmount > taxAmount) {
+      return TaxStatusEnum.OVER_PAID
+    }
+
+    return TaxStatusEnum.PARTIALLY_PAID
+  }
+
+  async getListOfTaxesByBirthnumber(
+    birthNumber: string,
+  ): Promise<ResponseGetTaxesListDto> {
+    if (!birthNumber) {
+      throw this.throwerErrorGuard.ForbiddenException(
+        CustomErrorTaxTypesEnum.BIRTHNUMBER_NOT_EXISTS,
+        CustomErrorTaxTypesResponseEnum.BIRTHNUMBER_NOT_EXISTS,
+      )
+    }
+
+    const taxPayer = await this.prisma.taxPayer.findUnique({
+      where: {
+        birthNumber,
+      },
+      include: {
+        taxAdministrator: true,
+      },
+    })
+    const taxAdministrator = taxPayer ? taxPayer.taxAdministrator : null
+
+    const taxes = await this.prisma.tax.findMany({
+      where: {
+        taxPayer: {
+          birthNumber,
+        },
+      },
+      orderBy: {
+        year: 'desc',
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        amount: true,
+        year: true,
+      },
+    })
+    const currentYear = new Date().getFullYear()
+    const currentMonth = new Date().getMonth()
+    const createdAtLessThanAWeekAgo = taxPayer
+      ? dayjs(taxPayer.createdAt).isAfter(dayjs().subtract(1, 'week'))
+      : false
+
+    const shouldAddCurrentYear =
+      createdAtLessThanAWeekAgo ||
+      (currentMonth >= waitForTaxFromMonth && currentMonth < waitForTaxTilMonth)
+
+    if (taxes.length === 0) {
+      const availabilityStatus = shouldAddCurrentYear
+        ? TaxAvailabilityStatus.LOOKING_FOR_YOUR_TAX
+        : TaxAvailabilityStatus.TAX_NOT_ON_RECORD
+      return {
+        availabilityStatus,
+        items: [],
+        taxAdministrator,
+      }
+    }
+
+    const items: ResponseGetTaxesListBodyDto[] = await Promise.all(
+      taxes.map(async (tax) => {
+        const paid = await this.getAmountAlreadyPaidByTaxId(tax.id)
+        const amountToBePaid = tax.amount - paid
+        const status = this.getExistingTaxStatus(tax.amount, paid)
+        return {
+          createdAt: tax.createdAt,
+          year: tax.year,
+          amountToBePaid,
+          status,
+        }
+      }),
+    )
+
+    const currentYearTaxExists = items.some((item) => item.year === currentYear)
+
+    if (!currentYearTaxExists && shouldAddCurrentYear) {
+      items.unshift({
+        year: currentYear,
+        status: TaxStatusEnum.AWAITING_PROCESSING,
+      })
+    }
+
+    return {
+      availabilityStatus: TaxAvailabilityStatus.AVAILABLE,
+      items,
+      taxAdministrator,
     }
   }
 

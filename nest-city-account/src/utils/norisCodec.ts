ac750f85cee36eee4f443d6bfe7ab1c2471b818f
Jakub Hvolka <jhvolka@gmail.com>
2025-08-25 16:54:14 +0200

    Refactor `DeliveryMethodCodec` to simplify enum transformations and align with updated `zod` syntax.

--- nest-city-account/src/utils/norisCodec.ts
+++ nest-city-account/src/utils/norisCodec.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
-import {DeliveryMethodEnum} from "@prisma/client";
-import {DeliveryMethodNoris} from "./types/tax.types";
+import { DeliveryMethodEnum } from '@prisma/client'
+import { DeliveryMethodNoris } from './types/tax.types'
 
 const DELIVERY_METHOD_MAPPING = {
   [DeliveryMethodEnum.CITY_ACCOUNT]: DeliveryMethodNoris.CITY_ACCOUNT,
@@ -13,16 +13,12 @@ const REVERSE_DELIVERY_METHOD_MAPPING = Object.fromEntries(
 ) as Record<DeliveryMethodNoris, DeliveryMethodEnum>
 
 export const DeliveryMethodCodec = z.codec(
-  DeliveryMethodEnum,
-  DeliveryMethodNoris,
+  z.nativeEnum(DeliveryMethodEnum).nullable().optional(),
+  z.nativeEnum(DeliveryMethodNoris),
   {
-  decode: z
-    .enum(DeliveryMethodEnum)
-    .nullable()
-    .transform((value) => (value && DELIVERY_METHOD_MAPPING[value]) || DeliveryMethodNoris.POSTAL),
-
-  encode: z
-    .enum(DeliveryMethodNoris)
-    .transform((value) => REVERSE_DELIVERY_METHOD_MAPPING[value] || null)
-})
-
+    decode: (value: DeliveryMethodEnum | null | undefined): DeliveryMethodNoris =>
+      (value && DELIVERY_METHOD_MAPPING[value]) || DeliveryMethodNoris.POSTAL,
+    encode: (value: DeliveryMethodNoris): DeliveryMethodEnum | null =>
+      REVERSE_DELIVERY_METHOD_MAPPING[value] || null,
+  }
+)

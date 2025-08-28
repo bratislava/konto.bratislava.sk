265a3565f2ca3ba12c722905cac46c858d7bfd90
Jakub Hvolka <jhvolka@gmail.com>
2025-08-28 15:47:52 +0200

    Refactor: replace inline env vars with `ConfigService` for municipal tax lock date

--- nest-city-account/src/utils/constants/tax-deadline.ts
+++ nest-city-account/src/utils/constants/tax-deadline.ts
@@ -1,8 +1,19 @@
 import { DateTime } from 'luxon'
+import { ConfigService } from "@nestjs/config";
+
+const MUNICIPAL_TAX_LOCK_MONTH = Number(new ConfigService().getOrThrow('MUNICIPAL_TAX_LOCK_MONTH'))
+const MUNICIPAL_TAX_LOCK_DAY = Number(new ConfigService().getOrThrow('MUNICIPAL_TAX_LOCK_DAY') )
 
 export function getTaxDeadlineDate(): Date {
   return DateTime.now()
     .setZone('Europe/Bratislava')
-    .set({ month: Number(process.env.MUNICIPAL_TAX_LOCK_MONTH), day: Number(process.env.MUNICIPAL_TAX_LOCK_DAY), hour: 0, minute: 0, second: 0, millisecond: 0 })
+    .set({
+      month: MUNICIPAL_TAX_LOCK_MONTH,
+      day: MUNICIPAL_TAX_LOCK_DAY,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+    })
     .toJSDate()
 }

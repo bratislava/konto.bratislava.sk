58f23d5284f95a8e3cd562bd7e07c6f10e82ca99
Jakub Hvolka <jhvolka@gmail.com>
2025-09-01 14:45:18 +0200

    Refactor: remove lazy-loaded `ConfigService` from tax deadline utils

--- nest-city-account/src/utils/constants/tax-deadline.ts
+++ nest-city-account/src/utils/constants/tax-deadline.ts
@@ -1,14 +1,8 @@
 import { DateTime } from 'luxon'
 import { ConfigService } from "@nestjs/config";
 
-let configService: ConfigService | null = null
-
-function getConfigService(): ConfigService {
-  return configService ??= new ConfigService()
-}
-
 export function getTaxDeadlineDate(): Date {
-  const config = getConfigService()
+  const config = new ConfigService()
   const MUNICIPAL_TAX_LOCK_MONTH = Number(config.getOrThrow('MUNICIPAL_TAX_LOCK_MONTH'))
   const MUNICIPAL_TAX_LOCK_DAY = Number(config.getOrThrow('MUNICIPAL_TAX_LOCK_DAY') )
 

fbcc4bec522c2dc53e56b7a1b2f8902f5a54cf29
Erik Rehulka <erik.rehulka@bratislava.sk>
2025-09-10 13:24:03 +0200

    Throw Error 500 if someone tries to send a form which is not registered in slovensko.sk

--- nest-forms-backend/src/forms-v2/guards/form-send-only-registered.guard.ts
+++ nest-forms-backend/src/forms-v2/guards/form-send-only-registered.guard.ts
@@ -8,20 +8,15 @@ import {
 } from '../../forms/forms.errors.enum'
 import PrismaService from '../../prisma/prisma.service'
 import ThrowerErrorGuard from '../../utils/guards/thrower-error.guard'
-import { LineLoggerSubservice } from '../../utils/subservices/line-logger.subservice'
 import { FormSendService } from '../services/form-send.service'
 
 @Injectable()
 export class FormSendOnlyRegisteredGuard implements CanActivate {
-  private readonly logger: LineLoggerSubservice
-
   constructor(
     private readonly formSendService: FormSendService,
     private readonly prismaService: PrismaService,
     private readonly throwerErrorGuard: ThrowerErrorGuard,
-  ) {
-    this.logger = new LineLoggerSubservice(FormSendOnlyRegisteredGuard.name)
-  }
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>()
@@ -46,13 +41,10 @@ export class FormSendOnlyRegisteredGuard implements CanActivate {
       await this.formSendService.isFormRegisteredInSlovenskoSk(formDefinition)
 
     if (!result) {
-      // log it so we will be alerted in grafana
-      this.logger.error(
-        this.throwerErrorGuard.InternalServerErrorException(
-          FormsErrorsEnum.FORM_NOT_REGISTERED_IN_SLOVENSKO_SK,
-          FormsErrorsResponseEnum.FORM_NOT_REGISTERED_IN_SLOVENSKO_SK,
-          `Form definition with slug ${form.formDefinitionSlug} is not registered in slovensko.sk.`,
-        ),
+      throw this.throwerErrorGuard.InternalServerErrorException(
+        FormsErrorsEnum.FORM_NOT_REGISTERED_IN_SLOVENSKO_SK,
+        FormsErrorsResponseEnum.FORM_NOT_REGISTERED_IN_SLOVENSKO_SK,
+        `Form definition with slug ${form.formDefinitionSlug} is not registered in slovensko.sk.`,
       )
     }
 

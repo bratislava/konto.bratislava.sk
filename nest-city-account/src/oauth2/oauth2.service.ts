3049ff7a37fbbc64b41292191fbf6d3183bb6ae7
Lukas Babula <lukas.babula@gmail.com>
2025-10-31 15:11:44 +0100

    Add from parameter to oauth login redirect

--- nest-city-account/src/oauth2/oauth2.service.ts
+++ nest-city-account/src/oauth2/oauth2.service.ts
@@ -198,6 +198,11 @@ export class OAuth2Service {
     if (request.state) {
       redirectUrl.searchParams.set('state', request.state)
     }
+    redirectUrl.searchParams.set(
+      'from',
+      this.configService.get<string>('OAUTH2_LOGIN_FROM_URL') ||
+        'https://nest-city-account.dev.bratislava.sk'
+    )
     return redirectUrl.toString()
   }
 

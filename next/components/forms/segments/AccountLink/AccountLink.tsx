37612ba5d1f2f1a51ff44b1087fbe8c00a03d196
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-10-31 10:30:10 +0100

    Change AccountLink href type to be able to use UrlObject

--- next/components/forms/segments/AccountLink/AccountLink.tsx
+++ next/components/forms/segments/AccountLink/AccountLink.tsx
@@ -1,25 +1,30 @@
-import Button from 'components/forms/simple-components/Button'
+import Link, { LinkProps } from 'next/link'
+
+import cn from '../../../../frontend/cn'
 
 interface Props {
   label: string
   description: string
-  href: string
+  href: LinkProps['href']
   variant?: 'black' | 'category'
 }
 
 const AccountLink = ({ description, label, href, variant = 'black' }: Props) => {
-  const name = href.replaceAll('/', '')
+  // TODO OAuth revisit data-cy attribute and href.toString()
+  // const name = typeof href === 'string' ? href.replaceAll('/', '') : href.href?.replaceAll('/', '')
   return (
     <div className="flex flex-col justify-between md:flex-row">
       <div className="text-16-semibold text-gray-800">{description}</div>
-      <Button
-        size="sm"
-        variant={`link-${variant}`}
+      <Link
         href={href}
-        label={label}
-        hrefIconHidden
-        data-cy={`${name}-button`}
-      />
+        // data-cy={`${name}-button`}
+        className={cn('font-semibold underline', {
+          'text-category-700 hover:text-gray-600 focus:text-category-800': variant === 'black',
+          'text-gray-700 hover:text-category-600 focus:text-gray-800': variant === 'category',
+        })}
+      >
+        {label}
+      </Link>
     </div>
   )
 }

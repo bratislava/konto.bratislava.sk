c0246cfa24f57c93b85704fd31c1835c16a23f87
Adam Grund <adam.grund@gmail.com>
2025-11-25 17:15:12 +0100

    running npm run dev in strapi project changed these files

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,4 +1,17 @@
-import type { Schema, Attribute } from '@strapi/strapi'
+import type { Attribute, Schema } from '@strapi/strapi'
+
+export interface BlocksFormLandingPage extends Schema.Component {
+  collectionName: 'components_blocks_form_landing_pages'
+  info: {
+    description: ''
+    displayName: 'Form landing page'
+  }
+  attributes: {
+    formCta: Attribute.Component<'blocks.form-landing-page-form-cta'> & Attribute.Required
+    linkCtas: Attribute.Component<'blocks.form-landing-page-link-cta', true>
+    text: Attribute.RichText
+  }
+}
 
 export interface BlocksFormLandingPageFormCta extends Schema.Component {
   collectionName: 'components_blocks_form_landing_page_form_ctas'
@@ -6,9 +19,9 @@ export interface BlocksFormLandingPageFormCta extends Schema.Component {
     displayName: 'Form landing page form CTA'
   }
   attributes: {
-    title: Attribute.String & Attribute.Required
-    text: Attribute.Text
     buttonLabel: Attribute.String & Attribute.Required
+    text: Attribute.Text
+    title: Attribute.String & Attribute.Required
   }
 }
 
@@ -18,35 +31,22 @@ export interface BlocksFormLandingPageLinkCta extends Schema.Component {
     displayName: 'Form landing page link CTA'
   }
   attributes: {
-    title: Attribute.String & Attribute.Required
-    text: Attribute.Text
     buttonLabel: Attribute.String & Attribute.Required
+    text: Attribute.Text
+    title: Attribute.String & Attribute.Required
     url: Attribute.String & Attribute.Required
-  }
-}
-
-export interface BlocksFormLandingPage extends Schema.Component {
-  collectionName: 'components_blocks_form_landing_pages'
-  info: {
-    displayName: 'Form landing page'
-    description: ''
-  }
-  attributes: {
-    text: Attribute.RichText
-    linkCtas: Attribute.Component<'blocks.form-landing-page-link-cta', true>
-    formCta: Attribute.Component<'blocks.form-landing-page-form-cta'> & Attribute.Required
   }
 }
 
 export interface BlocksHelpCategory extends Schema.Component {
   collectionName: 'components_blocks_help_categories'
   info: {
-    displayName: 'Help category'
     description: ''
+    displayName: 'Help category'
   }
   attributes: {
-    title: Attribute.String & Attribute.Required
     items: Attribute.Component<'blocks.help-item', true> & Attribute.Required
+    title: Attribute.String & Attribute.Required
   }
 }
 
@@ -56,16 +56,16 @@ export interface BlocksHelpItem extends Schema.Component {
     displayName: 'Help item'
   }
   attributes: {
-    title: Attribute.String & Attribute.Required
     content: Attribute.RichText & Attribute.Required
+    title: Attribute.String & Attribute.Required
   }
 }
 
 export interface GeneralAlert extends Schema.Component {
   collectionName: 'components_general_alerts'
   info: {
-    displayName: 'Alert'
     description: ''
+    displayName: 'Alert'
   }
   attributes: {
     content: Attribute.RichText & Attribute.Required
@@ -77,9 +77,9 @@ export interface GeneralAlert extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'blocks.form-landing-page': BlocksFormLandingPage
       'blocks.form-landing-page-form-cta': BlocksFormLandingPageFormCta
       'blocks.form-landing-page-link-cta': BlocksFormLandingPageLinkCta
-      'blocks.form-landing-page': BlocksFormLandingPage
       'blocks.help-category': BlocksHelpCategory
       'blocks.help-item': BlocksHelpItem
       'general.alert': GeneralAlert

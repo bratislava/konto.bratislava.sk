6f0318adf7d794adc7e9d9d0a3d944e59a5df81d
Lukas Babula <lukas.babula@gmail.com>
2025-12-05 22:11:19 +0100

    Tighten nases natural person and corporate body shape conditions

--- nest-forms-backend/src/nases/utils-services/tokens.nases.service.ts
+++ nest-forms-backend/src/nases/utils-services/tokens.nases.service.ts
@@ -71,8 +71,9 @@ export function isUpvsNaturalPerson(
   contact: UpvsNaturalPerson | UpvsCorporateBody,
 ): contact is UpvsNaturalPerson {
   return (
-    contact.type === 'natural_person' ||
-    ('natural_person' in contact && contact.natural_person !== undefined)
+    contact.type === 'natural_person' &&
+    'natural_person' in contact &&
+    contact.natural_person !== undefined
   )
 }
 
@@ -80,8 +81,9 @@ export function isUpvsCorporateBody(
   contact: UpvsNaturalPerson | UpvsCorporateBody,
 ): contact is UpvsCorporateBody {
   return (
-    contact.type === 'legal_entity' ||
-    ('corporate_body' in contact && contact.corporate_body !== undefined)
+    contact.type === 'legal_entity' &&
+    'corporate_body' in contact &&
+    contact.corporate_body !== undefined
   )
 }
 

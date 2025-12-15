cae8f25f91b8e4c9d6fde818d97403431e83bfa3
Jakub Hvolka <jhvolka@gmail.com>
2025-12-15 17:11:10 +0100

    Mock environment variables in `MailgunService` tests to improve isolation and reliability.

--- nest-city-account/src/mailgun/mailgun.service.spec.ts
+++ nest-city-account/src/mailgun/mailgun.service.spec.ts
@@ -4,6 +4,14 @@ import { MailgunService } from './mailgun.service'
 describe('MailgunService', () => {
   let service: MailgunService
 
+  const ORIGINAL_ENV = process.env
+
+  beforeAll(() => {
+    process.env = { ...ORIGINAL_ENV }
+    process.env.MAILGUN_API_KEY = 'test-mailgun-api-key'
+    process.env.DEFAULT_MAILGUN_DOMAIN = 'test.example.com'
+  })
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MailgunService],

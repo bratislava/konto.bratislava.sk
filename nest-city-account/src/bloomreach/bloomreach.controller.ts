11eb05ad07cd56927baf1b7f8350ffb64348c6c9
Jakub Hvolka <jhvolka@gmail.com>
2025-12-15 16:43:21 +0100

    Add `BloomreachController` with endpoint for retrieving tax notification agreement PDF by hash

---
+++ nest-city-account/src/bloomreach/bloomreach.controller.ts
@@ -0,0 +1,69 @@
+import { Body, Controller, Get, HttpCode, Param, Post, Query, Res } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiQuery, ApiBody } from '@nestjs/swagger'
+import { Response } from 'express'
+import { ResponseInternalServerErrorDto } from '../utils/guards/dtos/error.dto'
+import ThrowerErrorGuard from '../utils/guards/errors.guard'
+import { BloomreachErrorEnum, BloomreachErrorResponseEnum } from './bloomreach.error.enum'
+import { BloomreachService } from './bloomreach.service'
+
+@Controller('bloomreach')
+export class BloomreachController {
+  constructor(
+    private readonly throwerErrorGuard: ThrowerErrorGuard,
+    private readonly bloomreachService: BloomreachService
+  ) {}
+
+  @HttpCode(200)
+  @ApiOperation({
+    summary: 'Get tax notification agreement PDF by hash.',
+  })
+  @ApiQuery({
+    name: 'hash',
+    required: true,
+    description: 'Unique hash that corresponds to a PDF file',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'PDF file with tax notification agreement.',
+    content: {
+      'application/pdf': {
+        schema: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Hash parameter is missing or invalid',
+    type: ResponseInternalServerErrorDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'PDF not found for the provided hash',
+    type: ResponseInternalServerErrorDto,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+    type: ResponseInternalServerErrorDto,
+  })
+  @Get('get-notify-agreement-pdf/:hash')
+  async getTaxDetailByYear(@Param('hash') hash: string, @Res() res: Response) {
+    if (!hash) {
+      throw this.throwerErrorGuard.BadRequestException(
+        BloomreachErrorEnum.HASH_NOT_PROVIDED,
+        BloomreachErrorResponseEnum.HASH_NOT_PROVIDED
+      )
+    }
+
+    const file = await this.bloomreachService.getFileByHash(hash)
+    // Set response headers for PDF
+    res.setHeader('Content-Type', file.contentType)
+    res.setHeader('Content-Disposition', `attachment; filename="${file.filename}"`)
+
+    res.send(file.data)
+  }
+}
--- nest-city-account/src/bloomreach/bloomreach.module.ts
+++ nest-city-account/src/bloomreach/bloomreach.module.ts
@@ -5,11 +5,12 @@ import { CognitoSubservice } from '../utils/subservices/cognito.subservice'
 import ThrowerErrorGuard from '../utils/guards/errors.guard'
 import { PrismaModule } from '../prisma/prisma.module'
 import { PdfConverterModule } from './pdf-converter/pdf-converter.module'
+import { BloomreachController } from './bloomreach.controller';
 
 @Module({
   imports: [PrismaModule, PdfConverterModule],
   providers: [BloomreachService, CognitoSubservice, ThrowerErrorGuard],
   exports: [BloomreachService],
-  controllers: [],
+  controllers: [BloomreachController],
 })
 export class BloomreachModule {}

9b2b78254854f0d09ecb5f0caefc5eddfaec5c4c
Jakub Hvolka <jhvolka@gmail.com>
2025-12-15 16:42:46 +0100

    Add `BloomreachErrorEnum` and `BloomreachErrorResponseEnum` for error handling in Bloomreach module

---
+++ nest-city-account/src/bloomreach/bloomreach.error.enum.ts
@@ -0,0 +1,7 @@
+export enum BloomreachErrorEnum {
+  HASH_NOT_PROVIDED = 'HASH_NOT_PROVIDED',
+}
+
+export enum BloomreachErrorResponseEnum {
+  HASH_NOT_PROVIDED = 'Hash was not provided.',
+}
--- nest-city-account/src/utils/guards/dtos/error.dto.ts
+++ nest-city-account/src/utils/guards/dtos/error.dto.ts
@@ -9,6 +9,7 @@ import { AdminErrorsEnum } from '../../../admin/admin.errors.enum'
 import { MagproxyErrorsEnum } from '../../../magproxy/magproxy.errors.enum'
 import { DeliveryMethodErrorsEnum } from './delivery-method.error'
 import { CustomErrorAdminTypesEnum } from '../../../admin/dtos/error.dto'
+import { BloomreachErrorEnum } from '../../../bloomreach/bloomreach.error.enum'
 
 // copied over from nest-forms-backend
 export enum ErrorsEnum {
@@ -54,6 +55,7 @@ export type CustomErrorEnums =
   | SendToQueueErrorsEnum
   | DeliveryMethodErrorsEnum
   | CustomErrorAdminTypesEnum
+  | BloomreachErrorEnum
 
 export class ErrorSymbols {
   static readonly alert: unique symbol = Symbol('alert')

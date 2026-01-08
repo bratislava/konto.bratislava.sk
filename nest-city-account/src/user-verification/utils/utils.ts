ec3dbf1b3fa19c1f3be953fe4130c7392f7cc6ac
Adam Grund <adam.grund@gmail.com>
2026-01-08 09:54:26 +0100

    remove undefined value from utils functions

--- nest-city-account/src/user-verification/utils/utils.ts
+++ nest-city-account/src/user-verification/utils/utils.ts
@@ -1,7 +1,7 @@
-export const extractBirthNumberFromUri = (uri: string): string | undefined => {
-  return uri.split('_')[0].split('/').at(-1)
+export const extractBirthNumberFromUri = (uri: string): string => {
+  return uri.split('_')[0].split('/').at(-1)!
 }
 
-export const extractIcoFromUri = (uri: string): string | undefined => {
-  return uri.split('/').at(-1)
+export const extractIcoFromUri = (uri: string): string => {
+  return uri.split('/').at(-1)!
 }

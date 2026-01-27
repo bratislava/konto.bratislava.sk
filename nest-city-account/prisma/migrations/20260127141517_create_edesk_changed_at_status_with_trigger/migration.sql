058f6ea707fd00ae14c90159d3f55b22ac824bfd
Jakub Hvolka <jhvolka@gmail.com>
2026-01-27 17:13:46 +0100

    Rename `activeEdeskUpdatedAt` to `edeskStatusChangedAt` and add triggers for automatic and restricted updates.

---
+++ nest-city-account/prisma/migrations/20260127141517_create_edesk_changed_at_status_with_trigger/migration.sql
@@ -0,0 +1,45 @@
+-- Rename activeEdeskUpdatedAt to edeskStatusChangedAt
+Alter TABLE "PhysicalEntity"
+    RENAME COLUMN "activeEdeskUpdatedAt" to "edeskStatusChangedAt";
+
+
+-- Add trigger to update edeskStatusChangedAt if activeEdesk changes
+CREATE OR REPLACE FUNCTION physical_entity_set_edesk_changed_at()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NEW."activeEdesk" IS DISTINCT FROM OLD."activeEdesk" THEN
+        NEW."edeskStatusChangedAt" := now();
+    ELSE
+        NEW."edeskStatusChangedAt" := OLD."edeskStatusChangedAt";
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trg_physical_entity_set_edesk_changed_at
+    BEFORE UPDATE OF "activeEdesk"
+    ON "PhysicalEntity"
+    FOR EACH ROW
+EXECUTE PROCEDURE physical_entity_set_edesk_changed_at();
+
+
+-- Prevent manual updates of edeskStatusChangedAt
+CREATE OR REPLACE FUNCTION physical_entity_prevent_manual_edesk_changed_at()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    NEW."edeskStatusChangedAt" := OLD."edeskStatusChangedAt";
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS trg_physical_entity_prevent_manual_edesk_changed_at ON "PhysicalEntity";
+CREATE TRIGGER trg_physical_entity_prevent_manual_edesk_changed_at
+    BEFORE UPDATE OF "edeskStatusChangedAt" ON "PhysicalEntity"
+    FOR EACH ROW
+EXECUTE PROCEDURE physical_entity_prevent_manual_edesk_changed_at();

acb99f0e7ba3f0b1320618cab2c88aa4a68174ec
Adam <jakubicka.adam@gmail.com>
2026-01-27 13:53:38 +0100

    Separate menu-items logic into useMenu.tsx

--- next/components/layouts/AccountPageLayout.tsx
+++ next/components/layouts/AccountPageLayout.tsx
@@ -1,32 +1,9 @@
-import {
-  HelpIcon,
-  HomeIcon,
-  LogoutIcon,
-  MySubmissionsIcon,
-  PaymentIcon,
-  ProfileIcon,
-  ServicesIcon,
-} from '@assets/ui-icons'
 import { useResizeObserver } from '@react-aria/utils'
-import NavBar, { MenuSectionItemBase } from 'components/forms/segments/NavBar/NavBar'
-import { MenuItemBase } from 'components/forms/simple-components/MenuDropdown/MenuDropdown'
-import { useConditionalFormRedirects } from 'components/forms/useFormRedirects'
-import { useRouter } from 'next/router'
+import NavBar from 'components/forms/segments/NavBar/NavBar'
+import useMenu from 'components/forms/segments/NavBar/useMenu'
+import cn from 'frontend/cn'
 import { useTranslation } from 'next-i18next'
 import { ReactNode, useRef, useState } from 'react'
-
-import { ROUTES } from '../../frontend/api/constants'
-import cn from '../../frontend/cn'
-import { useQueryParamRedirect } from '../../frontend/hooks/useQueryParamRedirect'
-import { useSsrAuth } from '../../frontend/hooks/useSsrAuth'
-import { useSignOut } from '../../frontend/utils/amplifyClient'
-import { isDefined } from '../../frontend/utils/general'
-
-type AccountPageLayoutBase = {
-  className?: string
-  children: ReactNode
-  hiddenHeaderNav?: boolean
-}
 
 declare module 'react' {
   interface CSSProperties {
@@ -34,18 +11,21 @@ declare module 'react' {
   }
 }
 
-const AccountPageLayout = ({ className, children, hiddenHeaderNav }: AccountPageLayoutBase) => {
-  const { getRouteWithCurrentUrlRedirect } = useQueryParamRedirect()
-  const { isSignedIn, isLegalEntity } = useSsrAuth()
-  const { signOut } = useSignOut()
+type Props = {
+  children: ReactNode
+  hiddenHeaderNav?: boolean
+  className?: string
+}
 
-  const router = useRouter()
+const AccountPageLayout = ({ className, children, hiddenHeaderNav }: Props) => {
+  const { t } = useTranslation('account')
+
   // https://stackoverflow.com/a/59253905
   const [mainScrollTopMargin, setMainScrollTopMargin] = useState(0)
 
-  // It is not possible to measure the height of header directly, because it is `display: contents`, the header also
-  // might include status bar, that we don't want to include in the height calculation because it hides when scrolling
-  // (as it is not sticky)
+  // It is not possible to measure the height of header directly, because it is `display: contents`.
+  // The header also might include status bar, that we don't want to include in the height calculation 
+  // because it hides when scrolling (as it is not sticky)
   const desktopNavbarRef = useRef<HTMLDivElement>(null)
   const mobileNavbarRef = useRef<HTMLDivElement>(null)
 
@@ -61,104 +41,14 @@ const AccountPageLayout = ({ className, children, hiddenHeaderNav }: AccountPage
   useResizeObserver({ ref: desktopNavbarRef, onResize: handleHeaderResize })
   useResizeObserver({ ref: mobileNavbarRef, onResize: handleHeaderResize })
 
-  const { t } = useTranslation('account')
-
-  // we need to keep the work in progress of the open form if navigating away form it
-  const optionalFormRedirectsContext = useConditionalFormRedirects()
-  const login = optionalFormRedirectsContext
-    ? () => optionalFormRedirectsContext.login()
-    : async () => {
-        await router.push(getRouteWithCurrentUrlRedirect(ROUTES.LOGIN))
-      }
-
-  const register = optionalFormRedirectsContext
-    ? () => optionalFormRedirectsContext.register()
-    : async () => {
-        await router.push(getRouteWithCurrentUrlRedirect(ROUTES.REGISTER))
-      }
-
-  const sectionsList: MenuSectionItemBase[] = [
-    {
-      id: 0,
-      title: t('account_section_intro.navigation'),
-      icon: <HomeIcon className="size-6" />,
-      url: '/',
-    },
-    {
-      id: 1,
-      title: t('account_section_services.navigation'),
-      icon: <ServicesIcon className="size-6" />,
-      url: ROUTES.MUNICIPAL_SERVICES,
-    },
-    {
-      id: 2,
-      title: t('account_section_applications.navigation'),
-      icon: <MySubmissionsIcon className="size-6" />,
-      url: ROUTES.MY_APPLICATIONS,
-    },
-    {
-      id: 3,
-      title: t('account_section_payment.title'),
-      icon: <PaymentIcon className="size-6" />,
-      url: ROUTES.TAXES_AND_FEES,
-    },
-    {
-      id: 4,
-      title: t('account_section_help.navigation'),
-      icon: <HelpIcon className="size-6" />,
-      url: ROUTES.HELP,
-    },
-  ].filter(isDefined)
-
-  // Hide taxes and fees section for legal entities
-  const filteredSections = sectionsList.filter(
-    (section) => !(isLegalEntity && section.url === ROUTES.TAXES_AND_FEES),
-  )
-
-  // TODO consider using this in desktop menu
-  const menuItems: MenuItemBase[] = isSignedIn
-    ? [
-        {
-          id: 0,
-          title: t('menu_links.profile'),
-          icon: <ProfileIcon className="size-5" />,
-          url: ROUTES.USER_PROFILE,
-        },
-        {
-          id: 1,
-          title: t('menu_links.help'),
-          icon: <HelpIcon className="size-5" />,
-          url: ROUTES.HELP,
-        },
-        {
-          id: 2,
-          title: t('menu_links.logout'),
-          icon: <LogoutIcon className="size-5 text-negative-700" />,
-          onPress: () => signOut(),
-          itemClassName: 'bg-negative-50',
-        },
-      ]
-    : [
-        {
-          id: 0,
-          title: t('menu_links.login'),
-          icon: <ProfileIcon className="size-5" />,
-          onPress: login,
-        },
-        {
-          id: 1,
-          title: t('menu_links.register'),
-          icon: <ProfileIcon className="size-5" />,
-          onPress: register,
-        },
-      ]
+  const { menuSections, menuItems } = useMenu()
 
   return (
     <div className={cn('flex min-h-screen flex-col', className)}>
-      {/* `contents` is here for sticky elements inside to work */}
+      {/* 'contents' class in header enables sticky elements inside it to work */}
       <header className="relative z-30 contents">
         <NavBar
-          sectionsList={filteredSections}
+          sectionsList={menuSections}
           menuItems={menuItems}
           navHidden
           hiddenHeaderNav={hiddenHeaderNav}

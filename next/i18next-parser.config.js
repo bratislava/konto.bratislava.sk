4fcf85b9ff3582944a43fd07e410a1b08c468c16
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-02-01 17:46:54 +0100

    Move and cleanup CorrespondenceAddressForm translations

---
+++ next/i18next-parser.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  locales: ['sk'], // this should be in sync with next-i18next.config.js - TODO get it from one place?
+  input: './{components,pages,frontend}/**/*.{tsx,ts}',
+  output: './public/locales/$LOCALE/$NAMESPACE.json',
+  // if set to true preserves old values in a separate json file
+  createOldCatalogs: false,
+  sort: true,
+  // makes the translation json file flat
+  keySeparator: false,
+  keepRemoved: [
+    /^(?:account:)?auth\.fields\.[^.]+_(format|required)$/,
+    /^account:correspondence_address_change\.fields\.(street_address|locality|postal_code)_(format|required)$/,
+  ],
+}

4452809d187f54168045f1d44305073056c5c153
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-02-02 14:03:12 +0100

    Setup i18next-parser (lost commit during rebase)

--- next/i18next-parser.config.js
+++ next/i18next-parser.config.js
@@ -1,3 +1,4 @@
+// Docs: https://github.com/i18next/i18next-parser?tab=readme-ov-file#options
 module.exports = {
   locales: ['sk'], // this should be in sync with next-i18next.config.js - TODO get it from one place?
   input: './{components,pages,frontend}/**/*.{tsx,ts}',
@@ -7,6 +8,7 @@ module.exports = {
   sort: true,
   // makes the translation json file flat
   keySeparator: false,
+  // Translations in account forms (registration, login...) are thrown away during parsing. This is quick fix, how to keep them.
   keepRemoved: [
     /^(?:account:)?auth\.fields\.[^.]+_(format|required)$/,
     /^account:correspondence_address_change\.fields\.(street_address|locality|postal_code)_(format|required)$/,

abdb0caa3d7f0375f61b3d4973a7c8a6080eb4cc
Erik Rehulka <erik.rehulka@bratislava.sk>
2026-02-06 13:41:54 +0100

    fix failing tsc

--- nest-tax-backend/src/clients/clients.service.ts
+++ nest-tax-backend/src/clients/clients.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { createCityAccountClient } from 'openapi-clients/city-account'
+import {
+  createCityAccountClient,
+  type CityAccountClient,
+} from 'openapi-clients/city-account'
 
 @Injectable()
 export default class ClientsService {
   constructor(private readonly configService: ConfigService) {}
 
-  public readonly cityAccountApi = createCityAccountClient({
+  public readonly cityAccountApi: CityAccountClient = createCityAccountClient({
     basePath: this.configService.getOrThrow('CITY_ACCOUNT_API_URL'),
   })
 }

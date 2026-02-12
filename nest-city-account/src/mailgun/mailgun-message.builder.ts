d81c3e7495df07259e5708b6cca3ee584afe4ded
Jakub Hvolka <jhvolka@gmail.com>
2026-02-12 13:56:07 +0100

    Remove unused `2025-delivery-method-changed-notify` logic and tests from Mailgun service and message builder

--- nest-city-account/src/mailgun/mailgun-message.builder.ts
+++ nest-city-account/src/mailgun/mailgun-message.builder.ts
@@ -27,19 +27,6 @@ interface IdentityCheckRejectedMailgunParams extends BaseMailgunParams {
   }
 }
 
-interface DeliveryMethodChangedMailgunParams extends BaseMailgunParams {
-  variables: {
-    firstName: string | null
-    year: string
-    deliveryMethod: 'email' | 'edesk' | 'postal'
-  }
-  attachment?: {
-    data: Buffer
-    filename: string
-    contentType: string
-  }
-}
-
 export interface DeliveryMethodChangedWithUserDataParams {
   userEmail: string
   externalId: string
@@ -87,21 +74,6 @@ export class MailgunMessageBuilder {
       subject: 'Vašu identitu sa v Bratislavskom konte nepodarilo overiť',
       template: '2023-identity-check-rejected',
       'h:X-Mailgun-Variables': JSON.stringify(variables),
-    }
-  }
-
-  async '2025-delivery-method-changed-notify'({
-    to,
-    variables,
-    attachment,
-  }: DeliveryMethodChangedMailgunParams): Promise<MailgunMessageData> {
-    return {
-      from: MAILGUN.FROM_EMAIL,
-      to,
-      subject: 'Váš spôsob doručenia v Bratislavskom konte sa zmenil',
-      template: '2025-delivery-method-changed-notify',
-      'h:X-Mailgun-Variables': JSON.stringify(variables),
-      ...(attachment && { attachment }),
     }
   }
 

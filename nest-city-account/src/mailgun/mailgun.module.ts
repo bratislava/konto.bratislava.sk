d7c29c151315ae59c8a477292858d0b2dd329ea9
Jakub Hvolka <63741979+JHvolka@users.noreply.github.com>
2026-02-13 09:59:28 +0100

    Add ThrowerErrorGuard to MailgunModule providers (#3768)

--- nest-city-account/src/mailgun/mailgun.module.ts
+++ nest-city-account/src/mailgun/mailgun.module.ts
@@ -3,10 +3,11 @@ import { MailgunService } from './mailgun.service'
 import { MailgunMessageBuilder } from './mailgun-message.builder'
 import { PdfGeneratorModule } from '../pdf-generator/pdf-generator.module'
 import { CognitoSubservice } from '../utils/subservices/cognito.subservice'
+import ThrowerErrorGuard from '../utils/guards/errors.guard'
 
 @Module({
   imports: [PdfGeneratorModule],
-  providers: [MailgunService, MailgunMessageBuilder, CognitoSubservice],
+  providers: [MailgunService, MailgunMessageBuilder, CognitoSubservice, ThrowerErrorGuard],
   exports: [MailgunService],
 })
 export class MailgunModule {}

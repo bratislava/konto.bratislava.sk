c1c47f34edc15231f088fb40286210b0ebc4d544
Erik Rehulka <erik.rehulka@gmail.com>
2026-02-24 15:53:49 +0100

    add index

--- nest-tax-backend/prisma/migrations/20260224131711_add_bloomreach_unpaid_reminder_sent_to_installments/migration.sql
+++ nest-tax-backend/prisma/migrations/20260224131711_add_bloomreach_unpaid_reminder_sent_to_installments/migration.sql
@@ -3,3 +3,6 @@ CREATE TYPE "UnpaidReminderSent" AS ENUM ('NONE', 'BEFORE_DUE', 'AFTER_DUE', 'BO
 
 -- AlterTable
 ALTER TABLE "TaxInstallment" ADD COLUMN     "bloomreachUnpaidReminderSent" "UnpaidReminderSent" NOT NULL DEFAULT 'NONE';
+
+-- CreateIndex
+CREATE INDEX "TaxInstallment_taxId_order_idx" ON "TaxInstallment"("taxId", "order");

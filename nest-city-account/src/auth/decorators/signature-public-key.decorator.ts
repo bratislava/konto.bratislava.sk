682ee3b3a50c11b4d922325e545a82096755534d
Jakub Hvolka <jhvolka@gmail.com>
2026-03-04 14:41:46 +0100

    Format code example in `SignaturePublicKey` decorator JSDoc for clarity

--- nest-city-account/src/auth/decorators/signature-public-key.decorator.ts
+++ nest-city-account/src/auth/decorators/signature-public-key.decorator.ts
@@ -6,8 +6,10 @@ export const SIGNATURE_PUBLIC_KEY = 'signature_public_key'
  * Decorator to specify the environment variable name containing the public key for signature verification
  * @param envVarName - The name of the environment variable containing the public key
  * @example
- * @SignaturePublicKey('DPB_CLIENT_PUBLIC_KEY')
- * @UseGuards(SignatureGuard)
+ * ```ts
+ *   @SignaturePublicKey('DPB_CLIENT_PUBLIC_KEY')
+ *   @UseGuards(SignatureGuard)
+ * ```
  */
 export const SignaturePublicKey = (envVarName: string) =>
   SetMetadata(SIGNATURE_PUBLIC_KEY, envVarName)

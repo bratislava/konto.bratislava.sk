48cc36cb7db404af0c05f073078c838686fe2297
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-03-04 02:53:52 +0100

    Install and setup eslint v9

---
+++ next/eslint.config.mjs
@@ -0,0 +1,49 @@
+import { createNextConfig } from '@bratislava/eslint-config-next'
+
+export default [
+  ...createNextConfig({
+    ignores: ['*.config.*', '/src/clients/openapi-*/*.ts', '/src/clients/graphql-*/*.ts'],
+  }),
+
+  // Project-specific rule overrides
+  {
+    rules: {
+      'jsx-a11y/anchor-is-valid': 'off',
+      'no-multi-spaces': 'error',
+
+      // TODO good rules, require work to fix and were skipped over in eslint v9 upgrade
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
+      '@typescript-eslint/promise-function-async': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'sonarjs/slow-regex': 'off',
+      'sonarjs/prefer-regexp-exec': 'off',
+      'security/detect-unsafe-regex': 'off',
+      'security/detect-object-injection': 'off',
+      'no-implicit-coercion': 'off',
+
+      // '@typescript-eslint/no-unnecessary-condition': 'warn', // 170 violations
+      // '@typescript-eslint/no-unused-vars': 'warn', // 159 violations
+      // 'sonarjs/no-redundant-optional': 'warn', // 28 violations
+      // '@typescript-eslint/no-unsafe-member-access': 'warn', // 16 violations
+      // '@typescript-eslint/no-non-null-assertion': 'warn', // 10 violations
+      // 'require-await': 'warn', // 10 violations
+      // 'sonarjs/deprecation': 'warn', // 10 violations
+      // '@typescript-eslint/no-deprecated': 'warn', // 9 violations
+      // '@typescript-eslint/require-await': 'warn', // 5 violations
+      // '@typescript-eslint/switch-exhaustiveness-check': 'warn', // 5 violations
+      // 'react/no-unknown-property': 'warn', // 4 violations
+      // 'jsx-a11y/click-events-have-key-events': 'warn', // 3 violations
+      // 'jsx-a11y/no-static-element-interactions': 'warn', // 3 violations
+      // 'react-hooks/set-state-in-effect': 'warn', // 3 violations
+
+      'tailwindcss/no-custom-classname': 'off',
+      'tailwindcss/classnames-order': 'off',
+    },
+  },
+]

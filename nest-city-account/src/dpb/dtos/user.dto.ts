d2c97346f81cab23f1bed647c6cd39aa024ed19b
Jakub Hvolka <jhvolka@gmail.com>
2026-03-06 11:32:54 +0100

    Rename `cognitoId` to `id` in `UserDTO` and update property description.

--- nest-city-account/src/dpb/dtos/user.dto.ts
+++ nest-city-account/src/dpb/dtos/user.dto.ts
@@ -67,9 +67,9 @@ export class DPBUserLoginStatistics {
 
   @ApiProperty({
     name: 'id',
-    description: 'Cognito user ID',
+    description: 'User ID',
     example: '123e4567-e89b-12d3-a456-426614174000',
     nullable: true,
   })
-  cognitoId: string
+  id: string
 }

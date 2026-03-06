92d1de07969cf167b5b1ed7b5de6da1e39b49df9
Jakub Hvolka <jhvolka@gmail.com>
2026-03-06 10:05:12 +0100

    Update JSDoc in `SignatureRequest` to fix decorator name

--- nest-city-account/src/auth/types/signature-request.types.ts
+++ nest-city-account/src/auth/types/signature-request.types.ts
@@ -3,7 +3,7 @@ import { Request } from 'express'
 export interface SignatureRequest extends Request {
   /**
    * Environment variable name containing the public key for signature verification
-   * Set by @SignaturePublicKey decorator via SignatureGuard
+   * Set by @SignaturePublicKeyEnvVarName decorator via SignatureGuard
    */
   signaturePublicKeyEnvVar?: string
 }

c0853f26e4fd2b9f35c104444b735d1ba3a94964
Erik Rehulka <erik.rehulka@gmail.com>
2026-03-10 10:49:14 +0100

    turn off selected rules in tests

--- nest-forms-backend/eslint.config.mjs
+++ nest-forms-backend/eslint.config.mjs
@@ -4,4 +4,18 @@ export default [
   ...createNestConfig({
     tsconfigRootDir: import.meta.dirname,
   }),
+  {
+    files: ['**/*.spec.ts', '**/*_test_.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      'dot-notation': 'off', // to test private methods
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      'sonarjs/no-nested-functions': 'off',
+    },
+  },
 ]

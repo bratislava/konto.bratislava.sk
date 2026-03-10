289c03410903e525b21b3680733581d96a612618
Jakub Hvolka <jhvolka@gmail.com>
2026-03-10 13:46:27 +0100

    Add CacheModule using Redis for nonce-based replay protection.

--- nest-city-account/src/auth/auth.module.ts
+++ nest-city-account/src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
+import { CacheModule } from '../cache/cache.module'
 import { AuthController } from './auth.controller'
 import { CognitoStrategy } from './strategies/cognito.strategy'
 import { SignatureStrategy } from './strategies/signature.strategy'
 import { SignatureGuard } from './guards/signature.guard'
+import { NonceService } from './services/nonce.service'
 
 @Module({
-  imports: [PassportModule],
-  providers: [CognitoStrategy, SignatureStrategy, SignatureGuard],
+  imports: [PassportModule, CacheModule],
+  providers: [CognitoStrategy, SignatureStrategy, SignatureGuard, NonceService],
   exports: [SignatureGuard],
   controllers: [AuthController],
 })

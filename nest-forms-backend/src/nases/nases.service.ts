8b8e12d66b3f9ee9051775cb79f269a0c2f74f0b
Erik Rehulka <erik.rehulka@bratislava.sk>
2026-03-11 14:45:52 +0100

    final lint fix

--- nest-forms-backend/src/nases/nases.service.ts
+++ nest-forms-backend/src/nases/nases.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-misused-spread */
 import { HttpStatus, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'

8416acc9f4477b0c7353bd074e607bccfed6d592
Lukáš Babula <lukas.babula@gmail.com>
2026-03-26 14:59:20 +0100

    Fix tax consent anonymization when disabling user (#3944)

--- nest-city-account/src/bloomreach/bloomreach.service.ts
+++ nest-city-account/src/bloomreach/bloomreach.service.ts
@@ -275,6 +275,11 @@ export class BloomreachService {
             category: GDPRCategoryEnum.ESBS,
             subType: GDPRSubTypeEnum.unsubscribe,
           },
+          {
+            type: GDPRTypeEnum.FORMAL_COMMUNICATION,
+            category: GDPRCategoryEnum.TAXES,
+            subType: GDPRSubTypeEnum.unsubscribe,
+          },
         ],
         cognitoId
       )

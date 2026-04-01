748e76f5855b28b94ffd98e670337b0753dba664
Jakub Hvolka <jhvolka@gmail.com>
2026-04-01 14:53:11 +0200

    Refactor `EdeskRecordSchema` by extracting `EdeskRecordRawSchema` and reapplying transformations.

--- nest-city-account/src/noris/types/noris.types.ts
+++ nest-city-account/src/noris/types/noris.types.ts
@@ -8,44 +8,45 @@ export enum EdeskStatus {
   NONEXISTENT = 'NONEXISTENT',
 }
 
-export const EdeskRecordSchema = z
-  .object({
-    id_noris: z.number(),
-    uri_generated: z.string(),
+const EdeskRecordRawSchema = z.object({
+  id_noris: z.number(),
+  uri_generated: z.string(),
 
-    // TODO - these fields are not necessary now, but can be in the future for generating uris.
-    /*
-    edesk_status: z.enum(EdeskStatus).nullable(),
-    edesk_number: z.string().nullable(),
+  // TODO - these fields are not necessary now, but can be in the future for generating uris.
+  /*
+  edesk_status: z.enum(EdeskStatus).nullable(),
+  edesk_number: z.string().nullable(),
 
-    priezvisko: z.string(),
-    meno: z.string(),
+  priezvisko: z.string(),
+  meno: z.string(),
 
-    rc: z.string(),
-    ICO: z.string(),
-    obchodne_meno: z.string(),
+  rc: z.string(),
+  ICO: z.string(),
+  obchodne_meno: z.string(),
 
-    uri_edesk: z.string().nullable(),
-    uri_ginis: z.string().nullable(),
+  uri_edesk: z.string().nullable(),
+  uri_ginis: z.string().nullable(),
 
-    last_check: z.date(),
-    */
+  last_check: z.date(),
+  */
+})
+
+export const EdeskRecordSchema = EdeskRecordRawSchema.transform((data) => {
+  const sanitized = data.uri_generated.replaceAll(/\s/g, '')
+  return {
+    ...data,
+    uri_generated: sanitized,
+    uri_new: sanitized !== data.uri_generated ? sanitized : undefined,
+  }
+}).pipe(
+  EdeskRecordRawSchema.extend({
+    uri_generated: z
+      .string()
+      .min(1)
+      .regex(/^[\x20-\x7E]+$/),
+    uri_new: z.union([z.string(), z.undefined()]),
   })
-  .transform((data) => {
-    const sanitized = data.uri_generated.replaceAll(/\s/g, '')
-    return {
-      ...data,
-      uri_generated: sanitized,
-      uri_new: sanitized !== data.uri_generated ? sanitized : undefined,
-    }
-  })
-  .pipe(
-    z.object({
-      id_noris: z.number(),
-      uri_generated: z.string().min(1).regex(/^[\x20-\x7E]+$/),
-      uri_new: z.union([z.string(), z.undefined()]),
-    })
-  )
+)
 
 export type EdeskRecord = z.infer<typeof EdeskRecordSchema>
 

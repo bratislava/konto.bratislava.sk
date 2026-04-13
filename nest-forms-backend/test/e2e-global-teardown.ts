7bc9c584fc7f7d5c844c84b27e21080ec6a09aa5
Marek Bodinger <marek.bodinger@bratislava.sk>
2026-04-13 17:26:33 +0200

    test

--- nest-forms-backend/test/e2e-global-teardown.ts
+++ nest-forms-backend/test/e2e-global-teardown.ts
@@ -3,9 +3,24 @@ import { e2eGlobalShared } from './e2e-global-shared'
 export default async function e2eGlobalTeardown(): Promise<void> {
   const { postgresContainer } = e2eGlobalShared
   if (postgresContainer) {
-    console.log('Stopping container')
-    await postgresContainer.stop()
-    console.log('Container stopped')
+    const containerId = postgresContainer.getId()
+
+    console.log(`Stopping container ${containerId}`)
+
+    try {
+      await postgresContainer.stop()
+      console.log(`Container ${containerId} stopped`)
+    } catch (error) {
+      console.error(`Failed to stop container ${containerId} during Jest globalTeardown`)
+
+      if (error instanceof AggregateError) {
+        for (const nestedError of error.errors) {
+          console.error(nestedError)
+        }
+      } else {
+        console.error(error)
+      }
+    }
   } else {
     console.log('Container not found')
   }

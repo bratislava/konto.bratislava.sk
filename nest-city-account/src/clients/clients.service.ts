a90f834e9a300683256b1d8ebb7a1373487b9904
Marek Bodinger <marek.bodinger@bratislava.sk>
2026-04-14 15:29:28 +0200

    Fix TS2729: Property configService is used before its initialization.

--- nest-city-account/src/clients/clients.service.ts
+++ nest-city-account/src/clients/clients.service.ts
@@ -6,17 +6,23 @@ import { createTaxClient, type TaxClient } from 'openapi-clients/tax'
 
 @Injectable()
 export default class ClientsService {
-  constructor(private configService: ConfigService) {}
+  public readonly taxBackendApi: TaxClient
 
-  public readonly taxBackendApi: TaxClient = createTaxClient({
-    basePath: this.configService.getOrThrow('TAX_BACKEND_URL'),
-  })
+  public readonly magproxyApi: MagproxyClient
 
-  public readonly magproxyApi: MagproxyClient = createMagproxyClient({
-    basePath: this.configService.getOrThrow('MAGPROXY_URL'),
-  })
+  public readonly slovenskoSkApi: SlovenskoSkClient
 
-  public readonly slovenskoSkApi: SlovenskoSkClient = createSlovenskoSkClient({
-    basePath: this.configService.getOrThrow('SLOVENSKO_SK_CONTAINER_URI'),
-  })
+  constructor(private configService: ConfigService) {
+    this.taxBackendApi = createTaxClient({
+      basePath: this.configService.getOrThrow('TAX_BACKEND_URL'),
+    })
+
+    this.magproxyApi = createMagproxyClient({
+      basePath: this.configService.getOrThrow('MAGPROXY_URL'),
+    })
+
+    this.slovenskoSkApi = createSlovenskoSkClient({
+      basePath: this.configService.getOrThrow('SLOVENSKO_SK_CONTAINER_URI'),
+    })
+  }
 }

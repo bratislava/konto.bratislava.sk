4c99cc0a4b16d0e1454cd87c5df94bf159165bee
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-04-15 16:30:59 +0200

    Fix imports from 'next-i18next/pages' after merging master

--- next/src/components/fields/PasswordField.tsx
+++ next/src/components/fields/PasswordField.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from 'next-i18next/pages'
 import { forwardRef, Ref, useState } from 'react'
 import { ToggleButton as RACToggleButton } from 'react-aria-components'
 

dffd75d338ed6882dcc9586c3b4771b008bc0285
Erik Rehulka <erik.rehulka@gmail.com>
2026-04-15 15:06:15 +0200

    upgrade eslint config and remove last rule overrides

--- nest-tax-backend/eslint.config.mjs
+++ nest-tax-backend/eslint.config.mjs
@@ -4,13 +4,4 @@ export default [
   ...createNestConfig({
     tsconfigRootDir: import.meta.dirname,
   }),
-  // Project-specific rule overrides
-  {
-    files: ['**/*.spec.ts', '**/*_test_.ts'],
-    rules: {
-      'dot-notation': 'off', // to test private methods
-      'sonarjs/no-nested-functions': 'off',
-      '@typescript-eslint/no-misused-spread': 'off', // spreading DTOs in tests is fine, prototype is irrelevant
-    },
-  },
 ]

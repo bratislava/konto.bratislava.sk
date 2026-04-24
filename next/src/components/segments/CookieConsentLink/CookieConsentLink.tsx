ca32fd930e80e98d74125b44088ae803f42bde04
Ty-ci <teachy.jaho@gmail.com>
2026-04-24 12:26:40 +0200

    Adjust introductory comment in CookieConsentLink

--- next/src/components/segments/CookieConsentLink/CookieConsentLink.tsx
+++ next/src/components/segments/CookieConsentLink/CookieConsentLink.tsx
@@ -4,10 +4,15 @@ import { useTranslation } from 'next-i18next/pages'
 
 /**
  * Cookiebot docs: https://www.cookiebot.com/en/developer/
- *
- * - For local testing, you need to enable preview mode for localhost in GTM:
- * - https://support.google.com/tagmanager/thread/347094327/localhost-testing?hl=en
- * - For additional info and access to Cookiebot and GMT, see our team docs
+ * 
+ * Cookiebot is injected via Google Tag Manager (GTM)
+ * For local testing, you need to enable preview mode for localhost in GTM:
+ * https://support.google.com/tagmanager/thread/347094327/localhost-testing?hl=en
+ * Otherwise, Cookiebot script will not be injected.
+ * 
+ * Note that in GTM, we have a custom script adjusting Cookiebot behavior.
+ * 
+ * For additional info and access to Cookiebot and GMT, see our team docs.
  */
 
 const showCookiebotConsentBanner = () => {

7f361667ee7c814ddb39aa72fe9fed9bb07141cd
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-05-19 00:52:06 +0200

    Use Markdown to render tax payment info messages

--- next/src/components/page-contents/TaxesFees/TaxFeePageContent/TaxFeePaymentMethods/TaxFeePaymentMethodsItem.tsx
+++ next/src/components/page-contents/TaxesFees/TaxFeePageContent/TaxFeePaymentMethods/TaxFeePaymentMethodsItem.tsx
@@ -1,9 +1,10 @@
 import { Button, ButtonButtonProps, Typography } from '@bratislava/component-library'
 
 import { FormatCurrencyFromCents } from '@/src/components/formatting/formatCurrency'
+import Markdown from '@/src/components/formatting/Markdown'
 
 interface TaxFeePaymentMethodsItemProps {
-  title: React.ReactNode
+  title: string
   subtitle?: string
   amount: number
   buttonText: string
@@ -22,7 +23,7 @@ const TaxFeePaymentMethodsItem = ({
   return (
     <div className="flex w-full flex-col justify-between gap-4 border-gray-200 p-4 nth-2:border-t lg:flex-row lg:gap-0 lg:p-6">
       <div className="flex flex-col items-start justify-center gap-3">
-        <Typography variant="p-small">{title}</Typography>
+        <Markdown content={title} variant="small" />
         {subtitle ? (
           <Typography variant="p-small" className="font-semibold text-category-600">
             {subtitle}

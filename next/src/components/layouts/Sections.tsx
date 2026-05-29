ed14f868fb1aea8ac406ba91a12697b5b1993cdd
Ty-ci <teachy.jaho@gmail.com>
2026-05-29 14:30:54 +0200

    Setup FormLandingPage Sections in next

---
+++ next/src/components/layouts/Sections.tsx
@@ -0,0 +1,33 @@
+import { FormLandingPageSectionsFragment } from '@/src/clients/graphql-strapi/api'
+import RichTextSection from '@/src/components/sections/RichTextSection'
+
+/**
+ * Based on Bratislava.sk: https://github.com/bratislava/bratislava.sk/blob/be7785e45d5e61c9b2a23177b9dcfb8af109ebc6/next/src/components/layouts/Sections.tsx
+ */
+
+type SectionsProps = {
+  sections: FormLandingPageSectionsFragment[]
+}
+
+const SectionContent = ({ section }: { section: SectionsProps['sections'][number] }) => {
+  // eslint-disable-next-line sonarjs/no-small-switch
+  switch (section.__typename) {
+    case 'ComponentSectionsRichtext':
+      return <RichTextSection section={section} />
+
+    default:
+      return null
+  }
+}
+
+const Sections = ({ sections }: SectionsProps) => {
+  return (
+    <>
+      {sections.map((section, index) => (
+        <SectionContent key={index} section={section} />
+      ))}
+    </>
+  )
+}
+
+export default Sections

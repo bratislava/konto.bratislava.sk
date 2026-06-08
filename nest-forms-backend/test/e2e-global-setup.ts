4a9841ed54606a2bb2557fd68f99878e2826e5ff
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2026-06-08 14:02:25 +0200

    Pull request Docker pipeline (#4150)
    * Adjust Dockerfiles
    
    * Create resolve-cache-refs action
    
    * Ability to use CI PostgresSQL in Forms BE E2E tests
    
    * Update pipelines
    
    * Update pipelines
    
    * Rewrite action to use actions/github-script

--- nest-forms-backend/test/e2e-global-setup.ts
+++ nest-forms-backend/test/e2e-global-setup.ts
@@ -2,17 +2,22 @@ import { execSync } from 'node:child_process'
 
 import { PostgreSqlContainer } from '@testcontainers/postgresql'
 
-import { e2eGlobalShared } from './e2e-global-shared'
+import { e2eGlobalShared, getCiE2eDatabaseUrl } from './e2e-global-shared'
 
 export default async function e2eGlobalSetup(): Promise<void> {
-  const container = await new PostgreSqlContainer('postgres:alpine')
-    .withUsername('forms')
-    .withPassword('password')
-    .withDatabase('forms')
-    .start()
+  const ciE2eDatabaseUrl = getCiE2eDatabaseUrl()
+  if (ciE2eDatabaseUrl) {
+    process.env.DATABASE_URL = ciE2eDatabaseUrl
+  } else {
+    const container = await new PostgreSqlContainer('postgres:alpine')
+      .withUsername('forms')
+      .withPassword('password')
+      .withDatabase('forms')
+      .start()
 
-  process.env.DATABASE_URL = `postgresql://${container.getUsername()}:${container.getPassword()}@${container.getHost()}:${container.getPort()}/${container.getDatabase()}`
-  e2eGlobalShared.postgresContainer = container
+    process.env.DATABASE_URL = `postgresql://${container.getUsername()}:${container.getPassword()}@${container.getHost()}:${container.getPort()}/${container.getDatabase()}`
+    e2eGlobalShared.postgresContainer = container
+  }
 
   // eslint-disable-next-line sonarjs/no-os-command-from-path
   execSync('npx prisma db push --skip-generate', {

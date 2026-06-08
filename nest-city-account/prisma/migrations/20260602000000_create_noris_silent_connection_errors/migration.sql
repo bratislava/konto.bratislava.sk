0c42ce140230a5e241da5ea1584ca9ce0ade5ee1
Erik Řehulka <erik.rehulka@bratislava.sk>
2026-06-08 11:20:17 +0200

    fix migration setting config in nest-city-account (#4273)

--- nest-city-account/prisma/migrations/20260602000000_create_noris_silent_connection_errors/migration.sql
+++ nest-city-account/prisma/migrations/20260602000000_create_noris_silent_connection_errors/migration.sql
@@ -1,3 +1,3 @@
-INSERT INTO "Config" ("key", "value")
+INSERT INTO "Config" ("id", "updatedAt", "key", "value")
 VALUES
-  ('NORIS_SILENT_CONNECTION_ERRORS', '0');
+  ('0d3a89a7-c4e8-4951-bc46-eedb2c24ad57', now(), 'NORIS_SILENT_CONNECTION_ERRORS', '0');

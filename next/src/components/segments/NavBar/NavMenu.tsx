cc10afd4fe31a447cbb8d963c5145281f80b8710
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-06-19 23:25:42 +0200

    Cleanup

--- next/src/components/segments/NavBar/NavMenu.tsx
+++ next/src/components/segments/NavBar/NavMenu.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next/pages'
 
 import SectionContainer from '@/src/components/layouts/SectionContainer'
-import { useNavMenuContext } from '@/src/components/segments/NavBar/navMenuContext'
 import useMenu, { MenuSectionBase } from '@/src/components/segments/NavBar/useMenu'
 import cn from '@/src/utils/cn'
 
@@ -19,58 +18,52 @@ export const NavMenu = () => {
 
   const { menuSections } = useMenu()
 
-  const { menuValue, setMenuValue } = useNavMenuContext()
-
   const isActive = (sectionItem: MenuSectionBase) =>
     sectionItem.url === '/' ? router.pathname === '/' : router.pathname.startsWith(sectionItem.url)
 
   return (
-    menuSections && (
-      <SectionContainer>
-        <div className="flex h-[57px] w-full items-center justify-between border-t border-border-passive-primary">
-          <NavigationMenu.Root
-            value={menuValue}
-            onValueChange={setMenuValue}
-            aria-label={t('NavMenu.aria.navMenuLabel')}
-            /**
-             * Because of https://github.com/radix-ui/primitives/discussions/1874,
-             * we can't directly access subelement (<div style="position: relative;")
-             * of "<nav>" element that NavigationMenu.List creates when used.
-             * To solve this, we add grid class to the parent element.
-             */
-            className="grid size-full"
-          >
-            <NavigationMenu.List className="flex size-full items-center">
-              {menuSections.map((sectionItem) => (
-                <NavigationMenu.Item
-                  key={sectionItem.id}
-                  className="size-full rounded-sm wrapper-focus-ring"
-                >
-                  <NavigationMenu.Link asChild>
-                    <NextLink href={sectionItem.url}>
-                      <div
-                        className={cn(
-                          'flex size-full cursor-pointer items-center justify-center border-b-2 transition-all hover:border-main-700 hover:text-main-700',
-                          {
-                            'border-main-700 text-main-700': isActive(sectionItem),
-                            'border-transparent': !isActive(sectionItem),
-                          },
-                        )}
-                      >
-                        {sectionItem.icon}
-                        <Typography variant="p-small" as="span" className="ml-3 font-semibold">
-                          {sectionItem.title}
-                        </Typography>
-                      </div>
-                    </NextLink>
-                  </NavigationMenu.Link>
-                </NavigationMenu.Item>
-              ))}
-            </NavigationMenu.List>
-          </NavigationMenu.Root>
-        </div>
-      </SectionContainer>
-    )
+    <SectionContainer>
+      <div className="flex h-[57px] w-full items-center justify-between border-t border-border-passive-primary">
+        <NavigationMenu.Root
+          aria-label={t('NavMenu.aria.navMenuLabel')}
+          /**
+           * Because of https://github.com/radix-ui/primitives/discussions/1874,
+           * we can't directly access subelement (<div style="position: relative;")
+           * of "<nav>" element that NavigationMenu.List creates when used.
+           * To solve this, we add grid class to the parent element.
+           */
+          className="grid size-full"
+        >
+          <NavigationMenu.List className="flex size-full items-center">
+            {menuSections.map((sectionItem) => (
+              <NavigationMenu.Item
+                key={sectionItem.id}
+                className="size-full rounded-sm wrapper-focus-ring ring-inset"
+              >
+                <NavigationMenu.Link asChild>
+                  <NextLink href={sectionItem.url}>
+                    <div
+                      className={cn(
+                        'flex size-full cursor-pointer items-center justify-center border-b-2 transition-all hover:border-main-700 hover:text-main-700',
+                        {
+                          'border-main-700 text-main-700': isActive(sectionItem),
+                          'border-transparent': !isActive(sectionItem),
+                        },
+                      )}
+                    >
+                      {sectionItem.icon}
+                      <Typography variant="p-small" as="span" className="ml-3 font-semibold">
+                        {sectionItem.title}
+                      </Typography>
+                    </div>
+                  </NextLink>
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+            ))}
+          </NavigationMenu.List>
+        </NavigationMenu.Root>
+      </div>
+    </SectionContainer>
   )
 }
 

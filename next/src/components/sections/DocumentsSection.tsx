53d7ec04061c3c2c1e568fa1fff4e48bf9c20103
shegood <petrsigut10@gmail.com>
2026-06-26 13:08:24 +0200

    Add working version

--- next/src/components/sections/DocumentsSection.tsx
+++ next/src/components/sections/DocumentsSection.tsx
@@ -1,7 +1,11 @@
+import { Button, Typography } from '@bratislava/component-library'
+import { useTranslation } from 'next-i18next/pages'
+import { useState } from 'react'
 import { Fragment } from 'react/jsx-runtime'
 
 import { DocumentsSectionFragment } from '@/src/clients/graphql-strapi/api'
 import ExternalDocumentRowCard from '@/src/components/common/Documents/ExternalDocumentRowCard'
+import Icon from '@/src/components/icon-components/Icon'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
 import HorizontalDivider from '@/src/components/simple-components/HorizontalDivider'
@@ -14,6 +18,8 @@ type DocumentsSectionProps = {
 const DocumentsSection = ({
   section: { title, text, externalDocuments },
 }: DocumentsSectionProps) => {
+  const [showAllDocuments, setShowAllDocuments] = useState(false)
+  const { t } = useTranslation('account')
   const filteredExternalDocuments = externalDocuments?.filter(isDefined) ?? []
 
   return (
@@ -22,13 +28,45 @@ const DocumentsSection = ({
         <SectionHeader title={title} text={text} />
 
         <ul className="flex flex-col rounded-lg border border-border-active-default bg-background-passive-base py-2">
-          {filteredExternalDocuments.map((externalDocument, index) => (
-            <Fragment key={index}>
-              {index > 0 && <HorizontalDivider asListItem className="mx-6" />}
+          {filteredExternalDocuments
+            .slice(0, showAllDocuments ? filteredExternalDocuments.length : 5)
+            .map((externalDocument, index) => (
+              <Fragment key={index}>
+                {index > 0 && <HorizontalDivider asListItem className="mx-6" />}
 
-              <ExternalDocumentRowCard title={externalDocument?.title} url={externalDocument.url} />
-            </Fragment>
-          ))}
+                <ExternalDocumentRowCard
+                  title={externalDocument?.title}
+                  url={externalDocument.url}
+                />
+              </Fragment>
+            ))}
+          {filteredExternalDocuments?.length > 5 && (
+            <>
+              {<HorizontalDivider asListItem className="mx-6" />}
+
+              <li className="flex items-center justify-center gap-2 py-3">
+                <Button
+                  fullWidth
+                  className="mx-6 py-2"
+                  variant="plain"
+                  onClick={() => setShowAllDocuments(!showAllDocuments)}
+                >
+                  <Typography variant="button-large">
+                    {showAllDocuments
+                      ? t('documentSection.documents.showLess')
+                      : t('documentSection.documents.showMore', {
+                          count: filteredExternalDocuments?.length,
+                        })}
+                  </Typography>
+
+                  <Icon
+                    name={showAllDocuments ? 'chevron-up' : 'chevron-down'}
+                    className="size-6"
+                  />
+                </Button>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </SectionContainer>

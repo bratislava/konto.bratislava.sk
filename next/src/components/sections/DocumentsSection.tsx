4e692ee7ba85dd1579b608ff08a6bdb3e42a49ea
shegood <petrsigut10@gmail.com>
2026-06-30 15:40:33 +0200

    fix low hanging fruit

--- next/src/components/sections/DocumentsSection.tsx
+++ next/src/components/sections/DocumentsSection.tsx
@@ -1,11 +1,10 @@
-import { Button, Typography } from '@bratislava/component-library'
+import { Button } from '@bratislava/component-library'
 import { useTranslation } from 'next-i18next/pages'
 import { useState } from 'react'
 import { Fragment } from 'react/jsx-runtime'
 
 import { DocumentsSectionFragment } from '@/src/clients/graphql-strapi/api'
 import ExternalDocumentRowCard from '@/src/components/common/Documents/ExternalDocumentRowCard'
-import Icon from '@/src/components/icon-components/Icon'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
 import HorizontalDivider from '@/src/components/simple-components/HorizontalDivider'
@@ -15,11 +14,13 @@ type DocumentsSectionProps = {
   section: DocumentsSectionFragment
 }
 
+const AMOUNT_OF_DOCUMENTS_TO_SHOW = 5
+
 const DocumentsSection = ({
   section: { title, text, externalDocuments },
 }: DocumentsSectionProps) => {
-  const [showAllDocuments, setShowAllDocuments] = useState(false)
   const { t } = useTranslation('account')
+  const [showAllDocuments, setShowAllDocuments] = useState(false)
   const filteredExternalDocuments = externalDocuments?.filter(isDefined) ?? []
 
   return (
@@ -29,42 +30,43 @@ const DocumentsSection = ({
 
         <ul className="flex flex-col rounded-lg border border-border-active-default bg-background-passive-base py-2">
           {filteredExternalDocuments
-            .slice(0, showAllDocuments ? filteredExternalDocuments.length : 5)
+            .slice(
+              0,
+              showAllDocuments ? filteredExternalDocuments.length : AMOUNT_OF_DOCUMENTS_TO_SHOW,
+            )
             .map((externalDocument, index) => (
               <Fragment key={index}>
                 {index > 0 && <HorizontalDivider asListItem className="mx-6" />}
 
                 <ExternalDocumentRowCard
-                  title={externalDocument?.title}
+                  title={externalDocument.title}
                   url={externalDocument.url}
                 />
               </Fragment>
             ))}
-          {filteredExternalDocuments?.length > 5 && (
-            <>
-              {<HorizontalDivider asListItem className="mx-6" />}
-
-              <li className="flex items-center justify-center py-3">
-                <Button
-                  fullWidth
-                  className="mx-6 py-2"
-                  variant="plain"
-                  onClick={() => setShowAllDocuments(!showAllDocuments)}
-                >
-                  <Typography variant="button-large">
-                    {showAllDocuments
-                      ? t('documentSection.documents.showLess')
-                      : t('documentSection.documents.showMore', {
-                          count: filteredExternalDocuments?.length,
-                        })}
-                  </Typography>
-
-                  <Icon name={showAllDocuments ? 'chevron-up' : 'chevron-down'} />
-                </Button>
-              </li>
-            </>
-          )}
         </ul>
+
+        {filteredExternalDocuments.length > AMOUNT_OF_DOCUMENTS_TO_SHOW && (
+          <>
+            <HorizontalDivider asListItem className="mx-6" />
+
+            <div className="flex items-center justify-center py-3">
+              <Button
+                fullWidth
+                className="mx-6 py-2"
+                variant="plain"
+                onClick={() => setShowAllDocuments(!showAllDocuments)}
+                endIcon={showAllDocuments ? 'chevron-up' : 'chevron-down'}
+              >
+                {showAllDocuments
+                  ? t('DocumentsSection.documents.showLess')
+                  : t('DocumentsSection.documents.showMore', {
+                      count: filteredExternalDocuments.length,
+                    })}
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </SectionContainer>
   )

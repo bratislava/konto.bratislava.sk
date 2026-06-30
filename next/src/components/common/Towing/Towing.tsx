50f7265634c0b0d83ac3e266f437a2f462849c7c
Petr Šigut <64728606+shegood@users.noreply.github.com>
2026-06-30 08:44:03 +0200

    1587 Use form in Towing section (#4362)
    * add form
    
    * remove unneeded state, add submit to the button
    
    * move captcha below the button, unify the Turnstile component

--- next/src/components/common/Towing/Towing.tsx
+++ next/src/components/common/Towing/Towing.tsx
@@ -1,10 +1,11 @@
-import { Button, Spinner, Typography } from '@bratislava/component-library'
+import { Button, Typography } from '@bratislava/component-library'
 import { useMutation } from '@tanstack/react-query'
 import { isAxiosError } from 'axios'
 import { useTranslation } from 'next-i18next/pages'
 import { useState } from 'react'
+import { Controller } from 'react-hook-form'
 import Turnstile from 'react-turnstile'
-import { useCounter } from 'usehooks-ts'
+import { useCounter, useTimeout } from 'usehooks-ts'
 
 import { cityAccountClient } from '@/src/clients/city-account'
 import TowingNotFound from '@/src/components/common/Towing/TowingNotFound'
@@ -13,6 +14,8 @@ import TextField from '@/src/components/fields/TextField'
 import Icon from '@/src/components/icon-components/Icon'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
 import { environment } from '@/src/environment'
+import useHookForm from '@/src/frontend/hooks/useHookForm'
+import { isBrowser } from '@/src/frontend/utils/general'
 import logger from '@/src/frontend/utils/logger'
 
 export type TowingSectionProps = {
@@ -20,35 +23,54 @@ export type TowingSectionProps = {
   text?: string | null
 }
 
+type TowingFormData = {
+  licensePlate: string
+  turnstileToken: string
+}
+
+const schema = {
+  type: 'object',
+  properties: {
+    licensePlate: {
+      type: 'string',
+      minLength: 1,
+      errorMessage: { minLength: 'towing.licensePlate_required' },
+    },
+    turnstileToken: {
+      type: 'string',
+      minLength: 1,
+    },
+  },
+  required: ['licensePlate', 'turnstileToken'],
+}
+
 const Towing = ({ title, text }: TowingSectionProps) => {
   const { t } = useTranslation('account')
-  const [licensePlate, setLicensePlate] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-  const [turnstileToken, setTurnstileToken] = useState<string | null>(null)
   const [captchaWarning, setCaptchaWarning] = useState<'loading' | 'show' | 'hide'>('loading')
   const { count: captchaKey, increment: incrementCaptchaKey } = useCounter(0)
 
   const {
-    mutateAsync: handleSubmit,
-    data,
-    isSuccess,
-    isPending,
-    error,
-  } = useMutation({
-    mutationFn: async () => {
-      if (!turnstileToken) {
-        setErrorMessage(t('auth.finish_captcha'))
-        throw new Error('Turnstile token is required')
-      }
+    handleSubmit,
+    control,
+    errors,
+    formState: { isSubmitting },
+  } = useHookForm<TowingFormData>({
+    schema,
+    defaultValues: { licensePlate: '', turnstileToken: '' },
+  })
 
+  useTimeout(() => {
+    if (!isBrowser() || captchaWarning === 'hide') return
+    setCaptchaWarning('show')
+  }, 3000)
+
+  const { mutateAsync, data, isSuccess, variables, error } = useMutation({
+    mutationFn: async ({ licensePlate, turnstileToken }: TowingFormData) => {
       const response = await cityAccountClient.towingControllerGetPublicTowingByEcv(licensePlate, {
         turnstileToken,
       })
 
       return response.data
-    },
-    onSuccess: () => {
-      setErrorMessage('')
     },
     onError: (error) => {
       if (!isAxiosError(error) || error.response?.status !== 404) {
@@ -56,7 +78,6 @@ const Towing = ({ title, text }: TowingSectionProps) => {
           'Error fetching towing:',
           isAxiosError(error) ? error.response?.data.message : error.message,
         )
-        setErrorMessage(t('towing.error'))
       }
     },
     onSettled: () => {
@@ -64,69 +85,92 @@ const Towing = ({ title, text }: TowingSectionProps) => {
     },
   })
 
+  const isNotFound = isAxiosError(error) && error.response?.status === 404
+  const requestErrorMessage = error && !isNotFound ? t('towing.error') : ''
+
   return (
     <div className="flex flex-col gap-4">
       <SectionHeader title={title} text={text} />
 
-      <div className="flex flex-col justify-center gap-4 rounded-lg border px-5 py-6">
-        {/* TODO: Wrap this in useForm hook */}
-        <TextField
-          label={t('towing.licensePlate')}
-          displayOptionalLabel={false}
-          errorMessage={errorMessage}
-          onChange={(value) => setLicensePlate(value.trim().toUpperCase())}
-          helptext={t('towing.typeInInstructions')}
+      <form
+        className="flex flex-col justify-center gap-4 rounded-lg border px-5 py-6"
+        onSubmit={handleSubmit((formData) => mutateAsync(formData))}
+      >
+        <Controller
+          name="licensePlate"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              label={t('towing.licensePlate')}
+              displayOptionalLabel={false}
+              helptext={t('towing.typeInInstructions')}
+              autoCapitalize="characters"
+              autoCorrect="off"
+              spellCheck="false"
+              {...field}
+              onChange={(value) => field.onChange(value.trim().toUpperCase())}
+              errorMessage={errors.licensePlate ?? requestErrorMessage}
+            />
+          )}
+        />
+
+        <Controller
+          name="turnstileToken"
+          control={control}
+          render={({ field: { onChange } }) => (
+            <>
+              <Turnstile
+                theme="light"
+                key={captchaKey}
+                sitekey={environment.cloudflareTurnstileSiteKey}
+                className="self-center"
+                onVerify={(token) => {
+                  setCaptchaWarning('hide')
+                  onChange(token)
+                }}
+                onError={(error) => {
+                  logger.error('Turnstile error:', error)
+                  setCaptchaWarning('show')
+
+                  return onChange(null)
+                }}
+                onTimeout={() => {
+                  logger.error('Turnstile timeout')
+                  setCaptchaWarning('show')
+                  onChange(null)
+                }}
+                onExpire={() => {
+                  logger.warn('Turnstile expire - should refresh automatically')
+                  onChange(null)
+                }}
+              />
+
+              {captchaWarning === 'show' && (
+                <Typography variant="p-small" className="italic">
+                  {t('auth.captcha_warning')}
+                </Typography>
+              )}
+            </>
+          )}
         />
 
         <Button
-          onPress={() => handleSubmit()}
+          type="submit"
           variant="solid"
           fullWidth
-          isDisabled={licensePlate.length === 0 || !turnstileToken || isPending}
+          isLoading={isSubmitting}
+          loadingText={t('towing.searching')}
           startIcon={<Icon name="search" />}
         >
           {t('towing.searchButton')}
         </Button>
 
-        <Turnstile
-          theme="light"
-          key={captchaKey}
-          sitekey={environment.cloudflareTurnstileSiteKey}
-          className="self-center"
-          onVerify={(token) => {
-            setCaptchaWarning('hide')
-            setTurnstileToken(token)
-          }}
-          onError={(error) => {
-            logger.error('Turnstile error:', error)
-            setCaptchaWarning('show')
-            setTurnstileToken(null)
-          }}
-          onTimeout={() => {
-            logger.error('Turnstile timeout')
-            setCaptchaWarning('show')
-            setTurnstileToken(null)
-          }}
-          onExpire={() => {
-            logger.warn('Turnstile expire - should refresh automatically')
-            setTurnstileToken(null)
-          }}
-        />
-
-        {captchaWarning === 'show' && (
-          <Typography variant="p-tiny" className="italic">
-            {t('auth.captcha_warning')}
-          </Typography>
-        )}
-
-        {isPending ? (
-          <Spinner className="self-center" />
-        ) : isSuccess ? (
-          <TowingTable vehicle={data} initialLicensePlate={licensePlate} />
-        ) : isAxiosError(error) && error.response?.status === 404 ? (
-          <TowingNotFound initialLicensePlate={licensePlate} />
+        {isSuccess ? (
+          <TowingTable vehicle={data} initialLicensePlate={variables.licensePlate} />
+        ) : isNotFound ? (
+          <TowingNotFound initialLicensePlate={variables?.licensePlate ?? ''} />
         ) : null}
-      </div>
+      </form>
     </div>
   )
 }

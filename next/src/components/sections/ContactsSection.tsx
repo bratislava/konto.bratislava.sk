d06d3fcd7d26cffe62b957392a90820a67907b65
Ty-ci <teachy.jaho@gmail.com>
2026-07-10 12:43:36 +0200

    Micro cleanup in sections

--- next/src/components/sections/ContactsSection.tsx
+++ next/src/components/sections/ContactsSection.tsx
@@ -2,11 +2,15 @@ import { ContactsSectionFragment } from '@/src/clients/graphql-strapi/api'
 import Contacts from '@/src/components/common/Contacts/Contacts'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 
-type ContactsProps = {
+type Props = {
   section: ContactsSectionFragment
 }
 
-const ContactsSection = ({ section }: ContactsProps) => {
+/**
+ * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=19108-18946&t=aBqs3sK8a9uFmnaX-4
+ */
+
+const ContactsSection = ({ section }: Props) => {
   return (
     <SectionContainer>
       <Contacts section={section} />

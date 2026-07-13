5d56597b2ef6609263648006ef436a518cb7d9a9
shegood <petrsigut10@gmail.com>
2026-07-13 13:18:21 +0200

    fix paddings

--- next/src/components/page-contents/TaxesFees/TaxFeePageContent/DznAccordionTableTaxContent.tsx
+++ next/src/components/page-contents/TaxesFees/TaxFeePageContent/DznAccordionTableTaxContent.tsx
@@ -177,7 +177,7 @@ const DznAccordionTableTaxContent = ({
   data,
 }: DznAccordionTableTaxContentProps) => {
   return (
-    <DisclosureGroup className="p-4py-2 w-full rounded-lg border border-border-active-default bg-background-passive-base">
+    <DisclosureGroup className="w-full rounded-lg border border-border-active-default bg-background-passive-base px-4 py-2">
       <Disclosure>
         <DisclosureHeader className="px-4 py-2 ring-inset lg:px-6 lg:py-3">
           <div className="flex w-full justify-between pr-4">

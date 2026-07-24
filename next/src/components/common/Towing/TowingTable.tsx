b4ce40f2fa473c079bef8917388f54ff839f9427
Timotej Džugas <dzugas.timotej@gmail.com>
2026-07-24 13:04:14 +0200

    Move towreason translations from backend to frontend (#4476)
    * base for new map for frontend
    
    * updated the code after client regeneration
    
    * Refactor translations slightly
    
    ---------
    
    Co-authored-by: Ty-ci <teachy.jaho@gmail.com>

--- next/src/components/common/Towing/TowingTable.tsx
+++ next/src/components/common/Towing/TowingTable.tsx
@@ -1,7 +1,10 @@
 import { Typography } from '@bratislava/component-library'
 import { useTranslation } from 'next-i18next/pages'
-import { TowingSearchResponseDto } from 'openapi-clients/city-account'
-import { useState } from 'react'
+import {
+  TowingSearchResponseDto,
+  TowingSearchResponseDtoTowReasonEnum,
+} from 'openapi-clients/city-account'
+import { useMemo, useState } from 'react'
 
 import Table from '@/src/components/common/Table/Table'
 import { formatDate } from '@/src/components/formatting/FormatDate'
@@ -14,18 +17,56 @@ type Props = {
 }
 
 const TowingTable = ({ vehicle, initialLicensePlate }: Props) => {
-  const [licensePlate] = useState(initialLicensePlate) // TODO: Remove this once the license plate is part of the response
   const { t } = useTranslation('account')
+
+  const [licensePlate] = useState(initialLicensePlate) // TODO: Remove this once the license plate is part of the response
   const variant = vehicle.unloadingLocation ? 'relay' : 'towing'
 
-  const titleTranslationMap = {
+  const title = {
     relay: t('towing.informationTitle.relay', { licensePlate }),
     towing: t('towing.informationTitle.towing', { licensePlate }),
-  }
+  }[variant]
+
+  const towReasonTranslation = useMemo(() => {
+    const towReasonTranslationMap: Record<TowingSearchResponseDtoTowReasonEnum, string> = {
+      [TowingSearchResponseDtoTowReasonEnum.ReservedParking]: t(
+        'towing.towReason.RESERVED_PARKING',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.PrivateAccessObstacle]: t(
+        'towing.towReason.PRIVATE_ACCESS_OBSTACLE',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.ParkingNearPublicTransportStop]: t(
+        'towing.towReason.PARKING_NEAR_PUBLIC_TRANSPORT_STOP',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.ParkingNearPedestrianCrossing]: t(
+        'towing.towReason.PARKING_NEAR_PEDESTRIAN_CROSSING',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.ParkingOnSidewalk]: t(
+        'towing.towReason.PARKING_ON_SIDEWALK',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.ParkingInTrafficLane]: t(
+        'towing.towReason.PARKING_IN_TRAFFIC_LANE',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.ParkingAtStreetCrossing]: t(
+        'towing.towReason.PARKING_AT_STREET_CROSSING',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.Other]: t('towing.towReason.OTHER'),
+      [TowingSearchResponseDtoTowReasonEnum.TrafficFlowObstacle]: t(
+        'towing.towReason.TRAFFIC_FLOW_OBSTACLE',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.NoStoppingZone]: t('towing.towReason.NO_STOPPING_ZONE'),
+      [TowingSearchResponseDtoTowReasonEnum.StoppedAtCrosswalk]: t(
+        'towing.towReason.STOPPED_AT_CROSSWALK',
+      ),
+      [TowingSearchResponseDtoTowReasonEnum.NoParkingZone]: t('towing.towReason.NO_PARKING_ZONE'),
+    }
+
+    return vehicle.towReason ? towReasonTranslationMap[vehicle.towReason] : undefined
+  }, [t, vehicle.towReason])
 
   return (
     <>
-      <Typography variant="h3">{titleTranslationMap[variant]}</Typography>
+      <Typography variant="h3">{title}</Typography>
 
       <div className="flex flex-col gap-4">
         <Table
@@ -44,7 +85,12 @@ const TowingTable = ({ vehicle, initialLicensePlate }: Props) => {
               value: vehicle.loadingLocation,
             },
             ...(vehicle.towReason
-              ? [{ label: t('towing.informationTable.towReason'), value: vehicle.towReason }]
+              ? [
+                  {
+                    label: t('towing.informationTable.towReason'),
+                    value: towReasonTranslation ?? vehicle.towReason,
+                  },
+                ]
               : []),
             ...(vehicle.unloadingLocation
               ? [

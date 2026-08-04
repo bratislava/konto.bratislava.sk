85c6eccd35707a51faf5c35358aa3ba5d29a0f70
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-08-04 10:59:49 +0200

    Use Disclosure on respo

--- next/src/components/common/TableOfContents/TableOfContents.tsx
+++ next/src/components/common/TableOfContents/TableOfContents.tsx
@@ -1,8 +1,11 @@
 import { Button, Typography } from '@bratislava/component-library'
 import { useTranslation } from 'next-i18next/pages'
-import { ReactNode } from 'react'
+import { Fragment, ReactNode } from 'react'
 
 import useHeadings from '@/src/components/common/TableOfContents/useHeadings'
+import Disclosure from '@/src/components/simple-components/Disclosure/Disclosure'
+import DisclosureHeader from '@/src/components/simple-components/Disclosure/DisclosureHeader'
+import DisclosurePanel from '@/src/components/simple-components/Disclosure/DisclosurePanel'
 import HorizontalDivider from '@/src/components/simple-components/HorizontalDivider'
 import cn from '@/src/utils/cn'
 
@@ -42,6 +45,33 @@ const TableOfContents = ({
     window.scrollTo({ top: offsetPosition, behavior: 'smooth' })
   }
 
+  const tocTitle = (
+    <Typography variant="h5" as="h2">
+      {t('TableOfContents.title')}
+    </Typography>
+  )
+
+  const tocLinks = (
+    <ul className="flex flex-col px-4 py-2 lg:px-6">
+      {headings.map((heading) => {
+        return (
+          <li key={heading.id} className="py-2 lg:py-3">
+            <Button
+              variant="link"
+              onPress={() => {
+                handleItemPress(heading.id)
+              }}
+              // TODO remove when Button is updated in https://github.com/bratislava/component-library/pull/48
+              className="text-left"
+            >
+              {heading.text}
+            </Button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+
   return (
     <div
       className={cn(
@@ -51,32 +81,17 @@ const TableOfContents = ({
     >
       {headings.length ? (
         <>
-          <div className="p-4 lg:p-6">
-            <Typography variant="h5" as="h2">
-              {t('TableOfContents.title')}
-            </Typography>
+          <Disclosure className="lg:hidden">
+            <DisclosureHeader className="p-4 ring-inset open:border-b">{tocTitle}</DisclosureHeader>
+            {/* TODO remove "*:py-0" class, when our DisclosurePanel has option to disable the padding */}
+            <DisclosurePanel className="*:py-0">{tocLinks}</DisclosurePanel>
+          </Disclosure>
+
+          <div className="max-lg:hidden">
+            <div className="p-4 lg:p-6">{tocTitle}</div>
+            <HorizontalDivider />
+            {tocLinks}
           </div>
-
-          <HorizontalDivider />
-
-          <ul className="flex flex-col px-4 py-2 lg:px-6">
-            {headings.map((heading) => {
-              return (
-                <li key={heading.id} className="py-2 lg:py-3">
-                  <Button
-                    variant="link"
-                    onPress={() => {
-                      handleItemPress(heading.id)
-                    }}
-                    // TODO remove when Button is updated in https://github.com/bratislava/component-library/pull/48
-                    className="text-left"
-                  >
-                    {heading.text}
-                  </Button>
-                </li>
-              )
-            })}
-          </ul>
         </>
       ) : null}
 

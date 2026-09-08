0a7cd9fb84432f5ccafbc9a28274af5a0937f020
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-09-08 15:07:18 +0200

    Improve heading levels in tax detail page + minor styling fixes

--- next/src/components/page-contents/TaxesPageContent/TaxPageContent/TaxDetails.tsx
+++ next/src/components/page-contents/TaxesPageContent/TaxPageContent/TaxDetails.tsx
@@ -36,7 +36,7 @@ const TaxDetails = () => {
   return (
     <div className="flex w-full flex-col items-start gap-3 lg:gap-6">
       <div className="flex w-full flex-col justify-between lg:flex-row">
-        <Typography variant="h5" as="p" className="font-semibold">
+        <Typography variant="h5" as="h2">
           {taxAccordionsHeader}
         </Typography>
         <Button variant="link" endIcon={<Icon name="export" />} {...taxAccordionsHeaderLinkProps} />

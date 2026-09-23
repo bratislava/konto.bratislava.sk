12d63ac4022a5d010424990f6360fc631b97bbdb
Jakub Hvolka <jhvolka@gmail.com>
2026-09-23 16:13:49 +0200

    Apply @AllowList decorator to AppController

--- nest-city-account/src/app.controller.ts
+++ nest-city-account/src/app.controller.ts
@@ -1,7 +1,9 @@
+import { AllowList } from '@bratislava/log-nest'
 import { Controller, Get } from '@nestjs/common'
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @Controller()
+@AllowList(true)
 @ApiTags('default')
 export class AppController {
   @ApiOperation({
